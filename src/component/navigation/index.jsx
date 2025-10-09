4cfc726936fe1c3dfdf434cfe94d05e1b60d07fc
amir <amirtaki623@gmail.com>
2025-10-09 22:47:09 +0330

    part 30

--- src/component/navigation/index.jsx
+++ src/component/navigation/index.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react"
 import { BrowserRouter, Link } from "react-router-dom"
+import LinkNavigation from "./linkNavigation"
+
+
 
 const Navigation = () => {
     const [toggle, setToggle] = useState(true)
@@ -22,6 +25,7 @@ const Navigation = () => {
                 <div className={`${toggle ? "hidden" : "flex"}`}>
 
                     <BrowserRouter>
+                        <LinkNavigation />
                         <li className="absolute left-0  [transform-origin:100px] transform duration-500 delay-200">
                             <Link to="/" className="flex justify-center items-center w-[40px] h-[40px] bg-white rounded-full text-[#111] shadow-[0_3px_4px_rgba(0,0,0,0.15)]  ">
                                 <i className="bi bi-house-door "></i>
