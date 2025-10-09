fb69f21bfd6bab68f796b6bbadcc02392541ec3d
amir <amirtaki623@gmail.com>
2025-10-09 23:27:20 +0330

    part 50

--- src/component/navigation/linkNavigation.jsx
+++ src/component/navigation/linkNavigation.jsx
@@ -4,14 +4,16 @@ import { Link } from "react-router-dom";
 const LinkNavigation = ({icon, conter}) => {
     return (
 
-        <li className={`
+        <li 
+            
+        className={`
             absolute left-0 origin-[100px] transition-all duration-500 delay-[clac(.1s*${conter})] transform-[rotate(-deg)_translateX(80px)]`}
             >
             <Link 
-                state={{transform : `rotate(calc(360deg / -8 * ${conter}))`}}
+                style={{transform : `rotate(calc(360deg / -8 * ${conter}))`}}
                 to="/" 
                 className={`flex justify-center items-center w-[40px] h-[40px] bg-white rounded-full text-[#111] shadow-[0_3px_4px_rgba(0,0,0,0.15)]  
-          
+                 [transform-origin:80px_center]
                 `}>
                 <i className={icon}></i>
             </Link>
