12be49ac890f49585505cfbd9598adc64ec772da
amir <amirtaki623@gmail.com>
2025-10-09 22:10:08 +0330

    part 24

--- src/component/navigation/index.jsx
+++ src/component/navigation/index.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Link } from "react-router-dom"
 
 const [toggle, setToggle]  =  useState(true)
 const handelrToggle = () => {
-    
+    setToggle( (prevToggle) => (!prevToggle) )    
 }
 
 const Navigation = () => {
