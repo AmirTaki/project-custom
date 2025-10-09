4285dd40f712756bc7950b564c3f0b95ab98f2a2
amir <amirtaki623@gmail.com>
2025-10-09 22:13:16 +0330

    part 25

--- src/component/navigation/index.jsx
+++ src/component/navigation/index.jsx
@@ -1,73 +1,77 @@
 import { useState } from "react"
 import { BrowserRouter, Link } from "react-router-dom"
 
-const [toggle, setToggle]  =  useState(true)
+const [toggle, setToggle]  = useState(true)
+
 const handelrToggle = () => {
-    setToggle( (prevToggle) => (!prevToggle) )    
+    setToggle((prevToggle) => !prevToggle)    
 }
 
 const Navigation = () => {
     return (
         <div
             className="absolute left-[50%] top-[50%] list-none"
-            >
+            >className={`${toggle ? "hidden" : "flex"}`}
             <div 
-                onClick={() => {handelrToggle()}}
+                // onClick={() => {handelrToggle()}}
                 className="[transform-origin:100px] transform transition-[.5s] "
             >
                 <i className="bi bi-plus-lg"></i>
             </div>
-            <BrowserRouter >
-            
-                <li>
-                    <Link to = "/">
-                        <i className="bi bi-house-door"></i>
-                    </Link>
-                </li>
-            
-                <li>
-                    <Link to = "/">
-                        <i className="bi bi-person"></i>
-                    </Link>
-                </li>
-
-                <li>
-                    <Link to = "/">
-                        <i className="bi bi-gear"></i>
-                    </Link>
-                </li>
+            <div >
                 
-                <li>
-                    <Link to = "/">
-                        <i className="bi bi-envelope"></i>
-                    </Link>
-                </li>
+                <BrowserRouter >
+                
+                    <li>
+                        <Link to = "/">
+                            <i className="bi bi-house-door"></i>
+                        </Link>
+                    </li>
+                
+                    <li>
+                        <Link to = "/">
+                            <i className="bi bi-person"></i>
+                        </Link>
+                    </li>
 
-                <li>
-                    <Link to = "/">
-                        <i className="bi bi-key"></i>
-                    </Link>
-                </li>
-          
-                <li>
-                    <Link to = "/">
-                        <i className="bi bi-camera-video"></i>
-                    </Link>
-                </li>
-          
-                <li>
-                    <Link to = "/">
-                        <i className="bi bi-controller"></i>
-                    </Link>
-                </li>
+                    <li>
+                        <Link to = "/">
+                            <i className="bi bi-gear"></i>
+                        </Link>
+                    </li>
+                    
+                    <li>
+                        <Link to = "/">
+                            <i className="bi bi-envelope"></i>
+                        </Link>
+                    </li>
 
-                <li>
-                    <Link to = "/">
-                        <i className="bi bi-camera"></i>
-                    </Link>
-                </li>
-          
-            </BrowserRouter>
+                    <li>
+                        <Link to = "/">
+                            <i className="bi bi-key"></i>
+                        </Link>
+                    </li>
+            
+                    <li>
+                        <Link to = "/">
+                            <i className="bi bi-camera-video"></i>
+                        </Link>
+                    </li>
+            
+                    <li>
+                        <Link to = "/">
+                            <i className="bi bi-controller"></i>
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link to = "/">
+                            <i className="bi bi-camera"></i>
+                        </Link>
+                    </li>
+            
+                </BrowserRouter>
+            </div>
         </div>
     )
 }
