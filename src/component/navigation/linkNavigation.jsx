a1a5c4ff73970d214a288bae0e534eca704da4a6
amir <amirtaki623@gmail.com>
2025-10-09 23:25:55 +0330

    part 49

--- src/component/navigation/linkNavigation.jsx
+++ src/component/navigation/linkNavigation.jsx
@@ -8,11 +8,12 @@ const LinkNavigation = ({icon, conter}) => {
             absolute left-0 origin-[100px] transition-all duration-500 delay-[clac(.1s*${conter})] transform-[rotate(-deg)_translateX(80px)]`}
             >
             <Link 
+                state={{transform : `rotate(calc(360deg / -8 * ${conter}))`}}
                 to="/" 
                 className={`flex justify-center items-center w-[40px] h-[40px] bg-white rounded-full text-[#111] shadow-[0_3px_4px_rgba(0,0,0,0.15)]  
-                transform-rotate-[calc(360deg/-8*${conter})]
+          
                 `}>
-                <i className={icon}>{conter}</i>
+                <i className={icon}></i>
             </Link>
         </li>
         //            
