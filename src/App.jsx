244e526363b0ccf92f2bc7c882ebaa2945444400
amir <amirtaki623@gmail.com>
2025-10-09 21:58:13 +0330

    part 10

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ function App() {
     <div className="bg-linear-[25deg,red_5%,yellow_60%,lime_90%,teal] h-[100vh]">
 
        <Navigation />
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias cumque esse, ab tenetur, harum explicabo excepturi placeat veniam amet qui in, alias obcaecati veritatis commodi delectus deserunt. Sint, asperiores fuga!
     </div>
     </>
   )
