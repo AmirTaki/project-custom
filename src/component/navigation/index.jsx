a1d96bc3279390ea009d9e21f62be223a417a114
amir <amirtaki623@gmail.com>
2025-10-09 22:00:21 +0330

    part 11

--- src/component/navigation/index.jsx
+++ src/component/navigation/index.jsx
@@ -3,7 +3,9 @@ const Navigation = () => {
         <div
             className="absolute left-[50%] top-[50%]"
             >
-            <div className=""><i className="bi bi-plus-lg"></i></div>
+            <div 
+                className="[transform-origin:100px] transform transition-[.5s] "
+            ><i className="bi bi-plus-lg"></i></div>
         </div>
     )
 }
