fc05d1e1080086a84eee9db0bfcaf5f790a802d5
amir <amirtaki623@gmail.com>
2025-10-09 22:47:59 +0330

    part 31

--- src/component/navigation/linkNavigation.jsx
+++ src/component/navigation/linkNavigation.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
+
+
 const LinkNavigation = () => {
     return (
-        <div className="">
-            link
-        </div>
+        <li className="absolute left-0  [transform-origin:100px] transform duration-500 delay-200">
+            <Link to="/" className="flex justify-center items-center w-[40px] h-[40px] bg-white rounded-full text-[#111] shadow-[0_3px_4px_rgba(0,0,0,0.15)]  ">
+                <i className="bi bi-house-door "></i>
+            </Link>
+        </li>
     )
 }
 
