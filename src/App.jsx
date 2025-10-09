8353a1298a7ecfbc3bef6756dec59ec0c4d9e08f
amir <amirtaki623@gmail.com>
2025-10-09 21:48:39 +0330

    part 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
+import Navigation from './component/navigation'
 
 function App() {
-
+  
   return (
     <>
     <div className="text-blue-500">
 
+       <Navigation />
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias cumque esse, ab tenetur, harum explicabo excepturi placeat veniam amet qui in, alias obcaecati veritatis commodi delectus deserunt. Sint, asperiores fuga!
     </div>
     </>
