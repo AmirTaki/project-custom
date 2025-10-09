45769004240f19880fa8dd368bb40907f198c6cc
amir <amirtaki623@gmail.com>
2025-10-09 21:43:29 +0330

    part 2

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ function App() {
 
   return (
     <>
+    <div className="text-blue-500">
+
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias cumque esse, ab tenetur, harum explicabo excepturi placeat veniam amet qui in, alias obcaecati veritatis commodi delectus deserunt. Sint, asperiores fuga!
+    </div>
     </>
   )
 }
