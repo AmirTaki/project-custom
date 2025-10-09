db09bfd832c644e8287c5bd88632be7c33715146
amir <amirtaki623@gmail.com>
2025-10-09 23:35:23 +0330

    part 54

--- src/component/navigation/linkNavigation.jsx
+++ src/component/navigation/linkNavigation.jsx
@@ -8,13 +8,13 @@ const LinkNavigation = ({icon, conter}) => {
             style={{'--i': conter }}
             className={`
                 [transition-delay:calc(0.1s*var(--i))] 
-                absolute left-0 origin-[100px] transition-all duration-500  transform-[rotate(0deg)_translateX(80px)]`}
+                absolute -left-39 top-3  origin-[100px] transition-all duration-500  transform-[rotate(0deg)_translateX(80px)]`}
             >
             <Link 
                 style={{transform : `rotate(calc(360deg / -8 * ${conter}))`}}
                 to="/" 
                 className={`flex justify-center items-center w-[40px] h-[40px] bg-white rounded-full text-[#111] shadow-[0_3px_4px_rgba(0,0,0,0.15)]  
-                 [transform-origin:80px_center]
+                 [transform-origin:100px_center]
                 `}>
                 <i className={icon}></i>
             </Link>
