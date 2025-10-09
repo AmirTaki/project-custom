de4ba4f09d99ef6854f3344e4713891f8568f06c
amir <amirtaki623@gmail.com>
2025-10-09 23:42:46 +0330

    part 56

--- src/component/navigation/linkNavigation.jsx
+++ src/component/navigation/linkNavigation.jsx
@@ -5,15 +5,16 @@ const LinkNavigation = ({icon, conter}) => {
     return (
 
         <li 
-            style={{transitionDelay: `calc(.1s * ${conter})` }}
+            style={{transitionDelay: `calc(.1s * ${conter})`,
+            transform : `rotate(calc(360deg / -8 * ${conter}))` }}
             className={`
-                absolute  origin-[100px] transition-all duration-500  transform-[rotate(0deg)_translateX(0px)]`}
+                absolute  origin-[100px]  transition-all duration-500  transform-[rotate(0deg)_translateX(0px)]`}
             >
             <Link 
                 style={{transform : `rotate(calc(360deg / -8 * ${conter}))`}}
                 to="/" 
                 className={`flex justify-center items-center w-[40px] h-[40px] bg-white rounded-full text-[#111] shadow-[0_3px_4px_rgba(0,0,0,0.15)]  
-                 [transform-origin:100px_center]
+                 [transform-origin:100px_center] duration-500!
                 `}>
                 <i className={icon}></i>
             </Link>
