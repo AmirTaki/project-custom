833b7e77cacbfac29dca642c5b3af64a40f63db8
amir <amirtaki623@gmail.com>
2025-10-10 15:37:02 +0330

    day 2 part 3-

--- src/component/9DoctsMenu/linkMenu.jsx
+++ src/component/9DoctsMenu/linkMenu.jsx
@@ -6,12 +6,13 @@ const LinkMenu = ({item, toggle, index}) => {
             <span
                 style={{
                     transform : `translate(clac(12px * ${item.x}), clac(12px * ${item.y}))`,
-                    transitionDelay : `calc(.1 * ${index})`
+                    transitionDelay : `calc(.1s * ${index})`
                         
                 }}
                 className={`
                     absolute w-[7px] h-[7px] flex items-center justify-centerbg-white rounded-[50%] 
-                    
+                    transition-transform transition-width transition-height transition-colors 
+                    duration-500
                     `}
             >
                 <i className={item.icon}></i>
