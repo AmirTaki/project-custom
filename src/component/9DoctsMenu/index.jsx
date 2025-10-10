ee585373ab239c7650a31d2bc113e76a26559ebd
amir <amirtaki623@gmail.com>
2025-10-10 14:52:23 +0330

    day 2 part 20

--- src/component/9DoctsMenu/index.jsx
+++ src/component/9DoctsMenu/index.jsx
@@ -1,16 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
 import { iconsList } from "./iconList";
 import LinkMenu from "./linkMenu";
+import { useState } from "react";
 
 const DoctsMenu = () => {
 
+    const [toggle, setToggle] = useState(false)
     return(
         // navigation
         <div className="
             relative w-[70px] h-[70px] bg-[#212532] rounded-[10px] cursor-pointer
             flex items-center justify-center duration-[500ms] delay-[800ms]
         ">
-
             <BrowserRouter>
                 {iconsList.map((item, index) => {
                     return(
