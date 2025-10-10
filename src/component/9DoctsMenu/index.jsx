d3d555fa5941f06a824d5fbfd8720e50b3d1573d
amir <amirtaki623@gmail.com>
2025-10-10 14:42:37 +0330

    day 2 part 13

--- src/component/9DoctsMenu/index.jsx
+++ src/component/9DoctsMenu/index.jsx
@@ -1,12 +1,16 @@
+import { BrowserRouter } from "react-router-dom";
 import { iconsList } from "./iconList";
-
 
 const DoctsMenu = () => {
 
     return(
+        // navigation
         <div className="">
 
-            
+            <BrowserRouter>
+                
+            </BrowserRouter>
+
         </div>
     )
 }
