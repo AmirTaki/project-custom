1471519ae0caa21c5687fe2e1140e49a829beb97
amir <amirtaki623@gmail.com>
2025-10-10 14:34:59 +0330

    day 2 part 6

---
+++ src/component/9DoctsMenu/iconList.jsx
@@ -0,0 +1,25 @@
+export const iconsList = [
+    {
+        icon : "bi bi-camera",
+        x : -1 ,
+        y : 0
+    },
+    {
+        icon : "bi bi-camera",
+        x : -1 ,
+        y : 0
+    },
+    {
+        icon : "bi bi-camera",
+        x : -1 ,
+        y : 0
+    },
+    {
+        icon : "bi bi-camera",
+        x : -1 ,
+        y : 0
+    },
+    
+    "bi bi-gem", "bi bi-chat", "bi bi-alarm",  "bi bi-controller", "bi bi-moon",  "bi bi-bell", "bi bi-droplet",  "bi bi-clock" ]
+
+]        
--- src/component/9DoctsMenu/index.jsx
+++ src/component/9DoctsMenu/index.jsx
@@ -1,10 +1,5 @@
 const DoctsMenu = () => {
-    const iconsList = [
-        {
-            icon : "bi bi-camera",
-            x : -1 ,
-            y : 0
-        }, "bi bi-gem", "bi bi-chat", "bi bi-alarm",  "bi bi-controller", "bi bi-moon",  "bi bi-bell", "bi bi-droplet",  "bi bi-clock" ]
+
     return(
         <div className="">
 
