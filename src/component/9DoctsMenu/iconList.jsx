a7fc179cccdccd1303650f2769b44dd3a36fe2a2
amir <amirtaki623@gmail.com>
2025-10-10 14:38:32 +0330

    day 2 part 8

--- src/component/9DoctsMenu/iconList.jsx
+++ src/component/9DoctsMenu/iconList.jsx
@@ -15,9 +15,24 @@ export const iconsList = [
         y : -1
     },
     {
-        icon : "bi bi-camera",
-        x : -1 ,
-        y : 0
+        icon : "bi bi-alarm",
+        x : 0 ,
+        y : 1
+    },
+    {
+        icon : "bi bi-alarm",
+        x : 0 ,
+        y : 1
+    },
+    {
+        icon : "bi bi-alarm",
+        x : 0 ,
+        y : 1
+    },
+    {
+        icon : "bi bi-alarm",
+        x : 0 ,
+        y : 1
     },
     
     "", "", "bi bi-alarm",  "bi bi-controller", "bi bi-moon",  "bi bi-bell", "bi bi-droplet",  "bi bi-clock" ]
