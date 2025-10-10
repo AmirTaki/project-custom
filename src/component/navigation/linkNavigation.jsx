ed49a11339ff8cc1a9f413366a8e92fae6f8cca5
amir <amirtaki623@gmail.com>
2025-10-10 03:34:43 +0330

    day 1 part 4

--- src/component/navigation/linkNavigation.jsx
+++ src/component/navigation/linkNavigation.jsx
@@ -10,7 +10,7 @@ const LinkNavigation = ({icon, index, toggle}) => {
          }}
             className={`
                
-                absolute left-0 list-none origin-[100px] transition-all duration-500 
+                absolute top-3 -left-37  list-none origin-[100px] transition-all duration-500 
                 `
             }
             >
@@ -18,7 +18,7 @@ const LinkNavigation = ({icon, index, toggle}) => {
                 style={{transform : `${toggle ? `` : `rotate(calc(360deg / -8 * ${index}))`} `}}
                 to="/" 
                 className={`flex justify-center items-center w-[40px] h-[40px] bg-white rounded-full text-[#111] shadow-[0_3px_4px_rgba(0,0,0,0.15)]  
-                 [transform-origin:100px_center] duration-500!
+                 [transform-origin:100px] duration-500!
                 `}>
                 <i className={icon}>{index}</i>
             </Link>
