539b4f948725ae59fc072747065d5d8f7412de59
amir <amirtaki623@gmail.com>
2025-10-10 14:32:02 +0330

    day 2 part 4

--- src/component/9DoctsMenu/index.jsx
+++ src/component/9DoctsMenu/index.jsx
@@ -1,4 +1,5 @@
 const DoctsMenu = () => {
+    const iconsList = ["bi bi-camera", "bi bi-gem", "bi bi-chat", "bi bi-alarm",  "bi bi-controller", "bi bi-moon",  "bi bi-bell", "bi bi-droplet",  "bi bi-clock" ]
     return(
         <div className="">
 
