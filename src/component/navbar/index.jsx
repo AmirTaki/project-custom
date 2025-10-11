7c6aad207b955feadb849408b5fc7a9e7e43c3d6
amir <amirtaki623@gmail.com>
2025-10-11 20:50:19 +0330

    day 3 part 45

--- src/component/navbar/index.jsx
+++ src/component/navbar/index.jsx
@@ -30,15 +30,18 @@ const Navbar = () => {
                     </div>
                 </div>
                                
-                <div className={`${dropMenu ? "h-0  " : "   lg:justify-between lg:items-center lg:flex lg:gap-10 max-lg:hidden"}`}>
+                <div className={`${dropMenu ? " lg:flex " : "lg:justify-between lg:items-center lg:flex lg:gap-10 max-lg:hidden"}  `}>
                     <ul className={`${dropMenu ?
-                        "flex flex-col items-center justify-center absolute right-[2rem] top-[60px] transition-[height]! duration-200! ease-out! h-[240px]! w-[300px] bg-[rgba(255,255,255,.1)] overflow-hidden"
-                        : 'flex justify-center items-center gap-10 text-lg h-0!'}`}
+                        "flex flex-col items-center justify-center absolute right-[2rem]  top-[60px] transition-[height] duration-200 ease-[cubic-bezier(.175,.885,.32,1.275)] h-[280px]! w-[300px] bg-[rgba(0,0,0,.2)]  overflow-hidden"
+                        : 'flex justify-center items-center gap-10 text-lg h-0 transition-[height] duration-200 ease-[cubic-bezier(.175,.885,.32,1.275)] '}`}
                     >
-                        <li  className={`${dropMenu ? " p-[.7rem] text-lg" : "text-md"} cursor-pointer duration-300 hover:text-[orange] `} ><Link to = '/'>Home</Link></li>
-                        <li  className={`${dropMenu ? " p-[.7rem] text-lg" : "text-md"} cursor-pointer duration-300 hover:text-[orange] `} ><Link to = '/'>About</Link></li>
-                        <li  className={`${dropMenu ? " p-[.7rem] text-lg" : "text-md"} cursor-pointer duration-300 hover:text-[orange] `} ><Link to = '/'>Services</Link></li>
-                        <li  className={`${dropMenu ? " p-[.7rem] text-lg" : "text-md"} cursor-pointer duration-300 hover:text-[orange] `} ><Link to = '/'>Contact</Link></li>
+                        <li  className={`${dropMenu ? " p-[.7rem] text-md" : "text-md"} cursor-pointer duration-300 hover:text-[orange] `} ><Link to = '/'>Home</Link></li>
+                        <li  className={`${dropMenu ? " p-[.7rem] text-md" : "text-md"} cursor-pointer duration-300 hover:text-[orange] `} ><Link to = '/'>About</Link></li>
+                        <li  className={`${dropMenu ? " p-[.7rem] text-md" : "text-md"} cursor-pointer duration-300 hover:text-[orange] `} ><Link to = '/'>Services</Link></li>
+                        <li  className={`${dropMenu ? " p-[.7rem] text-md" : "text-md"} cursor-pointer duration-300 hover:text-[orange] `} ><Link to = '/'>Contact</Link></li>
+                        <div className="border-0 w-[80%] text-center oumdine-0 px-[1rem] py-[.5rem] rounded-[20px] cursor-pointer text-[.8rem] font-bold bg-[orange] hover:scale-111 duration-200 active:scale-95 lg:hidden!">
+                            Get Started
+                        </div>
                     </ul>
                 </div>
                 <div className="flex items-center justify-center  max-lg:hidden ">
