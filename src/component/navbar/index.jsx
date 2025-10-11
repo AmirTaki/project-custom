7f53851aaba9c871b2d529e727629dc7f2bbfecf
amir <amirtaki623@gmail.com>
2025-10-11 17:23:34 +0330

    day 3 part 17

--- src/component/navbar/index.jsx
+++ src/component/navbar/index.jsx
@@ -1,14 +1,27 @@
+import { BrowserRouter, Link } from "react-router-dom"
+
+
 const Navbar = () => {
     return(
         <div className="flex justify-between items-center bg-blue-500">
-            <div className="  h-20 bg-red-500 flex items-center justify-center">
-                {/* logo */}
-                <div className="">
-                    <a href="">Web developer programming</a>
+            <BrowserRouter >
+                <div className="  h-20 bg-red-500 flex items-center justify-center">
+                    {/* logo */}
+                    <div className="">
+                        <a href="">Web developer programming</a>
+                    </div>
                 </div>
-            </div>
-            <div className=" w-10 h-20 bg-green-500"></div>
-            <div className=" w-10 h-20 bg-orange-500"></div>
+                <div className=" w-10 h-20 bg-green-500">
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+                <div className=" w-10 h-20 bg-orange-500"></div>
+            </BrowserRouter>
         </div>
     )
 }
