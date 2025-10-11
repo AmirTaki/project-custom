4e7218b83404f611dd915b2d03de7fdca96af024
amir <amirtaki623@gmail.com>
2025-10-11 14:34:56 +0330

    day 3 part 1

--- src/component/9DoctsMenu/index.jsx
+++ src/component/9DoctsMenu/index.jsx
@@ -10,9 +10,9 @@ const DoctsMenu = () => {
         // navigation
         <div 
             className={` 
-                ${toggle ? "w-[200px] h-[200px] delay-[0ms] " : "w-[70px] h-[70px] delay-[800ms]"}
-                  bg-[#212532] rounded-[10px] cursor-pointer 
-                 duration-[500ms]  flex  items-center justify-center fixed  left-50 top-50` 
+                ${toggle ? "w-[200px] h-[200px] delay-0 " : "w-[70px] h-[70px] delay-[800ms]"}
+                  bg-[#212532] rounded-[10px] cursor-pointer  relative
+                 duration-[500ms] flex items-center  justify-center` 
             }
             onClick={() => {setToggle((prev) => (!prev))}}
             >
