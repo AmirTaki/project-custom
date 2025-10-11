540b05662cf1366cb81715125afd9be6aa46c1f5
amir <amirtaki623@gmail.com>
2025-10-11 19:38:49 +0330

    day 3 part 34

--- src/component/navbar/index.jsx
+++ src/component/navbar/index.jsx
@@ -4,17 +4,17 @@ import { BrowserRouter, Link } from "react-router-dom"
 const Navbar = () => {
     return(
         <div className="flex justify-between items-center  px-[4rem] text-white
-            w-[100%] fixed bg-[rgba(0,0,0,.2)]
+            w-[100%] fixed bg-[rgba(0,0,0,.2)] h-[60px]
         ">
             <BrowserRouter >
-                <div className="  h-20 flex items-center justify-center ">
+                <div className="flex items-center justify-center  ">
                     {/* logo */}
                     <div className="">
                         <a className="text-[1.5rem] font-bold  cursor-pointer duration-300 hover:text-[orange]" href="">Web developer </a>
                     </div>
                 </div>
-                <div className="  h-20 
-                  justify-between items-center flex gap-10
+                <div className="  
+                  justify-between items-center flex gap-10 max-lg:hidden
                 ">
                     <ul className="  flex justify-center items-center gap-10 text-lg">
                         <li  className="cursor-pointer duration-300 hover:text-[orange]"><Link to = '/'>Home</Link></li>
@@ -23,13 +23,16 @@ const Navbar = () => {
                         <li  className="cursor-pointer duration-300 hover:text-[orange]"><Link to = '/'>Contact</Link></li>
                     </ul>
                 </div>
-                <div className="flex items-center justify-center  h-20">
+                <div className="flex items-center justify-center  max-lg:hidden ">
                     <div className="border-0 outline-0 px-[1rem] py-[.5rem]
                         rounded-[20px] cursor-pointer text-[.8rem] font-bold bg-[orange]
                         hover:scale-111 duration-200 active:scale-95
                     ">
                         Get Started
                     </div>
+                </div>
+                <div className="lg:hidden">
+                    <div className="toggle_btn"><i className="fa-solid fa-bars"></i></div>
                 </div>
                 
             </BrowserRouter>
