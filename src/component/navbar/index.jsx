6b27733985553cf90e744b86f6352237836f9759
amir <amirtaki623@gmail.com>
2025-10-11 21:41:53 +0330

    day 3 part 46

--- src/component/navbar/index.jsx
+++ src/component/navbar/index.jsx
@@ -18,6 +18,9 @@ const Navbar = () => {
       }
     },[] )
 
+        //  "lg:flex flex-col items-center justify-center absolute right-[2rem]  top-[60px] transition-[height] duration-200 ease-[cubic-bezier(.175,.885,.32,1.275)] h-[280px]! w-[300px] bg-[rgba(0,0,0,.2)]  overflow-hidden"
+        //                 : 'flex justify-center items-center gap-10 text-lg h-0 transition-[height] duration-200 ease-[cubic-bezier(.175,.885,.32,1.275)]  lg:justify-between lg:items-center lg:flex lg:gap-10 max-lg:hidden'
+
     return(
         <div className="flex justify-between items-center  px-[4rem] text-white
             w-[100%] fixed bg-[rgba(0,0,0,.2)] h-[60px]
@@ -30,20 +33,17 @@ const Navbar = () => {
                     </div>
                 </div>
                                
-                <div className={`${dropMenu ? " lg:flex " : "lg:justify-between lg:items-center lg:flex lg:gap-10 max-lg:hidden"}  `}>
-                    <ul className={`${dropMenu ?
-                        "flex flex-col items-center justify-center absolute right-[2rem]  top-[60px] transition-[height] duration-200 ease-[cubic-bezier(.175,.885,.32,1.275)] h-[280px]! w-[300px] bg-[rgba(0,0,0,.2)]  overflow-hidden"
-                        : 'flex justify-center items-center gap-10 text-lg h-0 transition-[height] duration-200 ease-[cubic-bezier(.175,.885,.32,1.275)] '}`}
-                    >
-                        <li  className={`${dropMenu ? " p-[.7rem] text-md" : "text-md"} cursor-pointer duration-300 hover:text-[orange] `} ><Link to = '/'>Home</Link></li>
-                        <li  className={`${dropMenu ? " p-[.7rem] text-md" : "text-md"} cursor-pointer duration-300 hover:text-[orange] `} ><Link to = '/'>About</Link></li>
-                        <li  className={`${dropMenu ? " p-[.7rem] text-md" : "text-md"} cursor-pointer duration-300 hover:text-[orange] `} ><Link to = '/'>Services</Link></li>
-                        <li  className={`${dropMenu ? " p-[.7rem] text-md" : "text-md"} cursor-pointer duration-300 hover:text-[orange] `} ><Link to = '/'>Contact</Link></li>
-                        <div className="border-0 w-[80%] text-center oumdine-0 px-[1rem] py-[.5rem] rounded-[20px] cursor-pointer text-[.8rem] font-bold bg-[orange] hover:scale-111 duration-200 active:scale-95 lg:hidden!">
-                            Get Started
-                        </div>
-                    </ul>
-                </div>
+               
+                <ul  className="bg-red-500 absolute right-[2rem]  top-[60px]">
+                    <li  className={`${dropMenu ? " p-[.7rem] text-md" : "text-md"} cursor-pointer duration-300 hover:text-[orange] `} ><Link to = '/'>Home</Link></li>
+                    <li  className={`${dropMenu ? " p-[.7rem] text-md" : "text-md"} cursor-pointer duration-300 hover:text-[orange] `} ><Link to = '/'>About</Link></li>
+                    <li  className={`${dropMenu ? " p-[.7rem] text-md" : "text-md"} cursor-pointer duration-300 hover:text-[orange] `} ><Link to = '/'>Services</Link></li>
+                    <li  className={`${dropMenu ? " p-[.7rem] text-md" : "text-md"} cursor-pointer duration-300 hover:text-[orange] `} ><Link to = '/'>Contact</Link></li>
+                    <div className="border-0 w-[80%] text-center oumdine-0 px-[1rem] py-[.5rem] rounded-[20px] cursor-pointer text-[.8rem] font-bold bg-[orange] hover:scale-111 duration-200 active:scale-95 lg:hidden!">
+                        Get Started
+                    </div>
+                </ul>
+         
                 <div className="flex items-center justify-center  max-lg:hidden ">
                     <div className="border-0 outline-0 px-[1rem] py-[.5rem]
                         rounded-[20px] cursor-pointer text-[.8rem] font-bold bg-[orange]
