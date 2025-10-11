adc1668e98dbef8a4e2d2af9de712276dd2c87fa
amir <amirtaki623@gmail.com>
2025-10-11 17:21:20 +0330

    day 3 part 15

--- src/component/navbar/index.jsx
+++ src/component/navbar/index.jsx
@@ -1,9 +1,14 @@
 const Navbar = () => {
     return(
         <div className="flex justify-between items-center bg-blue-500">
-            <div className=" w-10 h-20 bg-red-500"></div>
+            <div className=" w-10 h-20 bg-red-500">
+                {/* logo */}
+                <div className="">
+                    <a href="">Web developer programming</a>
+                </div>
+            </div>
             <div className=" w-10 h-20 bg-green-500"></div>
-            <div className=" w-10 h-20 bg-orange-500-500"></div>
+            <div className=" w-10 h-20 bg-orange-500"></div>
         </div>
     )
 }
