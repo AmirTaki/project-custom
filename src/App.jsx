2b985fd6e9940cbb693892961e73e29b431b3ae0
amir <amirtaki623@gmail.com>
2025-10-11 17:39:57 +0330

    day 3 part 25

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,10 @@ function App() {
   return (
     <>
     {/*  */}
-    <div className={`h-[100vh] bg-[url(${background})]`}>
+    <div 
+      className={`h-[100vh]`}
+      style={{backgroundImage : `url(${background})`}}
+      >
 
       {/* NAVIGATION & DOCTS MENE 9  =>  bg-linear-[25deg,red_5%,yellow_60%,lime_90%,teal] ,bg-[#10131c]   */}
       {/* <Navigation /> */}
