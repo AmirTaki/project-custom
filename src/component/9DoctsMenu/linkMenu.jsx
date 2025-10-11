358fd5ee7e74209a61d4ea47cfa63cef39d00b82
amir <amirtaki623@gmail.com>
2025-10-11 14:42:00 +0330

    day 3 part 4

--- src/component/9DoctsMenu/linkMenu.jsx
+++ src/component/9DoctsMenu/linkMenu.jsx
@@ -5,18 +5,17 @@ const LinkMenu = ({item, toggle, index}) => {
         <Link to = "/">
             <span
                 style={{
-                    transform : `${toggle ? `translate(calc(60px * ${item.x}), calc(60px * ${item.y}))` : `translate(calc(15px * ${item.x}), calc(15px * ${item.y}))`}`,
+                    transform : `${toggle ? `translate(calc(60px * ${item.x}), calc(60px * ${item.y}))` : `translate(calc(12px * ${item.x}), calc(12px * ${item.y}))`}`,
                     transitionDelay : `calc(.1s * ${index})`
                         
                 }}
                 className={`
                     ${toggle ? "w-[45px] h-[45px] bg-[#333849]" : "w-[7px] h-[7px] bg-white"}
-                         flex  justify-center items-center   rounded-[50%] absolute
-
+                        flex  justify-center items-center   rounded-[50%] absolute
                     `}
             >
                 <i 
-                    className={`${item.icon} ${toggle ? "text-[1.35em] text-white" : "text-[0em]" } duration-500! `}
+                    className={`${item.icon} ${toggle ? "text-[1.35em] text-white" : "text-[0em] duration-500! " } `}
                 ></i>
             </span>
         </Link>
