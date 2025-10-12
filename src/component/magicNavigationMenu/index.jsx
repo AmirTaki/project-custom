06fe764f7d71e0c335bdc02ad1724f4e206cd88a
amir <amirtaki623@gmail.com>
2025-10-12 21:03:25 +0330

    day 4 part 25

--- src/component/magicNavigationMenu/index.jsx
+++ src/component/magicNavigationMenu/index.jsx
@@ -20,7 +20,11 @@ const MagicNavigationMenu = () => {
                                 <LinkMenu key = {index} item = {item} />                            )
                         })}
                         <div 
-                            className="absolute  -top-[50%] w-[70px] h-[70px] bg-[#29fd53] rounded-[50%] border-6 border-[#29fd53] duration-500"
+                            className="
+                            absolute  -top-[50%] w-[70px] h-[70px] bg-[#29fd53] rounded-[50%] border-6 border-[#29fd53] duration-500
+                            before:content before:absolute before:top-[50%] before:left[-22px] before:w-[20px] before:h-[20px] before:bg-red-500 before:rounded-tr-[20px] before:shadow-[0_-10px_0_0_#222327]
+                            
+                            "
                         ></div>
                     </ul>
                 </BrowserRouter>
