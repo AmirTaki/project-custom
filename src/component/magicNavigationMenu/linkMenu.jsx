c6990e58730409ef45cd5dcfe37a2e0c2d0906ef
amir <amirtaki623@gmail.com>
2025-10-12 20:15:19 +0330

    day 4 part 16

--- src/component/magicNavigationMenu/linkMenu.jsx
+++ src/component/magicNavigationMenu/linkMenu.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 Link
 const LinkMenu = ({item}) => {
     return(
-        <li>
+        <li className={`relative list-none w-[70px] h-[70px] z-10`}>
             <Link to = '/' >
                 <span className=""><i className={`${item.icon}`}></i></span>
                 <span className="">{item.name}</span>
