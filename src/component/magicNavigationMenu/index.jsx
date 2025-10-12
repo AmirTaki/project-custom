3d2a7a05f65e987c485b525d38ed02bba91f565f
amir <amirtaki623@gmail.com>
2025-10-12 19:51:28 +0330

    day 4 part 4

--- src/component/magicNavigationMenu/index.jsx
+++ src/component/magicNavigationMenu/index.jsx
@@ -1,10 +1,19 @@
+import { BrowserRouter } from "react-router-dom";
+
+
 const MagicNavigationMenu = () => {
     return(
         // navigation
         <div className="
-            flex justify-center items-center 
-            w-[400px] h-[70px] bg-white relative rounded-[10px]">
-            magic navigaton menu
+            flex justify-center items-center   w-[400px] h-[70px] bg-white relative rounded-[10px]"
+            >
+                <BrowserRouter>
+                    <ul>
+                        <li>
+                            
+                        </li>
+                    </ul>
+                </BrowserRouter>
         </div>
     )
 }
