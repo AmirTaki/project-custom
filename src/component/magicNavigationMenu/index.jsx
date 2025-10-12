7bb231ddccf863552fffeddb5f4063a9651ac13e
amir <amirtaki623@gmail.com>
2025-10-12 21:44:06 +0330

    day 4 part 39

--- src/component/magicNavigationMenu/index.jsx
+++ src/component/magicNavigationMenu/index.jsx
@@ -22,23 +22,23 @@ const MagicNavigationMenu = () => {
         <div className="
             flex justify-center items-center   w-[400px] h-[70px] bg-white relative rounded-[10px]"
             >
-                <contextNaviation.Provider>
-                <BrowserRouter>
-                    <ul className="flex w-[350px] ">
-                        {state.map((item) => {
-                            return(
-                                <LinkMenu key = {item.id} item = {item}  />                            
-                            )
-                        })}
-                        <div 
-                            className={`
-                                absolute  -top-[50%] w-[70px] h-[70px] bg-[#29fd53] rounded-[50%] border-6 border-[#29fd53] duration-500
-                                before:content-[''] before:absolute before:top-[50%] before:left[-22px] before:w-[20px] before:h-[20px] before:bg-blue-500 before:rounded-tr-[20px] before:shadow-[0_-10px_0_0_#222327]
-                                after:content-[''] after:absolute after:top-[50%] after:right-[-22px] after:w-[20px] after:h-[20px] after:bg-red-500 after:rounded-tl-[20px] after:shadow-[0_-10px_0_0_#222327]
-                            `}
-                        ></div>
-                    </ul>
-                </BrowserRouter>
+                <contextNaviation.Provider value = {{state, dispatch}}>
+                    <BrowserRouter>
+                        <ul className="flex w-[350px] ">
+                            {state.map((item) => {
+                                return(
+                                    <LinkMenu key = {item.id} item = {item}  />                            
+                                )
+                            })}
+                            <div 
+                                className={`
+                                    absolute  -top-[50%] w-[70px] h-[70px] bg-[#29fd53] rounded-[50%] border-6 border-[#29fd53] duration-500
+                                    before:content-[''] before:absolute before:top-[50%] before:left[-22px] before:w-[20px] before:h-[20px] before:bg-blue-500 before:rounded-tr-[20px] before:shadow-[0_-10px_0_0_#222327]
+                                    after:content-[''] after:absolute after:top-[50%] after:right-[-22px] after:w-[20px] after:h-[20px] after:bg-red-500 after:rounded-tl-[20px] after:shadow-[0_-10px_0_0_#222327]
+                                `}
+                            ></div>
+                        </ul>
+                    </BrowserRouter>
                 </contextNaviation.Provider>
         </div>
     )
