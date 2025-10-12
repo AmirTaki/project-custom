d1d3853c614e1c4e3727b1db807bbfb387cb5e13
amir <amirtaki623@gmail.com>
2025-10-12 21:05:51 +0330

    day 4 part 26

--- src/component/magicNavigationMenu/index.jsx
+++ src/component/magicNavigationMenu/index.jsx
@@ -22,7 +22,8 @@ const MagicNavigationMenu = () => {
                         <div 
                             className="
                             absolute  -top-[50%] w-[70px] h-[70px] bg-[#29fd53] rounded-[50%] border-6 border-[#29fd53] duration-500
-                            before:content before:absolute before:top-[50%] before:left[-22px] before:w-[20px] before:h-[20px] before:bg-red-500 before:rounded-tr-[20px] before:shadow-[0_-10px_0_0_#222327]
+                            before:content before:absolute before:top-[50%] before:left[-22px] before:w-[20px] before:h-[20px] before:bg-transparent before:rounded-tr-[20px] before:shadow-[0_-10px_0_0_#222327]
+                            after:content after:absolute after:top-[50%] after:right[-22px] after:w-[20px] after:h-[20px] after:bg-transparent after:rounded-tr-[20px] after:shadow-[0_-10px_0_0_#222327]
                             
                             "
                         ></div>
