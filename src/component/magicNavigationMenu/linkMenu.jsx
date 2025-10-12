9bb881bd4981c49c37dc3af0accf2983af5c9601
amir <amirtaki623@gmail.com>
2025-10-12 20:16:39 +0330

    day 4 part 17

--- src/component/magicNavigationMenu/linkMenu.jsx
+++ src/component/magicNavigationMenu/linkMenu.jsx
@@ -4,7 +4,9 @@ Link
 const LinkMenu = ({item}) => {
     return(
         <li className={`relative list-none w-[70px] h-[70px] z-10`}>
-            <Link to = '/' >
+            <Link to = '/' 
+                className={`relative flex justify-center items-center flex-col w-[100%] text-center font-[500]`}
+            >
                 <span className=""><i className={`${item.icon}`}></i></span>
                 <span className="">{item.name}</span>
             </Link>
