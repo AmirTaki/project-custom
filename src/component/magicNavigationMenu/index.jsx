b439554d71221dc4e6b3ceb03ef5fccc6450d0ab
amir <amirtaki623@gmail.com>
2025-10-12 20:14:39 +0330

    day 4 part 15

--- src/component/magicNavigationMenu/index.jsx
+++ src/component/magicNavigationMenu/index.jsx
@@ -14,12 +14,12 @@ const MagicNavigationMenu = () => {
             flex justify-center items-center   w-[400px] h-[70px] bg-white relative rounded-[10px]"
             >
                 <BrowserRouter>
-                    <ul>
+                    <ul className="flex w-[350px]">
                         {IconListNaviation.map((item, index) => {
                             return(
                                 <LinkMenu key = {index} item = {item} />                            )
                         })}
-                        
+                        <div className=""></div>
                     </ul>
                 </BrowserRouter>
         </div>
