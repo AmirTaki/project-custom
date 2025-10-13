e0a31c70c0716cb96d745b9141f447345f444c4a
amir <amirtaki623@gmail.com>
2025-10-13 20:40:48 +0330

    day 5 part 4

--- src/component/layerdHoverEffect/index.jsx
+++ src/component/layerdHoverEffect/index.jsx
@@ -3,15 +3,16 @@ import { BrowserRouter, Link } from "react-router-dom";
 
 const LayerdHoverEffect = () => {
     return(
-        <div className="">
+        <div className="text-white">
             <BrowserRouter>
                 <Link>
                     <div className="">
                         <span></span>
                         <span></span>
                         <span></span>
-                        <span></span>
+                        <span className="fab fa-facebook-f"></span>
                     </div>
+                    <div className="">Facebook</div>
                 </Link>
             </BrowserRouter>
         </div>
