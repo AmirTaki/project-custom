0edba0a3838ef9c8d7dd81b4a400e324bfc8119f
amir <amirtaki623@gmail.com>
2025-10-13 23:22:40 +0330

    day 5 part 57

--- src/component/switchButton/index.jsx
+++ src/component/switchButton/index.jsx
@@ -1,9 +1,13 @@
 const SwitchButton = () => {
+    
+    
+    const spans = Array.from({length : 40}, (_, i))
+    
     return(
         <div className="relative flex flex-col gap-30">
-            <div className="box-custom-magic"></div>
-            <div className="box-custom-magic"></div>
-            <div className="box-custom-magic"></div>
+            <div className="box-custom-magic">{spans}</div>
+            <div className="box-custom-magic">{spans}</div>
+            <div className="box-custom-magic">{spans}</div>
         </div>
     )
 }
