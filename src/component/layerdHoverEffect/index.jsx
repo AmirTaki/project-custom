4afa0810da2608e5f068e7a65f4b4857ce67813c
amir <amirtaki623@gmail.com>
2025-10-13 21:25:37 +0330

    day 5 part 24

--- src/component/layerdHoverEffect/index.jsx
+++ src/component/layerdHoverEffect/index.jsx
@@ -1,33 +1,14 @@
 import { BrowserRouter, Link } from "react-router-dom";
+import LinkLayerd from "./linkLayerd";
 
 
 const LayerdHoverEffect = () => {
     return(
         <div className="inline-flex">
             <BrowserRouter>
+                <LinkLayerd />
                 {/* a => link */}
-                <Link className="mx-[25px] my-[0] decoration-0 text-white block relative       group">
-                    <div className="w-[55px] h-[55px] transition-transform duration-300
-                        hover:transform hover:rotate-[-35deg] hover:skew-x-[20deg]
-                  
-                    ">
-                        {/* span */}
-                        <span className="custom-span-layerd"></span>
-                        <span className="custom-span-layerd"></span>
-                        <span className="custom-span-layerd"></span>
-                        {/* span icon */}
-                        <span 
-                            className="fab fa-facebook-f 
-                            custom-span-layerd text-[30px] text-center leading-[55px]!
-                        "></span>
-                    </div>
-                    {/* text */}
-                    <div className="
-                        absolute bottom-[-5%] left-[50%] translate-x-[-50%] 
-                        transition-[bottom_0.3s_ease,opacity_0.3s_ease] opacity-0 
-                        group-hover:opacity-100! group-hover:bottom-[-60%]!
-                    ">Facebook</div>
-                </Link>
+              
             </BrowserRouter>
         </div>
     )
