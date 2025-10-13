3b80e81235977f27400264dc013183b70dd588de
amir <amirtaki623@gmail.com>
2025-10-14 00:02:04 +0330

    day 5 part 78

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,12 @@
-// import background from "../public/background.avif"
 import './App.css'
-import DoctsMenu from './component/9DoctsMenu'
-import LayerdHoverEffect from './component/layerdHoverEffect'
-import MagicNavigationMenu from "./component/magicNavigationMenu"
-import Navbar from './component/navbar'
-import Navigation from './component/navigation'
-import SwitchButton from './component/switchButton'
-
+import Home from './component/home/Home'
 
 function App() {
   
   return (
     <>
-    {/*  */}
-    <div 
-      className={`h-[100vh] bg-[#10131c] flex justify-center items-center   `}
-      >
+    <Home />
 
-      {/* NAVIGATION & DOCTS MENE 9  =>  bg-linear-[25deg,red_5%,yellow_60%,lime_90%,teal] ,bg-[#10131c]   */}
-      {/* <Navigation /> */}
-      {/* <DoctsMenu /> */}
-
-      {/* NAVBAR  => bg-no-repeat bg-cover bg-center => style={{backgroundImage : `url(${background})`}} */}
-      <Navbar />
-
-      {/* magic navigation menu */}
-      {/* <MagicNavigationMenu /> */}
-      
-      {/* LayerdHoverEffect */}
-      {/* <LayerdHoverEffect /> */}
-
-      {/* button switch animation */}
-      <SwitchButton />
-
-    </div>
     </>
   )
 }
--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -1,6 +1,36 @@
+import DoctsMenu from "../9DoctsMenu"
+import LayerdHoverEffect from "../layerdHoverEffect"
+import MagicNavigationMenu from "../magicNavigationMenu"
+import Navbar from "../navbar"
+import Navigation from "../navigation"
+import SwitchButton from "../switchButton"
+
+
 const Home =  ()  => {
     return(
-        <div className=""></div>
+   
+        <div 
+            className={`h-[100vh] bg-[#10131c] flex justify-center items-center   `}
+        >
+
+            {/* NAVIGATION & DOCTS MENE 9  =>  bg-linear-[25deg,red_5%,yellow_60%,lime_90%,teal] ,bg-[#10131c]   */}
+            <Navigation />
+            <DoctsMenu />
+
+            {/* NAVBAR  => bg-no-repeat bg-cover bg-center => style={{backgroundImage : `url(${background})`}} */}
+            <Navbar />
+
+            {/* magic navigation menu */}
+            <MagicNavigationMenu />
+            
+            {/* LayerdHoverEffect */}
+            <LayerdHoverEffect />
+
+            {/* button switch animation */}
+            <SwitchButton />
+
+        </div>
+   
     )
 }
 
