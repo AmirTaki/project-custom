e9f576ce43effcebbb1a2dd9bd08e96560630a4e
amir <amirtaki623@gmail.com>
2025-10-13 23:35:44 +0330

    day 5 part 60

--- src/component/switchButton/index.jsx
+++ src/component/switchButton/index.jsx
@@ -1,19 +1,18 @@
 
 const SwitchButton = () => {
     const spans = Array.from({length : 40}, (_, i) => {
-        const randomDelay = Math.random() * .25
-    
+        return  Math.random() * .25
     })
-    
-    console.log(spans)
+
+
     return(
         <div className="relative flex flex-col gap-30">
             <div className="box-custom-magic"></div>
             <div className="box-custom-magic"></div>
             <div className="box-custom-magic">
-                {spans.map((i) => (
+                {spans.map((i, index) => (
                     <span 
-                        key = {i}
+                        key = {index}
                         style={{top : `${i * 1}px`, transitionDelay : `${3}s`}}
                     ></span>
                 ))}
