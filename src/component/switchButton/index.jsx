d76075bec840e354c922abf82ed51089b114afca
amir <amirtaki623@gmail.com>
2025-10-13 23:40:04 +0330

    day 5 part 64

--- src/component/switchButton/index.jsx
+++ src/component/switchButton/index.jsx
@@ -1,19 +1,27 @@
+import { useState } from "react"
 
 const SwitchButton = () => {
     const spans = Array.from({length : 40}, (_, i) => {
         return  Math.random() * .25
     })
 
+    const [active, setActive] = useState(false)
+    const handleBox = () => {
+
+    }
 
     return(
         <div className="relative flex flex-col gap-30">
             <div className="box-custom-magic"></div>
             <div className="box-custom-magic"></div>
-            <div className="box-custom-magic">
+            <div className="box-custom-magic"
+                onClick={handleBox}
+            >
                 {spans.map((i, index) => (
                     <span 
+                        className="box-span-custom "
                         key = {index}
-                        style={{top : `${i * 1}px`, transitionDelay : `${3}s`}}
+                        style={{top : `${index * 1}px`, transitionDelay : `${3}s`}}
                     ></span>
                 ))}
 
