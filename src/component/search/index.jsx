5714f190f9b286532271889b55f55986f0ef442b
amir <amirtaki623@gmail.com>
2025-10-14 21:46:02 +0330

    day 6 part 50

--- src/component/search/index.jsx
+++ src/component/search/index.jsx
@@ -1,7 +1,11 @@
 const SearchInput = () => {
     return (
-        <div className="">
-            <img src="https://purepng.com/public/uploads/medium/search-icon-sl7.png" alt="" />
+        <div className=
+            "flex center bg-[aliceblue] p-5 w-[300px] h-[50px] rounded-[50px]"
+        >
+            <img src="https://purepng.com/public/uploads/medium/search-icon-sl7.png"
+
+            alt="" />
             <input type="text" placeholder="Search ..." />
             <img src="https://tse4.mm.bing.net/th/id/OIP.FkcQWaj4fNpipuwvqAOvMwHaI6?pid=Api&P=0&h=22" alt="" />
         </div>
