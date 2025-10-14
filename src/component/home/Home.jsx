5171efa99c7bf439f34578349e35648bc2920576
amir <amirtaki623@gmail.com>
2025-10-14 17:00:16 +0330

    day 6 part 17

--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -1,7 +1,8 @@
+import ComponentNavigation from "../componentNavigation"
 import LayerdHoverEffect from "../layerdHoverEffect"
-import MagicNavigationMenu from "../magicNavigationMenu"
 import Navbar from "../navbar"
-import SwitchButton from "../switchButton"
+
+
 
 
 const Home =  ()  => {
@@ -10,14 +11,16 @@ const Home =  ()  => {
         <div 
             className={`h-[100vh] bg-[#10131c] flex justify-center items-center   `}
         >        
-            
-        
+
+            {/* component navigation */}
+            <ComponentNavigation />
+
             {/* NAVBAR  => bg-no-repeat bg-cover bg-center => style={{backgroundImage : `url(${background})`}} */}
             <Navbar />
 
 
             {/* LayerdHoverEffect */}
-            <LayerdHoverEffect />
+            {/* <LayerdHoverEffect /> */}
         </div>
    
     )
