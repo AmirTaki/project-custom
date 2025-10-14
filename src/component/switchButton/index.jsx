22bc5acb5955761ae945ccb3f5d135fd94995dbd
amir <amirtaki623@gmail.com>
2025-10-14 19:49:08 +0330

    day 6 part 41

--- src/component/switchButton/index.jsx
+++ src/component/switchButton/index.jsx
@@ -8,16 +8,17 @@ const SwitchButton = () => {
 
     return(
         <div 
-            className={`${menu ? " right-0 duration-300! shadow-[5px_5px_50px_#7ef0ff] " : "-right-50 duration-300!  "}  flex flex-row  items-center justify-center  fixed w-[200px] h-[300px]  border-2 border-[#7ef0ff]  rounded-4xl z-100!  `}
+            className={`${menu ? " right-0  shadow-[5px_5px_50px_#7ef0ff] " : "-right-50 duration-300!  "}  duration-400! flex flex-row  items-center justify-center  fixed w-[200px] h-[300px]  border-2 border-[#7ef0ff]  rounded-4xl z-100!  `}
         >  
-            <div className={`                
+            <div className={` 
+                ${menu ? " before:left-[1%] " : " before:left-[-9%] "}               
                 before:content-[''] before:absolute before:w-[30px] before:h-[40px] before:bg-transparent before:border-2 before:border-[#7ef0ff]
-                before:top-[45%]!  before:left-[-9%]  before:rounded-full before:cursor-pointer before:z-4`}
+                before:top-[45%]!  before:rounded-full before:cursor-pointer before:z-4 duration-400!`}
                 onClick={() => {setMenu((pre) => (!pre))}}
             >
                 <i className={`
-                    ${menu ? "rotate-180" : ""}
-                    bi bi-caret-left-fill text-[#7ef0ff] absolute -left-4 top-34 text-2xl z-5! cursor-pointer duration-200`}    
+                    ${menu ? "rotate-180 left-2" : " -left-4 "}
+                    bi bi-caret-left-fill text-[#7ef0ff] absolute  top-34 text-2xl z-5! cursor-pointer duration-400!`}    
                 ></i>   
             </div>
            
