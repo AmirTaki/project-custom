6bb08154a76603a63c09e5350a9ba008aaa799c4
amir <amirtaki623@gmail.com>
2025-10-14 22:17:47 +0330

    day 6 part 61

--- src/component/search/index.jsx
+++ src/component/search/index.jsx
@@ -6,21 +6,21 @@ const SearchInput = () => {
     return (
         <div 
             className ={`
-                ${active ? "w-[300px]" : "w-15"}
+                ${active ? "w-[300px]" : "w-12"}
                 flex items-center bg-[aliceblue] p-[5px]  h-[50px] rounded-[50px] m-[10px]  relative transition-[width] duration-500
                 shadow-[6px_6px_10px_rgba(0,0,0,.2),-6px_-6px_10px_rgba(255,255,255,.7)]
             `}
         >
-            <img 
-                className="w-[25px] cursor-pointer absolute left-[20px] "
-                src="https://purepng.com/public/uploads/medium/search-icon-sl7.png"
-                alt="" 
+   
+            <i 
                 onClick={() => {setActive((pre) => (!pre))}}
-            />
+                className="bi bi-search w-[25px] cursor-pointer absolute left-[13px] ">
+            </i>
+
             <input 
                 type="text" placeholder="Search ..." 
                 onChange={(e)=> {setSearch(e.target.value)}}
-                className={`bg-transparet border-0 w-[500px]  m-[10px]  outline-0 text-[rgb(100, 100, 100)] transition-[width] duration-[1000s] delay-[500s]`}
+                className={`${active ? "w-[500px]!" : "w-0"} bg-blue-500 border-0  !  outline-0 text-[rgb(100, 100, 100)]  duration-[1000s] `}
             />
            
             <i className={`${active ? 'text-2xl' : 'w-0'} bi bi-mic-fill absolute right-[10px] transition-[text] delay-[1000s]`}></i>
