4ec217ec96b5a4c677ecc6954307a925f32732f2
amir <amirtaki623@gmail.com>
2025-10-14 22:27:01 +0330

    day 6 part 62

--- src/component/search/index.jsx
+++ src/component/search/index.jsx
@@ -8,19 +8,19 @@ const SearchInput = () => {
             className ={`
                 ${active ? "w-[300px]" : "w-12"}
                 flex items-center bg-[aliceblue] p-[5px]  h-[50px] rounded-[50px] m-[10px]  relative transition-[width] duration-500
-                shadow-[6px_6px_10px_rgba(0,0,0,.2),-6px_-6px_10px_rgba(255,255,255,.7)]
+                shadow-[6px_6px_10px_rgba(0_0_0_.2),-6px_-6px_10px_rgba(255_255_255_.7)] overflow-hidden
             `}
         >
    
             <i 
                 onClick={() => {setActive((pre) => (!pre))}}
-                className="bi bi-search w-[25px] cursor-pointer absolute left-[13px] ">
+                className="bi bi-search text-xl  cursor-pointer absolute left-[13px] text-[blue] ">
             </i>
 
             <input 
-                type="text" placeholder="Search ..." 
+                type="text" placeholder={`Search ...`} 
                 onChange={(e)=> {setSearch(e.target.value)}}
-                className={`${active ? "w-[500px]!" : "w-0"} bg-blue-500 border-0  !  outline-0 text-[rgb(100, 100, 100)]  duration-[1000s] `}
+                className={`${active ? 'w-[100%]' : "w-0!"} border-0  mx-12   outline-0 text-[rgb(100_100_100)] transition-[width]  duration-[1000s] `}
             />
            
             <i className={`${active ? 'text-2xl' : 'w-0'} bi bi-mic-fill absolute right-[10px] transition-[text] delay-[1000s]`}></i>
