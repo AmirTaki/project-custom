82d336aa81ac7dfbbc31bf81c946d201dd3bf0bb
amir <amirtaki623@gmail.com>
2025-10-14 21:45:09 +0330

    day 6 part 49

--- src/component/search/index.jsx
+++ src/component/search/index.jsx
@@ -3,6 +3,7 @@ const SearchInput = () => {
         <div className="">
             <img src="https://purepng.com/public/uploads/medium/search-icon-sl7.png" alt="" />
             <input type="text" placeholder="Search ..." />
+            <img src="https://tse4.mm.bing.net/th/id/OIP.FkcQWaj4fNpipuwvqAOvMwHaI6?pid=Api&P=0&h=22" alt="" />
         </div>
     )
 }
