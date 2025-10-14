c3b220dd566d5894fe2ff7cb4242abf9933c5918
amir <amirtaki623@gmail.com>
2025-10-14 17:01:54 +0330

    day 6 part 18

--- src/component/componentNavigation/index.jsx
+++ src/component/componentNavigation/index.jsx
@@ -1,10 +1,34 @@
+import { useReducer } from "react";
 import DoctsMenu from "../9DoctsMenu";
 import MagicNavigationMenu from "../magicNavigationMenu";
 import Navigation from "../navigation";
 import SwitchButton from "../switchButton";
 
-
 const ComponentNavigation = () => {
+
+
+    const reducer = (state, action) => {
+        switch (action.type){
+            case "switchChange" : 
+                return state.map((item) => ({
+                    ...item,
+                    status : item.id === action.payload.box ? !item.status : item.status
+                }))    
+
+            case  "switchHandle" : 
+                return {...state, listSwitch : {...state.listSwitch,  [action.payload.box] : !state.listSwitch[action.payload.box]}}
+        }
+    }
+
+
+
+    // {id : 0, status : false},
+    // {id : 1, status : false},
+    // {id : 2, status : false},
+    const [stateSwich, dispatchSwitch] = useReducer(reducer, {
+        listSwitch : {}
+    });
+    
     return (
         <>
             {/* button switch animation */}
--- src/component/switchButton/boxSwich.jsx
+++ src/component/switchButton/boxSwich.jsx
@@ -1,34 +1,8 @@
 import Switch from "./switch"
-import { useEffect, useReducer, useState } from "react"
 
 const BoxSwitch = ({box}) => {
 
-    const reducer = (state, action) => {
-        switch (action.type){
-            case "switchChange" : 
-                return state.map((item) => ({
-                    ...item,
-                    status : item.id === action.payload.box ? !item.status : item.status
-                }))    
-    
-            case  "switchHandle" : 
-                return {...state, listSwitch : {...state.listSwitch,  [action.payload.box] : !state.listSwitch[action.payload.box]}}
-        }
-    }
-    
-
-
-
-    // {id : 0, status : false},
-    // {id : 1, status : false},
-    // {id : 2, status : false},
-    const [stateSwich, dispatchSwitch] = useReducer(reducer, {
-        listSwitch : {}
-    });
-
-    useEffect(() => {
-       console.log( stateSwich.listSwitch)   
-    })
+  
     return(
         <div className="box-custom-magic"
             // onClick={() => {dispatchSwitch ({type : "switchChange", payload : {box : box}})}}
