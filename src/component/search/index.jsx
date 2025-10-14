0356e3c66611d3631f4d9e8ab62f0ea1ab31d8c9
amir <amirtaki623@gmail.com>
2025-10-14 22:51:58 +0330

    day 6 part 68

--- src/component/search/index.jsx
+++ src/component/search/index.jsx
@@ -6,8 +6,8 @@ const SearchInput = () => {
     return (
         <div 
             className ={`
-                ${active ? "w-[300px]" : "w-12"}
-                flex items-center bg-[aliceblue] p-[5px]  h-[40px] rounded-[50px] m-[10px]  relative transition-all duration-900
+                ${active ? "w-[300px]" : "w-12 border-0!"}
+                flex items-center bg-transparent p-[5px] border-2 h-[40px] hover:border-blue-500 rounded-[50px] m-[10px]  relative transition-all duration-900
                 shadow-[6px_6px_10px_rgba(0_0_0_.2),-6px_-6px_10px_rgba(255_255_255_.7)] overflow-hidden
             `}
         >
@@ -20,10 +20,10 @@ const SearchInput = () => {
             <input 
                 type="text" placeholder={`Search ...`} 
                 onChange={(e)=> {setSearch(e.target.value)}}
-                className={`${active ? 'w-[100%]' : "w-0!"} border-0  mx-12   outline-0 text-[rgb(100_100_100)] transition-all  delay-500 duration-1000 `}
+                className={`${active ? 'w-[100%]' : "w-0!"} border-0  mx-12   outline-0 text-white transition-all  delay-500 duration-1000 `}
             />
            
-            <i className={`${active ? 'text-2xl' : 'text-[0px]'} bi bi-mic-fill absolute text-black hover:text-red-400! hover:delay-0 cursor-pointer not-hover:delay-0 right-[10px] delay-900 transition-all duration-200`}></i>
+            <i className={`${active ? 'text-2xl' : 'text-[0px]'} bi bi-mic-fill absolute text-blue-500 hover:text-red-400! hover:delay-0 cursor-pointer not-hover:delay-0 right-[10px] delay-900 transition-all duration-200`}></i>
          
          
         </div>
