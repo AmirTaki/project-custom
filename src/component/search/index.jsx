09790176ae2efa9d5090cd12d836bb5301e46b1c
amir <amirtaki623@gmail.com>
2025-10-14 21:54:13 +0330

    day 6 part 52

--- src/component/search/index.jsx
+++ src/component/search/index.jsx
@@ -1,7 +1,9 @@
 const SearchInput = () => {
     return (
         <div 
-            className= "flex center bg-[aliceblue] p-[5px] w-[300px] h-[50px] rounded-[50px] m-[10px] relative transition-[width] duration-150"
+            className ="flex center bg-[aliceblue] p-[5px] w-[300px] h-[50px] rounded-[50px] m-[10px] relative transition-[width] duration-150
+                shadow-[6px_6px_10px_rgba(0,0,0,.2),-6px_-6px_10px_rgba(255,255,255,.7)]
+            "
         >
             <img src="https://purepng.com/public/uploads/medium/search-icon-sl7.png"
 
