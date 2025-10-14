8e01a64a69b3e981841f939fac60d5231181e46d
amir <amirtaki623@gmail.com>
2025-10-14 19:42:14 +0330

    day 6 part 39

--- src/component/switchButton/index.jsx
+++ src/component/switchButton/index.jsx
@@ -11,11 +11,11 @@ const SwitchButton = () => {
             className={`${menu ? " right-0 duration-300! shadow-[5px_5px_50px_#7ef0ff] " : "-right-50 duration-300!  "}  flex flex-row  items-center justify-center  fixed w-[200px] h-[300px]  border-2 border-[#7ef0ff]  rounded-4xl z-100!  `}
         >  
             <div className={`                
-                before:content-[''] before:absolute before:w-[40px] before:h-[40px] before:bg-gray-600
-                before:top-[45%]!  before:left-[-10%]  before:rounded-full before:cursor-pointer before:z-4`}
+                before:content-[''] before:absolute before:w-[30px] before:h-[40px] before:bg-transparent before:border-2 before:border-[#7ef0ff]
+                before:top-[45%]!  before:left-[-16%]  before:rounded-full before:cursor-pointer before:z-4`}
                 onClick={() => {setMenu((pre) => (!pre))}}
             >
-                <i className="bi bi-caret-left text-[#7ef0ff] absolute -left-6 top-33.5 text-3xl z-5! cursor-pointer"></i>   
+                <i className={`bi bi-caret-left-fill text-[#7ef0ff] absolute -left-7 top-34 text-2xl z-5! cursor-pointer `}></i>   
             </div>
            
             <div className=" flex flex-col gap-7 relative
