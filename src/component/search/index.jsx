02596966ac5a518bcc63261a180cd1c077e19ba5
amir <amirtaki623@gmail.com>
2025-10-14 22:10:04 +0330

    day 6 part 59

--- src/component/search/index.jsx
+++ src/component/search/index.jsx
@@ -7,7 +7,7 @@ const SearchInput = () => {
         <div 
             className ={`
                 ${active ? "w-[300px]" : "w-15"}
-                flex items-center bg-[aliceblue] p-[5px]  h-[50px] rounded-[50px] m-[10px]  relative transition-[width] duration-150
+                flex items-center bg-[aliceblue] p-[5px]  h-[50px] rounded-[50px] m-[10px]  relative transition-[width] duration-500
                 shadow-[6px_6px_10px_rgba(0,0,0,.2),-6px_-6px_10px_rgba(255,255,255,.7)]
             `}
         >
@@ -22,9 +22,12 @@ const SearchInput = () => {
                 onChange={(e)=> {setSearch(e.target.value)}}
                 className="bg-transparet border-0 w-[500px]   outline-0 text-[rgb(100, 100, 100)] transition-[width] duration-[1000s] delay-[500s]"
             />
+            <div className="">
+                <i className="bi bi-mic-fill"></i>
+            </div>
             <img 
                 src="https://tse4.mm.bing.net/th/id/OIP.FkcQWaj4fNpipuwvqAOvMwHaI6?pid=Api&P=0&h=22" alt="" 
-                className="w-[30px] absolute right-[10px] transition-[width] delay-[1000s]"    
+                className={`${active ? "w-[100px]" : "w-"} w-0 absolute right-[10px] transition-[width] delay-[1000s]`}    
             />
         </div>
     )
