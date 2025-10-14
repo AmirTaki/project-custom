a67fa3ba61e2a93f84083d3d1528c542ae82b2fa
amir <amirtaki623@gmail.com>
2025-10-14 18:51:57 +0330

    day 6 part 25

--- src/component/switchButton/index.jsx
+++ src/component/switchButton/index.jsx
@@ -5,10 +5,12 @@ const SwitchButton = () => {
     const boxSwitch = [0, 1, 2];
 
     return(
-        <div className="flex flex-row fixed ">  
-            <div className="bg-blue-500 w-10 h-[300px]"></div>
+        <div className="flex flex-row fixed   border-2 border-amber-400 rounded-4xl overflow-auto right-0">  
+           
+            <div className="bg-blue-500 w-10 h-[260px]"></div>
+           
             <div className=" flex flex-col gap-7 relative
-                border-2 border-amber-400 h-[300px] w-[300px] items-center justify-center 
+             w-[200px] items-center justify-center 
             ">
                 {boxSwitch.map((box) => (
                     <BoxSwitch key = {box} box = {box} />
