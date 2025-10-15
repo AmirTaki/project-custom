78a01a26ea12185827f926887224e9ff8fb154c9
amir <amirtaki623@gmail.com>
2025-10-15 19:34:56 +0330

    day 7 part 32

--- src/component/downloadButton/index.jsx
+++ src/component/downloadButton/index.jsx
@@ -6,7 +6,11 @@ const DownloadButton = () => {
             cursor-pointer select-none overflow-hidden bg-[conic-gradient(#333_0%,_#333_100%)]
             before:content-['] before:absolute before:rounded-[50%] before:bg-[#222] opacity-90 before:inset-[15px]
          ">
-            <i className="fa-solid fa-arrow-down-long"></i>
+            {/* download */}
+            <i className="
+                fa-solid fa-arrow-down-long
+                absolute top-[50%]  translate-y-[-50%] text-[#03a9f5] text-[4em] duration-500
+            "></i>
             <h2></h2>
         </div>
     )
