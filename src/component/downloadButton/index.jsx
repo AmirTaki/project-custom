286ebd1ce395632e537569801ba31ea0687a2e1c
amir <amirtaki623@gmail.com>
2025-10-15 19:18:02 +0330

    day 7 part 29

--- src/component/downloadButton/index.jsx
+++ src/component/downloadButton/index.jsx
@@ -1,6 +1,9 @@
 const DownloadButton = () => {
     return (
-        <div className="">
+        // container
+        <div className="
+            relative w-[200px] h-[200px] bg-[conic-gradient(#333 0%, #333 100%)]
+        ">
             <i className="fa-solid fa-arrow-down-long"></i>
             <h2></h2>
         </div>
