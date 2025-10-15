5819cee5243650af4f54b86f2667f8e188f44155
amir <amirtaki623@gmail.com>
2025-10-15 19:24:08 +0330

    day 7 part 30

--- src/component/downloadButton/index.jsx
+++ src/component/downloadButton/index.jsx
@@ -2,8 +2,9 @@ const DownloadButton = () => {
     return (
         // container
         <div className="
-            relative w-[200px] h-[200px] bg-[conic-gradient(#333 0%, #333 100%)]
-        ">
+            relative w-[200px] h-[200px] rounded-[50%] flex justify-center items-center
+            cursor-pointer select-none overflow-hidden bg-[conic-gradient(#333_0%,_#333_100%)]
+         ">
             <i className="fa-solid fa-arrow-down-long"></i>
             <h2></h2>
         </div>
