095a9d3e00b6ab346f88c0c8cc94cdfec823b657
amir <amirtaki623@gmail.com>
2025-10-15 20:14:49 +0330

    day 7 part 36

--- src/component/downloadButton/index.jsx
+++ src/component/downloadButton/index.jsx
@@ -1,24 +1,28 @@
+import { useState } from "react"
+
 const DownloadButton = () => {
 
+    const [startDwonload, setStartDownload] = useState(false)
     const handleButton = () => {
-        
+        setStartDownload(!startDwonload)
     }
 
 
     return (
         // container
-        <div className="
+        <div className={`
             relative w-[200px] h-[200px] rounded-[50%] flex justify-center items-center
             cursor-pointer select-none overflow-hidden bg-[conic-gradient(#333_0%,_#333_100%)]
-            before:content-['] before:absolute before:rounded-[50%] before:bg-[#222] opacity-90 before:inset-[15px]
-         ">
+            before:content-[''] before:absolute before:rounded-[50%] before:bg-[#222] opacity-90 before:inset-[15px]
+        `}>
             {/* download */}
             <i 
                 onClick = {() => {handleButton}}
-                className="
-                fa-solid fa-arrow-down-long
-                absolute top-[50%]  translate-y-[-50%] text-[#03a9f5] text-[4em] duration-500
-            "></i>
+                className={`
+                    ${startDwonload ? 'translate-y-[-50%]' : "translate-y-[170%]"}
+                    fa-solid fa-arrow-down-long
+                    absolute top-[50%]  text-[#03a9f5] text-[4em] duration-500
+                `}></i>
             {/* number */}
             <h2 className="
                 relative z-100 text-[3em] text-center
