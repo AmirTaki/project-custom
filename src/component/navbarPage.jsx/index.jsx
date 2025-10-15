b7f7308adb2f18d0320b38b79ce67d6b2206d086
amir <amirtaki623@gmail.com>
2025-10-15 04:30:23 +0330

    day 7 part 14

--- src/component/navbarPage.jsx/index.jsx
+++ src/component/navbarPage.jsx/index.jsx
@@ -1,11 +1,16 @@
+import { createContext } from "react";
 import Navbar from "../navbar";
+import PageSearch from "../pageSearch";
 
+export const searchContext = createContext()
 
 const NavbarPaeg = () => {
     return(
        <>
             {/* NAVBAR  => bg-no-repeat bg-cover bg-center => style={{backgroundImage : `url(${background})`}} */}
             <Navbar />
+            <PageSearch />
+            
        </>
     )
 }
