4646104f941d483768970953dda4f37e1e02721e
amir <amirtaki623@gmail.com>
2025-10-15 20:28:07 +0330

    day 7 part 40

--- src/component/downloadButton/index.jsx
+++ src/component/downloadButton/index.jsx
@@ -11,18 +11,20 @@ const DownloadButton = () => {
         const timer =  setTimeout(() => {
             setNumber((number) => (number++))
 
-
+            contaienrRef.current.style.background  = `conic-gradient(#f5036c ${number}%, #333 0%)`
         }, [100])
     }
 
 
     return (
         // container
-        <div className={`
-            relative w-[200px] h-[200px] rounded-[50%] flex justify-center items-center
-            cursor-pointer select-none overflow-hidden bg-[conic-gradient(#333_0%,_#333_100%)]
-            before:content-[''] before:absolute before:rounded-[50%] before:bg-[#222] opacity-90 before:inset-[15px]
-        `}>
+        <div 
+            ref = {contaienrRef}
+            className={`
+                relative w-[200px] h-[200px] rounded-[50%] flex justify-center items-center
+                cursor-pointer select-none overflow-hidden bg-[conic-gradient(#333_0%,_#333_100%)]
+                before:content-[''] before:absolute before:rounded-[50%] before:bg-[#222] opacity-90 before:inset-[15px]
+            `}>
             {/* download */}
             <i 
                 onClick = {handleButton}
@@ -35,6 +37,7 @@ const DownloadButton = () => {
             <h2 className="
                 relative z-100 text-[3em] text-center
             ">
+                <span className={`${startDwonload ? 'flex' : 'hidden'}`}>{number}%</span>
             </h2>
         </div>
     )
