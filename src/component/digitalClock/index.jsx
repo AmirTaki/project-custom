e634f5149b75eff1ed3a7c4466c840954a1a7546
amir <amirtaki623@gmail.com>
2025-10-16 18:09:51 +0330

    day 8 part 6

--- src/component/digitalClock/index.jsx
+++ src/component/digitalClock/index.jsx
@@ -5,7 +5,12 @@ const DigitalClock = () => {
             className="flex gap-40 text-white "
         >
             {/* circle */}
-            <div className="relative w-[150px] h-[150px] bg-red-400 flex items-center justify-center"></div>
+            <div className="relative w-[150px] h-[150px] bg-red-400 flex items-center justify-center">
+                {/* dots */}
+                <div className="absolute w-[100%] h-[100%] z-10 flex justify-center items-center bg-amber-400"></div>
+            </div>
+
+
         </div>
     )
 }
