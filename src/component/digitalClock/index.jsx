02183b86b10edfd50c8451ec60fe310a52b4f577
amir <amirtaki623@gmail.com>
2025-10-16 18:41:15 +0330

    day 8 part 1

--- src/component/digitalClock/index.jsx
+++ src/component/digitalClock/index.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useRef } from "react"
 
 const DigitalClock = () => {
-    const hoursRef =  useRef ()
-    const hhRef = useRef()
+    // hours
+    const hours =  useRef ()
+    const hh = useRef()
+    const hr_dot = useRef()
     
     useEffect(() => {
 
@@ -15,16 +17,19 @@ const DigitalClock = () => {
             {/* circle */}
             <div className=" relative w-[150px] h-[150px] bg-red-400 flex items-center justify-center">
                 {/* dots */}
-                <div className="
-                    text-center font-[500] text-[1.5em]
-                    absolute w-[100%] h-[100%] z-10 flex justify-center items-center bg-amber-400
-                    before:content-[''] before:top-[-3px] before:absolute before:w-[15px] before:h-[15px] before:bg-[#ff2972]
-                    before:rounded-[50%] before:shadow-[0_0_20px_#ff2972,0_0_60px_#ff2972]
-                ">
+                <div 
+                    ref = {hr_dot}
+                    className="
+                        text-center font-[500] text-[1.5em]
+                        absolute w-[100%] h-[100%] z-10 flex justify-center items-center bg-amber-400
+                        before:content-[''] before:top-[-3px] before:absolute before:w-[15px] before:h-[15px] before:bg-[#ff2972]
+                        before:rounded-[50%] before:shadow-[0_0_20px_#ff2972,0_0_60px_#ff2972]
+                    "
+                >
                     {/* svg */}
                     <svg className="relative w-[150px] h-[150px] rotate-[270deg]">
                         <circle 
-                            ref = {hhRef}
+                            ref = {hh}
                             cx = '70' cy = "70" r = "70"
                             className="w-[100%] h-[100%] fill-transparent stroke-[#191919] stroke-4 translate-x-[5px] translate-y-[5px]"
                         ></circle>
@@ -36,7 +41,7 @@ const DigitalClock = () => {
                     </svg>
                     {/* hours */}
                     <div 
-                       ref = {hoursRef}
+                       ref = {hours}
                        className="absolute text-center font-[500] text-[1.5em]"
                     >00
                     </div>
