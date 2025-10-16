3ee7964361eed240c581760413e3ac2e9f475af5
amir <amirtaki623@gmail.com>
2025-10-16 20:22:03 +0330

    day 8 part 37

--- src/component/digitalClock/clcok.jsx
+++ src/component/digitalClock/clcok.jsx
@@ -11,7 +11,7 @@ const Clock = ({dots, svgCircle, circle, time, color, text   }) => {
                         text-center font-[500] text-[1.5em]
                         absolute w-[100%] h-[100%] z-10 flex justify-center items-center
                         before:content-[''] before:top-[-3px] before:absolute before:w-[15px] before:h-[15px] before:bg-[var(--before-color)]
-                        before:rounded-[50%] before:shadow-[0_0_20px_#ff2972,0_0_60px_#ff2972] 
+                        before:rounded-[50%] before:shadow-[0_0_20px_var(--before-color),0_0_60px_var(--before-color)] 
                     `}
                         
                 >
@@ -24,9 +24,10 @@ const Clock = ({dots, svgCircle, circle, time, color, text   }) => {
                     ></circle>
                     
                     <circle 
+                        style={{'--before-color': `#${color}` }}
                         ref = {svgCircle}
                         cx = '70' cy = "70" r = "70"
-                        className={`w-[100%] h-[100%] fill-transparent stroke-[#ff2972]  [stroke-dasharray:440]  stroke-4 translate-x-[5px] translate-y-[5px]`}  
+                        className={`w-[100%] h-[100%] fill-transparent stroke-[var(--before-color)]  [stroke-dasharray:440]  stroke-4 translate-x-[5px] translate-y-[5px]`}  
                     ></circle>
                 </svg>
                 {/* hours */}
