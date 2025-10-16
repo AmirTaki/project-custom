08ebc73e1f7eaa57900316c44bb0f96776ac8042
amir <amirtaki623@gmail.com>
2025-10-16 19:22:20 +0330

    day 8 part 24

--- src/component/digitalClock/index.jsx
+++ src/component/digitalClock/index.jsx
@@ -14,7 +14,7 @@ const DigitalClock = () => {
             setH((prevHour) => (prevHour = H))
      
             hh.current.style.strokeDashoffset = 440 - (440 * H) / 12
-            // hr_dot.current.style.transform = `rotate(${h * 30}deg)`
+            hr_dot.current.style.transform = `rotate(${h * 30}deg)`
     })
     return (
         // time
@@ -25,39 +25,38 @@ const DigitalClock = () => {
             <div className="     relative w-[150px] h-[150px]  flex items-center justify-center">
                 {/* dots */}
                 <div 
-                    // style={{transform : `rotate${h *30}deg`}}
+                    ref = {hr_dot}
                     className="
                         text-center font-[500] text-[1.5em]
                         absolute w-[100%] h-[100%] z-10 flex justify-center items-center
-                        before:content-[''] before:top-[-3px] before:absolute before:w-[15px] before:h-[15px] before:bg-[blue]
+                        before:content-[''] before:top-[-3px] before:absolute before:w-[15px] before:h-[15px] before:bg-[#ff2972]
                         before:rounded-[50%] before:shadow-[0_0_20px_#ff2972,0_0_60px_#ff2972] 
                     "
                 >
-                    {/* svg */}
-                    <svg className="relative w-[150px] h-[150px] rotate-[270deg]">
-                        <circle 
-                            cx = '70' cy = "70" r = "70"
-                            className="w-[100%] h-[100%] fill-transparent stroke-[#191919] stroke-4 translate-x-[5px] translate-y-[5px]"
-                        ></circle>
-                       
-                        <circle 
-                            ref = {hh}
-                            cx = '70' cy = "70" r = "70"
-                            className="w-[100%] h-[100%] fill-transparent stroke-[#ff2972]  [stroke-dasharray:440]  stroke-4 translate-x-[5px] translate-y-[5px]  "
-                        ></circle>
-                    </svg>
-                    {/* hours */}
-                    <div 
-                       ref = {hours}
-                       className="absolute text-center font-[500] text-[1.5em]"
-                    >
-                        {h} <br /> 
-                        <span className="absolute translate-x-[-50%] translate-y-[-3px] text-[12px] font-[300] uppercase tracking-[.1em]">
-                            Hours
-                        </span>
-                    </div>
                 </div>
-
+                {/* svg */}
+                <svg className="relative w-[150px] h-[150px] rotate-[270deg]">
+                    <circle 
+                        cx = '70' cy = "70" r = "70"
+                        className="w-[100%] h-[100%] fill-transparent stroke-[#191919] stroke-4 translate-x-[5px] translate-y-[5px]"
+                    ></circle>
+                    
+                    <circle 
+                        ref = {hh}
+                        cx = '70' cy = "70" r = "70"
+                        className="w-[100%] h-[100%] fill-transparent stroke-[#ff2972]  [stroke-dasharray:440]  stroke-4 translate-x-[5px] translate-y-[5px]  "
+                    ></circle>
+                </svg>
+                {/* hours */}
+                <div 
+                    ref = {hours}
+                    className="absolute text-center font-[500] text-[1.5em]"
+                >
+                    {h} <br /> 
+                    <span className="absolute translate-x-[-50%] translate-y-[-3px] text-[12px] font-[300] uppercase tracking-[.1em]">
+                        Hours
+                    </span>
+                </div>
             </div>
 
 
