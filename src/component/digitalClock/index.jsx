a445d8451e6b19fafd3f94f6d1f5a20c0adf64c9
amir <amirtaki623@gmail.com>
2025-10-16 19:19:19 +0330

    day 8 part 23

--- src/component/digitalClock/index.jsx
+++ src/component/digitalClock/index.jsx
@@ -12,8 +12,9 @@ const DigitalClock = () => {
             H > 12 ? H -=12 : H;
             H =  H < 10 ? "0" + H  : H;
             setH((prevHour) => (prevHour = H))
-            console.log(H)
+     
             hh.current.style.strokeDashoffset = 440 - (440 * H) / 12
+            // hr_dot.current.style.transform = `rotate(${h * 30}deg)`
     })
     return (
         // time
@@ -24,23 +25,23 @@ const DigitalClock = () => {
             <div className="     relative w-[150px] h-[150px]  flex items-center justify-center">
                 {/* dots */}
                 <div 
-                    ref = {hr_dot}
+                    // style={{transform : `rotate${h *30}deg`}}
                     className="
                         text-center font-[500] text-[1.5em]
                         absolute w-[100%] h-[100%] z-10 flex justify-center items-center
-                        before:content-[''] before:top-[-3px] before:absolute before:w-[15px] before:h-[15px] before:bg-[#ff2972]
-                        before:rounded-[50%] before:shadow-[0_0_20px_#ff2972,0_0_60px_#ff2972]
+                        before:content-[''] before:top-[-3px] before:absolute before:w-[15px] before:h-[15px] before:bg-[blue]
+                        before:rounded-[50%] before:shadow-[0_0_20px_#ff2972,0_0_60px_#ff2972] 
                     "
                 >
                     {/* svg */}
                     <svg className="relative w-[150px] h-[150px] rotate-[270deg]">
                         <circle 
-                            ref = {hh}
                             cx = '70' cy = "70" r = "70"
                             className="w-[100%] h-[100%] fill-transparent stroke-[#191919] stroke-4 translate-x-[5px] translate-y-[5px]"
                         ></circle>
                        
                         <circle 
+                            ref = {hh}
                             cx = '70' cy = "70" r = "70"
                             className="w-[100%] h-[100%] fill-transparent stroke-[#ff2972]  [stroke-dasharray:440]  stroke-4 translate-x-[5px] translate-y-[5px]  "
                         ></circle>
