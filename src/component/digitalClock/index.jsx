0527a8abbb7d171dc86df9cc055a4cfe1ba5cb6c
amir <amirtaki623@gmail.com>
2025-10-16 18:15:49 +0330

    day 8 part 8

--- src/component/digitalClock/index.jsx
+++ src/component/digitalClock/index.jsx
@@ -10,7 +10,7 @@ const DigitalClock = () => {
                 <div className="
                     absolute w-[100%] h-[100%] z-10 flex justify-center items-center bg-amber-400
                     before:content-[''] before:top-[-3px] before:absolute before:w-[15px] before:h-[15px] before:bg-[#ff2972]
-                    before:rounded-[50%] before:shadow-[0_0_20px_bg-[#ff2972],0_0_60px_bg-[#ff2972]]
+                    before:rounded-[50%] before:shadow-[0_0_20px_#ff2972,0_0_60px_#ff2972]
                 "></div>
             </div>
 
