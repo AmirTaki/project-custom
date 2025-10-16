951643db04f69b249343b798ea8e5a7e5fbb456a
amir <amirtaki623@gmail.com>
2025-10-16 18:14:41 +0330

    day 8 part 7

--- src/component/digitalClock/index.jsx
+++ src/component/digitalClock/index.jsx
@@ -7,7 +7,11 @@ const DigitalClock = () => {
             {/* circle */}
             <div className="relative w-[150px] h-[150px] bg-red-400 flex items-center justify-center">
                 {/* dots */}
-                <div className="absolute w-[100%] h-[100%] z-10 flex justify-center items-center bg-amber-400"></div>
+                <div className="
+                    absolute w-[100%] h-[100%] z-10 flex justify-center items-center bg-amber-400
+                    before:content-[''] before:top-[-3px] before:absolute before:w-[15px] before:h-[15px] before:bg-[#ff2972]
+                    before:rounded-[50%] before:shadow-[0_0_20px_bg-[#ff2972],0_0_60px_bg-[#ff2972]]
+                "></div>
             </div>
 
 
