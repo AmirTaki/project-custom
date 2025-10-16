2dc72cc80185088485dd88f685328f46683ca9f2
amir <amirtaki623@gmail.com>
2025-10-16 18:08:21 +0330

    day 8 part 5

--- src/component/digitalClock/index.jsx
+++ src/component/digitalClock/index.jsx
@@ -3,7 +3,10 @@ const DigitalClock = () => {
         // time
         <div 
             className="flex gap-40 text-white "
-        >digital clock</div>
+        >
+            {/* circle */}
+            <div className="relative w-[150px] h-[150px] bg-red-400 flex items-center justify-center"></div>
+        </div>
     )
 }
 
