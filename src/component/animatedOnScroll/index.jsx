5fc0bf312bfffe06c11032cec8a9b398d831cff6
amir <amirtaki623@gmail.com>
2025-10-17 23:41:51 +0330

    day 9 part 52

--- src/component/animatedOnScroll/index.jsx
+++ src/component/animatedOnScroll/index.jsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react";
+import { act, useReducer } from "react";
 import SectionScroll from "./sectionScroll";
 import Section  from "./section";
 import { DataSection } from "./dataSection";
@@ -7,6 +7,16 @@ const AnimatedOnScroll = () => {
     const reducer = (state, action) => {
         switch(action.type){
             case "scroll":
+                const {index} = action.payload
+                const {bool} = action.payload
+
+                state.map((item) => {
+                    if (item.id === index){
+                        return {...item, status : bool}
+                    }
+                    return item
+                })
+                return state
           
         }
     }
@@ -19,7 +29,7 @@ const AnimatedOnScroll = () => {
                     <div key = {section.id} className="overflow-x-hidden">
                         <SectionScroll index = {section.id}  dispatch= {dispatch} >
                             <Section 
-                                showAnimate = {showAnimate[section.id]}
+                                showAnimate = {section.status}
                                 h = {section.h1}     
                                 p = {section.p}
                                 showSection = {section.showSection}
