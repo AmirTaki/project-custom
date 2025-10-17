7efcf4e1db0b5f59d26ab1a54249431f2da3df1c
amir <amirtaki623@gmail.com>
2025-10-17 21:58:39 +0330

    day 9 part 25

--- src/component/animatedOnScroll/index.jsx
+++ src/component/animatedOnScroll/index.jsx
@@ -3,10 +3,38 @@ import SectionScroll from "./sectionScroll";
 
 const AnimatedOnScroll = () => {
 
-    const [showAnimate, setShowAnimate] = useState(false)
+    const [showAnimate, setShowAnimate] = useState(true)
     const sectionRef = useRef()
     return(
         <>
+
+            <SectionScroll sectionRef = {sectionRef} setShowAnimate= {setShowAnimate}>
+            {   
+            // section
+            <section
+                ref = {sectionRef}
+                // sec-1 show-animate
+                className={`
+                    ${showAnimate ? "translate-x-0" : "translate-x-[-100%]"}
+                    flex justify-center items-center flex-col min-h-[100vh] overflow-hidden
+                `}
+                >
+            {/* animate */}
+            <h1 className={`
+                ${showAnimate ? "opacity-100 blur-[0]" : "opacity-0 blur-[5px]"}  duration-1000
+                relative text-[90px] text-[#fff]
+            `}    
+            >
+                Hi, I'm Codehal
+            </h1>
+            
+            <p  className={`
+                    ${showAnimate ? "opacity-100 blur-[0]" : "opacity-0 blur-[5px]"}    duration-1000
+                relative text-[35px]  text-[#0ef] font-[600]`}
+            >Animation on Scroll using React js and tailwind and hock</p>
+            </section>
+            }
+            </SectionScroll>
 
             <SectionScroll >
             {   
--- src/component/animatedOnScroll/sectionScroll.jsx
+++ src/component/animatedOnScroll/sectionScroll.jsx
@@ -1,24 +1,25 @@
 import { useEffect, useRef, useState } from "react";
-const SectionScroll = (props) => {
+const SectionScroll = (props, {sectionRef, setShowAnimate}) => {
 
     // const [showAnimate, setShowAnimate] = useState(false)
     // const sectionRef = useRef()
-    // const animatedScroll = () => {
+    
+    const animatedScroll = () => {
 
-    //     const top = window.scrollY
-    //     const offset = sectionRef.current.scrollTop - 150
-    //     const height = sectionRef.current.offsetHeight;
+        const top = window.scrollY
+        const offset = sectionRef.current.scrollTop - 150
+        const height = sectionRef.current.offsetHeight;
 
-    //     setShowAnimate(top >= offset && top < offset + height ? true : false)
+        setShowAnimate(top >= offset && top < offset + height ? true : false)
 
-    // }
+    }
 
-    // useEffect(() => {
-    //     window.addEventListener('scroll', animatedScroll )
-    //     return() => {
-    //         window.removeEventListener('scroll', animatedScroll )
-    //     }
-    // }, [])
+    useEffect(() => {
+        window.addEventListener('scroll', animatedScroll )
+        return() => {
+            window.removeEventListener('scroll', animatedScroll )
+        }
+    }, [])
 
 
     return(
