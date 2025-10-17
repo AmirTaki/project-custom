e6436ce846b9666dac314c2bad6b7278eeb458ec
amir <amirtaki623@gmail.com>
2025-10-18 03:08:57 +0330

    day 9 part 68

--- src/component/animatedOnScroll/dataSection.jsx
+++ src/component/animatedOnScroll/dataSection.jsx
@@ -29,8 +29,18 @@ export const DataSection = [
         showStyles : " opacity-100 blur-[0] text-[#e0f]",
         hideStyles : ' opacity-0 blur-3xl',
         status : false
-
     },
+    {
+        id : 3,
+        h1 : "Very Good     <span className={`${section.status ? 'w-[100%] ' : 'w-0'} abolute top-0 right-0 h-[100%] bg-[#1f242d] duration-1000 delay-75`}></span>",
+        p : "Animation on Scroll using HTML CSS and Javascript",
+        showSection : 'scale-100 ' ,
+        hideSection : 'scale-[.2]',
+        showStyles : " opacity-100 blur-[0] text-[#e0f]",
+        hideStyles : ' opacity-0 blur-3xl',
+        status : false
+    },
+
 
 
 ]
