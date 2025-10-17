86a283429894543eea0f215c960f0b51216249a0
amir <amirtaki623@gmail.com>
2025-10-17 23:58:32 +0330

    day 9 part 57

--- src/component/animatedOnScroll/dataSection.jsx
+++ src/component/animatedOnScroll/dataSection.jsx
@@ -20,6 +20,17 @@ export const DataSection = [
         status : false
 
     },
+    {
+        id : 2,
+        h1 : "Nice",
+        p : "Animation on Scroll using HTML CSS and Javascript",
+        showSection : 'translate-x-0! ' ,
+        hideSection : 'translate-x-[100%]! ',
+        showStyles : "opacity-100 blur-[0] text-[#0f0]",
+        hideStyles : 'opacity-0 blur-[5px] ',
+        status : false
+
+    },
 
 
 ]
