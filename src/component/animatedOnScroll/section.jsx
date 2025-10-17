23a89b740b9a13066478e42ab19dcc84497ab1d7
amir <amirtaki623@gmail.com>
2025-10-18 03:22:54 +0330

    day 9 part 71

--- src/component/animatedOnScroll/section.jsx
+++ src/component/animatedOnScroll/section.jsx
@@ -11,15 +11,15 @@ const Section = ({section}) => {
         {/* animate */}
         <h1 className={`
             ${section.status ? section.showStyles : section.hideStyles}  
-            relative text-[90px] text-[#fff] delay-200 duration-1000
+            relative text-[90px] text-[#fff]  duration-800
         `}    
         >
-            {section.h1}  
+            {section.h1}
         </h1>
         {/* animate */}
         <p  className={`
             ${section.status ? section.showStyles : section.hideStyles}    
-            relative text-[35px]  text-[#0ef] font-[600] delay-200 duration-1000`}
+            relative text-[35px]  text-[#0ef] font-[600]  duration-800`}
         >{section.p}</p>
         </section>
             
