21dd17ce39a43ea77026afca755db0fcc2a628a5
amir <amirtaki623@gmail.com>
2025-10-17 22:16:38 +0330

    day 9 part 34

--- src/component/animatedOnScroll/index.jsx
+++ src/component/animatedOnScroll/index.jsx
@@ -39,6 +39,32 @@ const AnimatedOnScroll = () => {
             </section>
             }
             </SectionScroll>
+            <SectionScroll  dispatch= {dispatch} index = {1}>
+            {   
+            // section
+            <section
+                // sec-1 show-animate
+                className={`
+                    ${showAnimate[1] ? "translate-x-0" : "translate-x-[-100%]"}
+                    flex justify-center items-center flex-col min-h-[100vh] overflow-hidden
+                `}
+                >
+            {/* animate */}
+            <h1 className={`
+                ${showAnimate[1] ? "opacity-100 blur-[0]" : "opacity-0 blur-[5px]"}  duration-1000
+                relative text-[90px] text-[#fff]
+            `}    
+            >
+                Hi, I'm Codehal
+            </h1>
+            
+            <p  className={`
+                    ${showAnimate[1] ? "opacity-100 blur-[0]" : "opacity-0 blur-[5px]"}    duration-1000
+                relative text-[35px]  text-[#0ef] font-[600]`}
+            >Animation on Scroll using React js and tailwind and hock</p>
+            </section>
+            }
+            </SectionScroll>
          
 
          
