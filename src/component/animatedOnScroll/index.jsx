8b5bc760146a62db6527770001b0f304cadbb5af
amir <amirtaki623@gmail.com>
2025-10-17 21:28:35 +0330

    day 9 part 13

--- src/component/animatedOnScroll/index.jsx
+++ src/component/animatedOnScroll/index.jsx
@@ -23,10 +23,9 @@ const AnimatedOnScroll = () => {
                     
                 >Hi, I'm Codehal</h1>
                 
-                <p  className="
-                    opacity-0 blur-[5px] duration-1000
-                    relative text-[35px]  text-[#0ef] font-[600]
-                "
+                <p  className={`
+                     ${showAnimate ? "opacity-100 blur-[0]" : "opacity-0 blur-[5px]"}    duration-1000
+                    relative text-[35px]  text-[#0ef] font-[600]`}
                 >Animation on Scroll using React js and tailwind and hock</p>
             </section>
 
