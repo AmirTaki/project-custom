cd476e4f367d46ffac8c2c2fc24ef860754a1507
amir <amirtaki623@gmail.com>
2025-10-17 21:56:01 +0330

    day 9 part 24

--- src/component/animatedOnScroll/sectionScroll.jsx
+++ src/component/animatedOnScroll/sectionScroll.jsx
@@ -1,28 +1,28 @@
 import { useEffect, useRef, useState } from "react";
-const SectionScroll = () => {
+const SectionScroll = (props) => {
 
-    const [showAnimate, setShowAnimate] = useState(false)
-    const sectionRef = useRef()
-    const animatedScroll = () => {
+    // const [showAnimate, setShowAnimate] = useState(false)
+    // const sectionRef = useRef()
+    // const animatedScroll = () => {
 
-        const top = window.scrollY
-        const offset = sectionRef.current.scrollTop - 150
-        const height = sectionRef.current.offsetHeight;
+    //     const top = window.scrollY
+    //     const offset = sectionRef.current.scrollTop - 150
+    //     const height = sectionRef.current.offsetHeight;
 
-        setShowAnimate(top >= offset && top < offset + height ? true : false)
+    //     setShowAnimate(top >= offset && top < offset + height ? true : false)
 
-    }
+    // }
 
-    useEffect(() => {
-        window.addEventListener('scroll', animatedScroll )
-        return() => {
-            window.removeEventListener('scroll', animatedScroll )
-        }
-    }, [])
+    // useEffect(() => {
+    //     window.addEventListener('scroll', animatedScroll )
+    //     return() => {
+    //         window.removeEventListener('scroll', animatedScroll )
+    //     }
+    // }, [])
 
 
     return(
-        <div className=""></div>
+        <div className="">{props.children}</div>
     )
 }
 
