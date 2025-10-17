ff186b6687e293f41878e4250747ec311ec83955
amir <amirtaki623@gmail.com>
2025-10-17 21:47:19 +0330

    day 9 part 22

--- src/component/animatedOnScroll/index.jsx
+++ src/component/animatedOnScroll/index.jsx
@@ -1,26 +1,7 @@
-import { useEffect, useRef, useState } from "react";
+
 
 const AnimatedOnScroll = () => {
-    const [showAnimate, setShowAnimate] = useState(false)
-    const sectionRef = useRef()
-    const animatedScroll = () => {
-
-        const top = window.scrollY
-        const offset = sectionRef.current.scrollTop - 150
-        const height = sectionRef.current.offsetHeight;
-
-        setShowAnimate(top >= offset && top < offset + height ? true : false)
-
-    }
-
-    useEffect(() => {
-        window.addEventListener('scroll', animatedScroll )
-        return() => {
-            window.removeEventListener('scroll', animatedScroll )
-        }
-    }, [])
-
-    return(
+   
         <div className="">
             {/* section */}
             <section
--- src/component/animatedOnScroll/sectionScroll.jsx
+++ src/component/animatedOnScroll/sectionScroll.jsx
@@ -1,4 +1,26 @@
+import { useEffect, useRef, useState } from "react";
 const SectionScroll = () => {
+
+    const [showAnimate, setShowAnimate] = useState(false)
+    const sectionRef = useRef()
+    const animatedScroll = () => {
+
+        const top = window.scrollY
+        const offset = sectionRef.current.scrollTop - 150
+        const height = sectionRef.current.offsetHeight;
+
+        setShowAnimate(top >= offset && top < offset + height ? true : false)
+
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', animatedScroll )
+        return() => {
+            window.removeEventListener('scroll', animatedScroll )
+        }
+    }, [])
+
+
     return(
         <div className=""></div>
     )
