b90e846aefdd6b8268f160b6f9468875566d7d20
amir <amirtaki623@gmail.com>
2025-10-18 23:40:08 +0330

    day 10 part 59

--- src/component/animationEffect/index.jsx
+++ src/component/animationEffect/index.jsx
@@ -2,14 +2,7 @@ const AnimationEffect = () => {
     return(
         // container
         <div className="relative w-[100%] flex justify-center items-center">
-            {/* ring */}
-          <div className="
-            relative w-[150px] h-[150px] border-4 border-transparent border-t-4 border-t-[#24ecff] rounded-[50%] m-[-30px]
-            glowSkyBlueEffect
 
-            before:content-['\f135'] before:absolute before:font-[fontAwesome] before:text-[#24ecff] 
-            before:top-[10px] before:-right-3 before:rotate-90 before:text-[2em]
-          "></div>
         </div>
     )
 }
--- src/component/animationEffect/ring.jsx
+++ src/component/animationEffect/ring.jsx
@@ -1,6 +1,14 @@
 const Ring = () => {
     return(
-        <div className=""></div>
+        <div className="
+            relative w-[150px] h-[150px] border-4 border-transparent border-t-4 border-t-[#24ecff] rounded-[50%] m-[-30px]
+            glowSkyBlueEffect
+
+            before:content-['\f135'] before:absolute before:font-[fontAwesome] before:text-[#24ecff] 
+            before:top-[10px] before:-right-3 before:rotate-90 before:text-[2em]
+          ">
+
+        </div>
     )
 }
 
