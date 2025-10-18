95c9b73ab819f1be7524a6a29f16412b1548c11c
amir <amirtaki623@gmail.com>
2025-10-18 21:43:09 +0330

    day 10 part 22

--- src/component/card3D/index.jsx
+++ src/component/card3D/index.jsx
@@ -1,9 +1,9 @@
 const Card = () => {
     return(
         // wrapper
-        <div className="relative w-[620px] h-[330px] perspective-[1000px] bg-amber-200">
-            {/* flipcard */}
-            <div className="">
+        <div className="relative w-[620px] h-[330px] perspective-[1000px] ">
+            {/* flipcard => transform-3d */}
+            <div className="relative w-[100%] h-[100%] bg-[#D48600] transform-style-3d ">
                 
                 {/* front end  */}
                 <div className=""></div>
@@ -16,3 +16,15 @@ const Card = () => {
 }
 
 export default Card;
+
+
+
+// ### 5. کلاس‌های مکمل برای افکت 3D:
+
+// - `perspective-[1000px]` - عمق دید سه‌بعدی
+// - `transform-style-3d` - حفظ فضای سه‌بعدی
+// - `backface-hidden` - مخفی کردن پشت عناصر
+// - `rotate-y-180` - چرخش 180 درجه روی محور Y
+// - `transition-transform` - انیمیشن نرم
+
+// این ترکیب یک کارت سه‌بعدی کامل ایجاد می‌کند که می‌تواند چرخش داشته باشد.
