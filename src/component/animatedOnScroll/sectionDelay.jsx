1191702cf095411b4b65ac1b7af63663dea4d140
amir <amirtaki623@gmail.com>
2025-10-18 03:31:45 +0330

    day 9 part 73

--- src/component/animatedOnScroll/sectionDelay.jsx
+++ src/component/animatedOnScroll/sectionDelay.jsx
@@ -1,6 +1,29 @@
 const SectionDelay = () => {
     return (
-
+    // section
+        <section
+        // sec- show-animate
+        className={`
+            ${section.status ? section.showSection : section.hideSection}
+            flex justify-center items-center flex-col min-h-[100vh]   duration-1000 
+        `}
+        >
+            {/* animate */}
+            <h1 className={`
+                ${section.status ? section.showStyles : section.hideStyles}  
+                relative text-[90px] text-[#fff]  duration-800
+            `}    
+            >
+               Very Good <span className={``} ></span>
+            </h1>
+            {/* animate */}
+            <p  className={`
+                ${section.status ? section.showStyles : section.hideStyles}    
+                relative text-[35px]  text-[#0ef] font-[600]  duration-800`}
+            >
+                Animation on Scroll using HTML CSS and Javascript <span class  ></span>
+            </p>
+        </section>    
     )
 }
 
