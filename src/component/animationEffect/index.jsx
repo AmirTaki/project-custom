b69b82b3e21a007c4e1db746622bcaa6927c2cdd
amir <amirtaki623@gmail.com>
2025-10-18 23:30:30 +0330

    day 10 part 50

--- src/component/animationEffect/index.jsx
+++ src/component/animationEffect/index.jsx
@@ -5,8 +5,9 @@ const AnimationEffect = () => {
             {/* ring */}
           <div className="
             relative w-[150px] h-[150px] border-4 border-transparent border-t-4 border-t-[#24ecff] rounded-[50%] m-[-30px]
-            drop-shadow-[0_0_10px_#24ecff] [0_0_30px_#24ecff] [0_0_50px_#24ecff]
+            glowSkyEffect animate-spin 
 
+            before:content-['\f135'] before:absolute before:
           "></div>
         </div>
     )
