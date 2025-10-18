725ed18ffc9c94ba5923019ae20b860c2483e96e
amir <amirtaki623@gmail.com>
2025-10-18 21:32:42 +0330

    day 10 part 19

--- src/component/card3D/index.jsx
+++ src/component/card3D/index.jsx
@@ -1,6 +1,7 @@
 const Card = () => {
     return(
-        <div className=""></div>
+        // wrapper
+        <div className="relative w-[620px] h-[330px] perspective-[1000px] bg-amber-200"></div>
     )
 }
 
