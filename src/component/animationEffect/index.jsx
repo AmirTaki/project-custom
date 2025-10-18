3a11f719ced900d38eb74cbb784b4bdd152db856
amir <amirtaki623@gmail.com>
2025-10-18 23:40:43 +0330

    day 10 part 60

--- src/component/animationEffect/index.jsx
+++ src/component/animationEffect/index.jsx
@@ -1,8 +1,11 @@
+import Ring from "./ring";
+
 const AnimationEffect = () => {
     return(
         // container
         <div className="relative w-[100%] flex justify-center items-center">
-
+          {/* ring */}
+          <Ring />
         </div>
     )
 }
