c55856b86f29fd3a796bf62125883c7f21d0eeb8
amir <amirtaki623@gmail.com>
2025-10-19 00:00:25 +0330

    day 10 part 65

--- src/component/animationEffect/ring.jsx
+++ src/component/animationEffect/ring.jsx
@@ -4,10 +4,9 @@ const Ring = ({color, icon, animation, start}) => {
         <div 
         style={{'--Color' : color , '--Icon' : icon, '--Animation' : animation}}
         className={`
-           relative w-[150px] h-[150px] border-4 border-transparent border-t-4 border-t-[#var(--Color)] rounded-[50%] m-[-30px]
-           
-
-        before:absolute before:font-[fontAwesome] before:text-[#${color}] 
+            relative w-[150px] h-[150px] border-4 border-transparent border-t-4 border-t-[#var(--Color)] rounded-[50%] m-[-30px]
+           var(--Animation)
+            before:content-['var(--Icon)'] before:absolute before:font-[fontAwesome] before:text-[#var(--Color)] 
             before:top-[10px] before:-right-3 before:rotate-90 before:text-[2em]
         `}>
         </div>
