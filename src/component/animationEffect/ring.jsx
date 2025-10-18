4e2dc902ac0b52bd21d0033fa1eb32b23d1d1456
amir <amirtaki623@gmail.com>
2025-10-18 23:45:14 +0330

    day 10 part 62

--- src/component/animationEffect/ring.jsx
+++ src/component/animationEffect/ring.jsx
@@ -1,10 +1,10 @@
-const Ring = ({color}) => {
+const Ring = ({color, icon}) => {
     return(
         <div className={`
            relative w-[150px] h-[150px] border-4 border-transparent border-t-4 border-t-[${color}] rounded-[50%] m-[-30px]
             glowSkyBlueEffect
 
-            before:content-["\f135"] before:absolute before:font-[fontAwesome] before:text-[${color}] 
+            before:content-[${icon}] before:absolute before:font-[fontAwesome] before:text-[${color}] 
             before:top-[10px] before:-right-3 before:rotate-90 before:text-[2em]
         `}>
         </div>
