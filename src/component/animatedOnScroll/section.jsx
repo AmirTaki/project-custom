8387e921caabc5f7e6502f7c5596fbe556582271
amir <amirtaki623@gmail.com>
2025-10-18 18:16:13 +0330

    day 10 part 5

--- src/component/animatedOnScroll/section.jsx
+++ src/component/animatedOnScroll/section.jsx
@@ -15,14 +15,14 @@ const Section = ({section}) => {
                 relative text-[90px] text-[#fff]  duration-800
             `}    
             >
-                {section.h1}
+                {section.h1}<span className= {`${section.span&&section.span}`}></span>
             </h1>
             {/* animate */}
             <p  className={`
                 ${section.status ? section.showStyles : section.hideStyles}    
                 relative text-[35px]  text-[#0ef] font-[600]  duration-800`}
             >
-                {section.p}
+                {section.p}<span className= {`${section.span&section.span}`}></span>
             </p>
     </section>  
           
