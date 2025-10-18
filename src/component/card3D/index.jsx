fdf58d759d44ea7468fbcf489053417ac703b3c8
amir <amirtaki623@gmail.com>
2025-10-18 21:35:22 +0330

    day 10 part 21

--- src/component/card3D/index.jsx
+++ src/component/card3D/index.jsx
@@ -1,7 +1,17 @@
 const Card = () => {
     return(
         // wrapper
-        <div className="relative w-[620px] h-[330px] perspective-[1000px] bg-amber-200"></div>
+        <div className="relative w-[620px] h-[330px] perspective-[1000px] bg-amber-200">
+            {/* flipcard */}
+            <div className="">
+                
+                {/* front end  */}
+                <div className=""></div>
+
+                {/* back end */}
+                <div className=""></div>
+            </div>
+        </div>
     )
 }
 
