4e15d4f68e180f31fbb96caa10ec17d3c57d5a63
amir <amirtaki623@gmail.com>
2025-10-18 21:50:58 +0330

    day 10 part 25

--- src/component/card3D/index.jsx
+++ src/component/card3D/index.jsx
@@ -9,7 +9,12 @@ const Card = () => {
                 <div className="
                     flex justify-center items-center w-[100%] h-[100%] bg-[#D48600] border-t-2 border-t-[#cc6600] border-l-2 border-l-[#cc6600]
                    shadow-[0_5px_10px_rgba(0,0,0,0.2)] backface-hidden absolute
-                "></div>
+                ">
+                    {/* circle */}
+                    <span className="w-[100px] h-[110px] border-[10px] border-[#4d2600]"></span>
+
+
+                </div>
 
                 {/* back card */}
                 <div className=""></div>
