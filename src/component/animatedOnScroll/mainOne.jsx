116c14f10ac3676914846f9e52c0977e601a6208
amir <amirtaki623@gmail.com>
2025-10-18 18:12:33 +0330

    day 10 part 4

--- src/component/animatedOnScroll/mainOne.jsx
+++ src/component/animatedOnScroll/mainOne.jsx
@@ -1,6 +1,29 @@
 const MainOne = () => {
     return (
-        <div className=""></div>
+    // section
+        <section
+        // sec- show-animate
+        className={`
+            ${section.status ? 'translate-x-0 ': 'translate-x-[-100%]'}
+            flex justify-center items-center flex-col min-h-[100vh]   duration-1000 
+        `}
+        >
+            {/* animate */}
+            <h1 className={`
+                ${section.status ? 'opacity-100 blur-[0]' : 'opacity-0 blur-3xl'}  
+                relative text-[90px] text-[#fff]  duration-800
+            `}    
+            >
+                Hi, I'm Codehal
+            </h1>
+            {/* animate */}
+            <p  className={`
+                ${section.status ? 'opacity-100 blur-[0]' : 'opacity-0 blur-3xl'}    
+                relative text-[35px]  text-[#0ef] font-[600]  duration-800`}
+            >
+              Animation on Scroll using HTML CSS and Javascript
+            </p>
+    </section>  
     )
 }
 
