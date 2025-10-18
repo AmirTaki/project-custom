9a16668cd49782b66c3246db0a454921206c247c
amir <amirtaki623@gmail.com>
2025-10-18 23:20:40 +0330

    day 10 part 48

--- src/component/animationEffect/index.jsx
+++ src/component/animationEffect/index.jsx
@@ -1,9 +1,13 @@
 const AnimationEffect = () => {
     return(
         // container
-        <div className="rlative w-[100%] flex justify-center items-center">
+        <div className="relative w-[100%] flex justify-center items-center">
             {/* ring */}
-          <div className=""></div>
+          <div className="
+            relative w-[150px] h-[150px] border-4 border-transparent border-t-4 border-t-[#24ecff] rounded-[50%] m-[-30px]
+            drop-shadow-[0_0_10px_#24ecff] ,drop-shadow-[0_0_30px_#24ecff] ,drop-shadow-[0_0_50px_#24ecff]
+
+          "></div>
         </div>
     )
 }
