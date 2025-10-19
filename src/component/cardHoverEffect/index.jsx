b27ffb4435253f4971a894b6d14538f4d714f393
amir <amirtaki623@gmail.com>
2025-10-19 22:38:10 +0330

    day 11 part 53

--- src/component/cardHoverEffect/index.jsx
+++ src/component/cardHoverEffect/index.jsx
@@ -1,3 +1,4 @@
+import "./styles.css"
 import img1 from "./Img/landscape-1.png"
 import img2 from "./Img/landscape-2.png"
 import img3 from "./Img/landscape-3.png"
@@ -8,7 +9,7 @@ const CardHoverEffect = () => {
             {/* card__conatiner */}
             <div className="grid gap-y-[3.5rem] xl:grid-cols-3">
                 {/* card__article */}
-                <article className="relative overflow-hidden">
+                <article className="relative overflow-hidden group">
                     {/* card__image */}
                     <img src={img1} alt="" className="w-[328px] rounded-[1.5rem] xl:w-[348px]"/>
 
@@ -17,7 +18,10 @@ const CardHoverEffect = () => {
                     <div 
                     className="w-[280px] bg-[hsl(0,0%,100%)] px-[1.5rem] py-[2rem]
                       rounded-[1rem] absolute bottom-[-9rem] left-0 shadow-[0_8px_24px_hsla(0,0%,0%,.15)]
-                        right-0 mx-auto opacity-0 transition-opacity duration-1000 xl:w-[316px] xl:px-[2.5rem] sm:w-[250px] sm:p-[]
+                        right-0 mx-auto opacity-0 transition-opacity duration-1000 xl:w-[316px] xl:px-[2.5rem] sm:w-[250px] sm:p-[1rem]
+                        group-hover:animate-[show-data_1s_forwards]
+                        group-hover:opacity-1000
+                        group-hover:duration-300
                     ">
                         {/* card_description*/}
                         <span className="block text-[.813rem] mb-[.25rem]">Vancover Muntains, Canada</span>
