447114181b1e86b3837602333c3235ce6bf3d370
amir <amirtaki623@gmail.com>
2025-10-19 21:56:52 +0330

    day 11 part 38

--- src/component/cardHoverEffect/index.jsx
+++ src/component/cardHoverEffect/index.jsx
@@ -3,6 +3,12 @@ const CardHoverEffect = () => {
         // container
         <div className="grid place-items-center mx-[1.5rem] py-[5rem]">
             {/* card__conatiner */}
+            <div className="grid gap-y-[3.5rem]">
+                {/* card__article */}
+                <article className="relative overflow-hidden">
+
+                </article>
+            </div>
 
         </div>
     )
