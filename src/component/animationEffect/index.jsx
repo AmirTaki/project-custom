22926d6acd35185a17da82fa6f74b2934f372ce5
amir <amirtaki623@gmail.com>
2025-10-19 03:47:41 +0330

    day 11 part 6

--- src/component/animationEffect/index.jsx
+++ src/component/animationEffect/index.jsx
@@ -1,12 +1,17 @@
+import { Data } from "./data";
 import Ring from "./ring";
+
 
 const AnimationEffect = () => {
     return(
         // container
         <div className="relative w-[100%] flex justify-center items-center">
           {/* ring */}
-          <Ring color = {"24ecff"} icon = {"f135"} animation = {'glowSkyBlueEffect'} />
-          <Ring color = {"93ff2d"} icon = {"f072"} animation = {'glowSkyGreenEffect'} />
+          {Data.map((ring) => {
+            return(
+              <Ring key = {ring.id} />
+            )
+          } )}
         </div>
     )
 }
