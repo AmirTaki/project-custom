23727ec47b72764950c002054d551aaeaf7baa6e
amir <amirtaki623@gmail.com>
2025-10-19 23:21:24 +0330

    day 11 part 58

--- src/component/cardHoverEffect/dataCard.jsx
+++ src/component/cardHoverEffect/dataCard.jsx
@@ -4,6 +4,20 @@ const DataCard = [
         img : './Img/landscape-1.png',
         description : 'Vancouver Muntains, Canada',
         title : "The Great Path"
-
+    
+    },
+    {
+        id :1,
+        img : './Img/landscape-2.png',
+        description : 'Poon Hill, Nepal',
+        title : "Night"
+    
+    },
+    {
+        id :2,
+        img : './Img/landscape-3.png',
+        description : 'Bojcin Forest, Serbia',
+        title : "Path Of Peace"
+    
     },
 ]
