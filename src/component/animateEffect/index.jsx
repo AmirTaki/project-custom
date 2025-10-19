5a0ecde8b951fd5d975b7610a9bf15dd64415689
amir <amirtaki623@gmail.com>
2025-10-19 19:33:02 +0330

    day 11 part 22

--- src/component/animateEffect/index.jsx
+++ src/component/animateEffect/index.jsx
@@ -7,6 +7,7 @@ const AnimateEffect = () => {
         ">
             {/* box  */}
             <div 
+            style={{background : "linear-gradient(45deg, #00f376 10%, transparent 10%, transparent 50%, #00f376 50%, #00f376 60%, transparent 60%, transparent 100%),  linear-gradient(135deg, #00f37666 10%, transparent 10%, transparent 50%, #00f37666 50%, #00f37666 60%, transparent 60%, transparent 100%);"}}
             className={`
                 relative w-[200px] h-[200px] bg-size-[40px_40px] hue-rotate-[calc(1*90)deg]
                 rotate-[calc(1*90)deg]
