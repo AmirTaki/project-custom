5ce27da598a6192162031a299d0c25c457124155
amir <amirtaki623@gmail.com>
2025-10-19 23:18:41 +0330

    day 11 part 56

---
+++ src/component/cardHoverEffect/cardArticle.jsx
@@ -0,0 +1,35 @@
+import "./styles.css"
+const CardArticle = ({img}) =>  {
+    return(
+            
+        <article className="relative overflow-hidden group hover:animate-[remove-flow_2s_forwards]
+            not-hover:animate-[show-overflow_2s_forwards]"
+        >
+            {/* card__image */}
+            <img src={img} alt="" className="w-[328px] rounded-[1.5rem] xl:w-[348px]"/>
+
+            {/* card__data */}
+            <div 
+            className="
+                w-[280px] bg-[hsl(0,0%,100%)] px-[1.5rem] py-[2rem]
+                rounded-[1rem] absolute bottom-[-9rem] left-0 shadow-[0_8px_24px_hsla(0,0%,0%,.15)]
+                right-0 mx-auto opacity-0 transition-opacity duration-1000 xl:w-[316px] xl:px-[2.5rem] sm:w-[250px] sm:p-[1rem]
+                group-hover:opacity-1000
+                group-hover:duration-300
+                group-hover:animate-[show-data_1s_forwards]  
+                groupItem                  "
+            >
+            
+                {/* card_description*/}
+                <span className="block text-[.813rem] mb-[.25rem]">Vancover Muntains, Canada</span>
+                {/* card__title */}
+                <h2 className="text-[1.25rem] font-[500] text-[hsl(0,0%,15%)] mb-[.75rem]">The Great Path</h2>
+                {/* card__button */}
+                <a href="#" className="decoration-0 text-[.813rem] font-[500] text-[hsl(82,60%,28%)] hover:underline">Read More</a>
+
+            </div>
+        </article>
+    )
+}
+
+export default CardArticle;
--- src/component/cardHoverEffect/index.jsx
+++ src/component/cardHoverEffect/index.jsx
@@ -2,6 +2,8 @@ import "./styles.css"
 import img1 from "./Img/landscape-1.png"
 import img2 from "./Img/landscape-2.png"
 import img3 from "./Img/landscape-3.png"
+import CardArticle from "./cardArticle"
+
 const CardHoverEffect = () => {
     return(
         // container
@@ -9,35 +11,7 @@ const CardHoverEffect = () => {
             {/* card__conatiner */}
             <div className="grid gap-y-[3.5rem] xl:grid-cols-3 md:grid-cols-2 md:gap-x-[1.5rem] ">
                 {/* card__article */}
-                <article className="relative overflow-hidden group hover:animate-[remove-flow_2s_forwards]
-                    not-hover:animate-[show-overflow_2s_forwards]
-                "
-                >
-                    {/* card__image */}
-                    <img src={img1} alt="" className="w-[328px] rounded-[1.5rem] xl:w-[348px]"/>
-
-
-                    {/* card__data */}
-                    <div 
-                    className="
-                        w-[280px] bg-[hsl(0,0%,100%)] px-[1.5rem] py-[2rem]
-                        rounded-[1rem] absolute bottom-[-9rem] left-0 shadow-[0_8px_24px_hsla(0,0%,0%,.15)]
-                        right-0 mx-auto opacity-0 transition-opacity duration-1000 xl:w-[316px] xl:px-[2.5rem] sm:w-[250px] sm:p-[1rem]
-                        group-hover:opacity-1000
-                        group-hover:duration-300
-                        group-hover:animate-[show-data_1s_forwards]  
-                        groupItem                  "
-                    >
-                    
-                        {/* card_description*/}
-                        <span className="block text-[.813rem] mb-[.25rem]">Vancover Muntains, Canada</span>
-                        {/* card__title */}
-                        <h2 className="text-[1.25rem] font-[500] text-[hsl(0,0%,15%)] mb-[.75rem]">The Great Path</h2>
-                        {/* card__button */}
-                        <a href="#" className="decoration-0 text-[.813rem] font-[500] text-[hsl(82,60%,28%)] hover:underline">Read More</a>
-
-                    </div>
-                </article>
+                <CardArticle  img = {img1}/>
             </div>
 
         </div>
