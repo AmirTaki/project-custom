2f8dc94acbee5cf4f07e4c83b641477688e20109
amir <amirtaki623@gmail.com>
2025-10-19 22:24:46 +0330

    day 11 part 48

--- src/component/cardHoverEffect/index.jsx
+++ src/component/cardHoverEffect/index.jsx
@@ -4,7 +4,7 @@ import img3 from "./Img/landscape-3.png"
 const CardHoverEffect = () => {
     return(
         // container
-        <div className="grid place-items-center mx-[1.5rem] py-[5rem] xl:h-[100vh]">
+        <div className="grid place-items-center mx-[1.5rem] py-[5rem] xl:h-[100vh] md:h-[100vh] sm:mx-[1rem]">
             {/* card__conatiner */}
             <div className="grid gap-y-[3.5rem] xl:grid-cols-3">
                 {/* card__article */}
@@ -17,7 +17,7 @@ const CardHoverEffect = () => {
                     <div 
                     className="w-[280px] bg-[hsl(0,0%,100%)] px-[1.5rem] py-[2rem]
                       rounded-[1rem] absolute bottom-[-9rem] left-0 shadow-[0_8px_24px_hsla(0,0%,0%,.15)]
-                        right-0 mx-auto opacity-0 transition-opacity duration-1000 xl:w-[316px] xl:px-[2.5rem]
+                        right-0 mx-auto opacity-0 transition-opacity duration-1000 xl:w-[316px] xl:px-[2.5rem] sm:w-[250px] sm:p-[1rem]
                     ">
                         {/* card_description*/}
                         <span className="block text-[.813rem] mb-[.25rem]">Vancover Muntains, Canada</span>
