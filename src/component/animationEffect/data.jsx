0620d7a326b663e47688a6ed9302285d3425c548
amir <amirtaki623@gmail.com>
2025-10-19 03:44:42 +0330

    day 11 part 4

--- src/component/animationEffect/data.jsx
+++ src/component/animationEffect/data.jsx
@@ -8,5 +8,15 @@ export const Data = [
         top : '10px',
         bottom : '',
         left : '-4px'
-    }
+    },
+    {
+        id : 1,
+        content : '\\f072',
+        rotate : '45deg',
+        color : "93ff2d ",
+        top : 'initial',
+        bottom : '-6px',
+        left : '12px',
+        right : 'initial'
+    },
 ]
