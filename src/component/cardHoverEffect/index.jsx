94adc83a454110acc6fbdfe5860271b9524f6a3a
amir <amirtaki623@gmail.com>
2025-10-19 22:10:18 +0330

    day 11 part 43

--- src/component/cardHoverEffect/index.jsx
+++ src/component/cardHoverEffect/index.jsx
@@ -20,6 +20,8 @@ const CardHoverEffect = () => {
                     ">
                         {/* card_description*/}
                         <span className="block text-[.813rem] mb-[.25rem]">Vancover Muntains, Canada</span>
+                        {/* card__title */}
+                        <h2 className="text-[1.25rem] font-[500] text-[hsl(0,0%,15%)] mb-[.75rem]">The Great Path</h2>
 
                     </div>
                 </article>
