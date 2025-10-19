b97554ab448910a5436e44a1e91e60827c61937f
amir <amirtaki623@gmail.com>
2025-10-19 19:22:22 +0330

    day 11 part 18

--- src/component/animateEffect/index.jsx
+++ src/component/animateEffect/index.jsx
@@ -2,6 +2,7 @@ const AnimateEffect = () => {
     return(
         // container
         <div className="relative flex justify-center items-center w-[100%] gap-[40px] 
+        [-webkit-box-reflect:below_2px_linear-gradient(transparent,#0004)]
             
         ">
 
