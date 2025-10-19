a950da9d246b7a562aecaa786cd98d4ab0254985
amir <amirtaki623@gmail.com>
2025-10-19 21:54:03 +0330

    day 11 part 37

--- src/component/cardHoverEffect/index.jsx
+++ src/component/cardHoverEffect/index.jsx
@@ -1,7 +1,10 @@
 const CardHoverEffect = () => {
     return(
         // container
-        <div className="grid place-items-center mx-[1.5rem] py-[5rem]"></div>
+        <div className="grid place-items-center mx-[1.5rem] py-[5rem]">
+            {/* card__conatiner */}
+
+        </div>
     )
 }
 
