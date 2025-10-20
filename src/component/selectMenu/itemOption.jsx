ced3507db610cb0834259cab939fd86647a0434e
amir <amirtaki623@gmail.com>
2025-10-21 00:23:20 +0330

    day 12 part 72

--- src/component/selectMenu/itemOption.jsx
+++ src/component/selectMenu/itemOption.jsx
@@ -5,7 +5,7 @@ const ItemOption = ({option, dispatch, setOpen}) => {
             style={{'--text-color' : `${option.status ? 'blue'  : 'black'}`}}
             className="
             px-8 py-3 font-[500]  text-[14px] transition-all duration-[300ms] ease-in-out 
-            hover:text-[#00a8ff] text-[var(--text-color)]!
+            hover:text-[#00a8ff] !text-[var(--text-color)]
             "
             onClick={() => {dispatch({type : "selected", payload : {data : option.id}}) ; setOpen(false)}} 
         >
