de645ba25e29fa729b495b3efc303145d0df7f40
amir <amirtaki623@gmail.com>
2025-10-20 23:54:25 +0330

    day 12 part 68

--- src/component/selectMenu/itemOption.jsx
+++ src/component/selectMenu/itemOption.jsx
@@ -2,7 +2,7 @@ const ItemOption = ({option, dispatch, setOpen}) => {
     return(
           
         <div
-            onClick={() => {dispatch({type : "selected", payload : {data : option.id}}) , setOpen(false)}} 
+            onClick={() => {dispatch({type : "selected", payload : {data : option.id}}) ; setOpen(false)}} 
             style={{color : `${option.status ? '#00a8ff'  : 'black'}`}}
             className={` 
                 px-8 py-3 font-[500]  text-[14px] transition-all duration-[300ms] ease-in-out 
