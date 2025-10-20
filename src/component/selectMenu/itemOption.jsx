7401de5ec80747836f14d25da8d9819b3adfbda6
amir <amirtaki623@gmail.com>
2025-10-20 23:25:45 +0330

    day 12 part 62

--- src/component/selectMenu/itemOption.jsx
+++ src/component/selectMenu/itemOption.jsx
@@ -2,8 +2,10 @@ const ItemOption = ({option}) => {
     return(
           
         <div 
-            className="px-8 py-3 font-[500]  text-[14px] transition-all duration-[300ms] ease-in-out 
-            hover:text-[#00a8ff]"
+            className={` ${option.status ? "text-[#00a8ff]" : "text-black"}
+                px-8 py-3 font-[500]  text-[14px] transition-all duration-[300ms] ease-in-out 
+                hover:text-[#00a8ff]    
+            `}
         >
             {option.title}
         </div>
