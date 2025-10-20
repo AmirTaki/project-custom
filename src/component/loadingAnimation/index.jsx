91ce935f7db88255f5f36d8a695692572b032777
amir <amirtaki623@gmail.com>
2025-10-20 19:34:29 +0330

    day 12 part 6

--- src/component/loadingAnimation/index.jsx
+++ src/component/loadingAnimation/index.jsx
@@ -1,9 +1,13 @@
 const LoadingAnimation = () => {
     return (
         // center
-        <div className=" w-20 h-20  ">
+        <div className=" w-20 h-20 relative  ">
             {/* circle  */}
-            <div className="bg-blue-600 text-white">learning </div>
+            <div className="
+                absolute w-[33px] h-[33px] bg-[#262626]
+                rounded-[50%]
+                shadow-[0_35px_35px_rgba(0,0,0,0.25)]
+            "> </div>
         </div>
     )
 }
