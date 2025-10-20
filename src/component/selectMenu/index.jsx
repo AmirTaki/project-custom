4254ed608bbd389ed9d9ff5a3df9c09eebd9121f
amir <amirtaki623@gmail.com>
2025-10-20 22:42:35 +0330

    day 12 part 51

--- src/component/selectMenu/index.jsx
+++ src/component/selectMenu/index.jsx
@@ -1,3 +1,5 @@
+import './styles.css'
+
 const SelectMenu = () => {
     return(
         // select menu
@@ -9,9 +11,23 @@ const SelectMenu = () => {
             </div>
 
             {/* option list */}
-            <div className="absolute w-[100%] bg-[#f9f9f9] rounded-[6px] mt-[14px] px-0 py-[24px] max-h-[180px] transition-all duration-[400ms] ease-linear delay-[200ms] h-0">
+            <div className="absolute w-[100%] bg-[#f9f9f9] rounded-[6px] mt-[14px] px-0 py-[24px] max-h-[180px] transition-all duration-[400ms] ease-linear delay-[200ms]  h-20 overflow-scroll  scrollbarStyle">
                 {/* option */}
-                <div className="px-8 font-[500] text-[14px] transition-all duration-[300ms] ease-in-out ">English</div>
+                <div className="px-8 font-[500] text-[14px] transition-all duration-[300ms] ease-in-out 
+                    hover:text-[#00a8ff]
+                ">English</div>
+                {/* option */}
+                <div className="px-8 font-[500] text-[14px] transition-all duration-[300ms] ease-in-out 
+                    hover:text-[#00a8ff]
+                ">English</div>
+                {/* option */}
+                <div className="px-8 font-[500] text-[14px] transition-all duration-[300ms] ease-in-out 
+                    hover:text-[#00a8ff]
+                ">English</div>
+                {/* option */}
+                <div className="px-8 font-[500] text-[14px] transition-all duration-[300ms] ease-in-out 
+                    hover:text-[#00a8ff]
+                ">English</div>
             </div>
         </div>
     )
