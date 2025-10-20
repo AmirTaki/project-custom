58c4feed258c79888035f88624985c6a6bc4e105
amir <amirtaki623@gmail.com>
2025-10-20 21:19:22 +0330

    day 12 part 36

--- src/component/customRightClick/icon.jsx
+++ src/component/customRightClick/icon.jsx
@@ -1,8 +1,14 @@
 export const Icon = [
     {
         id : 0, 
-        icon : "",
-        title : '',
+        icon : "fa-solid fa-house",
+        title : 'Home',
+        delay : 0
+    },
+    {
+        id : 1, 
+        icon : "fa-solid fa-cart-shopping",
+        title : 'Card',
         delay : 0
     },
 ]
