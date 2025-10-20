111ac6843286c4cf7a63c4efa39fd11800462bf1
amir <amirtaki623@gmail.com>
2025-10-20 21:21:57 +0330

    day 12 part 38

--- src/component/customRightClick/index.jsx
+++ src/component/customRightClick/index.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useRef, useState } from "react";
 import './styles.css'
+import { Icon } from "./icon";
+import ListItem from "./listItem";
+
 
 const CustomRightClick = () => {
     const [mouse, setMouse] = useState({x : 0, y : 0})
@@ -38,18 +41,14 @@ const CustomRightClick = () => {
             className={`${show ? "fixed!" : "hidden"}  w-[200px] `}>
             {/* ul */}
             <ul className="p-0 m-0 list-none">
-                {/* li */}
-       
-
-                <li 
-                style={{animation : "showMenu .5s ease-in-out 1 forwards", }}
-                className={`p-[20px] bg-[#525F6C] border-1 border-[#5d6a77] text-white duration-500 
-                    hover:bg-[#72DE73] hover:translate-x-0 hover:translate-y-[-10px] hover:shadow-[0_10px_50px_#636363] 
-                    opacity-0     
-                    `}>
-                    <i className="fa-solid fa-house mr-[15px]"></i> contact
-                </li>
               
+                {/* li */}
+                {Icon.map((item) => {
+                    return(
+                        <ListItem key = {item.id} item = {item} />
+                    )
+                })}
+            
             </ul>
 
         </div>
