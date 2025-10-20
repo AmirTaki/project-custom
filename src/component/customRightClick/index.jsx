40645812070d07af8ccf7b92698d90d7bb8c00dd
amir <amirtaki623@gmail.com>
2025-10-20 20:16:52 +0330

    day 12 part 16

--- src/component/customRightClick/index.jsx
+++ src/component/customRightClick/index.jsx
@@ -1,16 +1,22 @@
 const CustomRightClick = () => {
     return(
-        // menu
+        <>
+        {/* menu */}
         <div className="fixed w-[200px] bg-red-500 h-[500px] top-[10%] left-[50%]">
             {/* ul */}
             <ul className="p-0 m-0 list-none">
                 <li className="p-[20px] bg-[#525F6C] border-1 border-[#5d6a77] text-white duration-500 
                     hover:bg-[#72DE73] hover:translate-x-0 hover:translate-y-[-10px] hover:shadow-[0_10px_50px_#636363]
+                    animate-[showMenu_.5s_ease-in-out_1_forwards] opacity-0
                 ">
                     <i className="fa-solid fa-house mr-[15px]"></i> Home
                 </li>
             </ul>
+
         </div>
+            <button className="text-white" onClick={}>click</button>
+        </>
+ 
     )
 }
 
