8c84103cacd3b2dd92162ed84312f10cf37f0ee8
amir <amirtaki623@gmail.com>
2025-10-20 21:14:05 +0330

    day 12 part 34

--- src/component/customRightClick/index.jsx
+++ src/component/customRightClick/index.jsx
@@ -39,18 +39,10 @@ const CustomRightClick = () => {
             {/* ul */}
             <ul className="p-0 m-0 list-none">
                 {/* li */}
-                <li 
-                style={{animation : "showMenu .5s ease-in-out 1 forwards"}}
-                className={`p-[20px] bg-[#525F6C] border-1 border-[#5d6a77] text-white duration-500 
-                    hover:bg-[#72DE73] hover:translate-x-0 hover:translate-y-[-10px] hover:shadow-[0_10px_50px_#636363]    
-                    opacity-0    
-                `}>
-                    <i className="fa-solid fa-house mr-[15px]"></i> Home
-                </li>
-              
+       
 
                 <li 
-                style={{animation : "showMenu .5s ease-in-out 1 forwards", animationDelay : ".2s"}}
+                style={{animation : "showMenu .5s ease-in-out 1 forwards", }}
                 className={`p-[20px] bg-[#525F6C] border-1 border-[#5d6a77] text-white duration-500 
                     hover:bg-[#72DE73] hover:translate-x-0 hover:translate-y-[-10px] hover:shadow-[0_10px_50px_#636363] 
                     opacity-0     
