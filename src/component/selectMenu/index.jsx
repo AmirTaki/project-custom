38eb89ee9501a8f847b4f446570a315ca89b16fb
amir <amirtaki623@gmail.com>
2025-10-20 22:13:45 +0330

    day 12 part 45

--- src/component/selectMenu/index.jsx
+++ src/component/selectMenu/index.jsx
@@ -1,6 +1,12 @@
 const SelectMenu = () => {
     return(
-        <div className=""></div>
+        // select menu
+        <div className="w-[300px] cursor-pointer relative">
+            {/* select */}
+            <div className="bg-[#201e1e] p-[20px] text-white font-[500] rounded-[6px] flex justify-center items-center">
+                
+            </div>
+        </div>
     )
 }
 
