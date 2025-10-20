8a21482db3a06ca984ff83764f5483b08da34e92
amir <amirtaki623@gmail.com>
2025-10-20 23:03:06 +0330

    day 12 part 59

--- src/component/selectMenu/index.jsx
+++ src/component/selectMenu/index.jsx
@@ -2,8 +2,6 @@ import { useReducer, useState } from 'react';
 import './styles.css'
 import { inputTitle } from './inputItem';
 
-
-
 const SelectMenu = () => {
     const [open, setOpen] =  useState(false)
     const reducer = (state, action) =>{
@@ -36,13 +34,7 @@ const SelectMenu = () => {
 
                     )
                 })}
-                {/* option */}
-                <div 
-                    className="px-8 py-4 font-[500]  text-[14px] transition-all duration-[300ms] ease-in-out 
-                    hover:text-[#00a8ff]"
-                >
-                    English
-                </div>
+              
                
                
                
---
+++ src/component/selectMenu/itemOption.jsx
@@ -0,0 +1,13 @@
+const ItemOption = () => {
+    return(
+          
+        <div 
+            className="px-8 py-4 font-[500]  text-[14px] transition-all duration-[300ms] ease-in-out 
+            hover:text-[#00a8ff]"
+        >
+            English
+        </div>
+    )
+}
+
+export default ItemOption;
