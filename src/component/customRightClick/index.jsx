5fbd22ba59aeb4635fe9826496c8507e50e03e75
amir <amirtaki623@gmail.com>
2025-10-20 20:00:56 +0330

    day 12 part 12

--- src/component/customRightClick/index.jsx
+++ src/component/customRightClick/index.jsx
@@ -1,7 +1,14 @@
 const CustomRightClick = () => {
     return(
         // menu
-        <div className="fixed w-[200px] bg-red-500 h-[500px] top-[10%] left-[50%]"></div>
+        <div className="fixed w-[200px] bg-red-500 h-[500px] top-[10%] left-[50%]">
+            {/* ul */}
+            <ul>
+                <li>
+                    <i className="fa-solid fa-house"></i> Home
+                </li>
+            </ul>
+        </div>
     )
 }
 
