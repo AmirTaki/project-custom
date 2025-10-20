ee9b479bdd590d11ef3b0a5fbeecc0c0f3e47b2d
amir <amirtaki623@gmail.com>
2025-10-20 21:11:33 +0330

    day 12 part 2

--- src/component/customRightClick/index.jsx
+++ src/component/customRightClick/index.jsx
@@ -42,16 +42,17 @@ const CustomRightClick = () => {
                 <li 
                 style={{animation : "showMenu .5s ease-in-out 1 forwards"}}
                 className={`p-[20px] bg-[#525F6C] border-1 border-[#5d6a77] text-white duration-500 
-                    hover:bg-[#72DE73] hover:translate-x-0 hover:translate-y-[-10px] hover:shadow-[0_10px_50px_#636363]        
+                    hover:bg-[#72DE73] hover:translate-x-0 hover:translate-y-[-10px] hover:shadow-[0_10px_50px_#636363]    
+                    opacity-0    
                 `}>
                     <i className="fa-solid fa-house mr-[15px]"></i> Home
                 </li>
               
                 <li 
-                style={{animation : "showMenu .5s ease-in-out 1 forwards"}}
+                style={{animation : "showMenu .5s ease-in-out 1 forwards", animationDelay : ".2s"}}
                 className={`p-[20px] bg-[#525F6C] border-1 border-[#5d6a77] text-white duration-500 
-                    hover:bg-[#72DE73] hover:translate-x-0 hover:translate-y-[-10px] hover:shadow-[0_10px_50px_#636363]
-                        delay-300!
+                    hover:bg-[#72DE73] hover:translate-x-0 hover:translate-y-[-10px] hover:shadow-[0_10px_50px_#636363] 
+                    opacity-0     
                     `}>
                     <i className="fa-solid fa-house mr-[15px]"></i> contact
                 </li>
