f4e2487d1873d0cabe97694235c2860c722af8d5
amir <amirtaki623@gmail.com>
2025-10-20 20:58:55 +0330

    day 12 part 28

--- src/component/customRightClick/index.jsx
+++ src/component/customRightClick/index.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import './styles.css'
-import { use } from "react";
+
 //animate-[showMenu_0.5s_ease-in-out_forwards] 
 
 
@@ -44,7 +44,7 @@ const CustomRightClick = () => {
                 <li className={`p-[20px] bg-[#525F6C] border-1 border-[#5d6a77] text-white duration-500 
                     hover:bg-[#72DE73] hover:translate-x-0 hover:translate-y-[-10px] hover:shadow-[0_10px_50px_#636363]
 
-                     ${change ? 'opacity-100  translate-x-0 translate-y-0 ease-in-out'  : "opacity-0 translate-x-0 translate-y-[20px]"}  `}>
+                     ${show ? 'opacity-100  translate-x-0 translate-y-0 ease-in-out'  : "opacity-0 translate-x-0 translate-y-[20px]"}  `}>
                     <i className="fa-solid fa-house mr-[15px]"></i> Home
                 </li>
             </ul>
