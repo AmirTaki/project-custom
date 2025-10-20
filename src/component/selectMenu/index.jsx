24af0007793501feb5d9c7c851c97fdddecfc879
amir <amirtaki623@gmail.com>
2025-10-20 22:26:43 +0330

    day 12 part 48

--- src/component/selectMenu/index.jsx
+++ src/component/selectMenu/index.jsx
@@ -3,10 +3,9 @@ const SelectMenu = () => {
         // select menu
         <div className="w-[300px] cursor-pointer relative">
             {/* select */}
-            <div className="bg-[#201e1e] p-[20px] text-white font-[500] rounded-[6px] flex justify-center items-center">
-                <span>
-                    Select Languge 
-                </span>
+            <div className="bg-[#201e1e] p-[20px] text-white font-[500] rounded-[6px] flex justify-between items-center">
+                <span > Select Languge </span>
+                <i className="fas fa-angle-down"></i>
             </div>
         </div>
     )
