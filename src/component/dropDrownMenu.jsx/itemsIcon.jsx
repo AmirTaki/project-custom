91fdb13a0b1f17c7b519e4244400848f97524378
amir <amirtaki623@gmail.com>
2025-10-22 22:29:49 +0330

    day 14 part 55

--- src/component/dropDrownMenu.jsx/itemsIcon.jsx
+++ src/component/dropDrownMenu.jsx/itemsIcon.jsx
@@ -1,12 +1,12 @@
-import { Link, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
-const ItemsIcon = () => {
+const ItemsIcon = ({item}) => {
     return(
             <li>
                 <Link to = "/" className="flex items-center my-[20px] mx-0 text-[1em] gap-[10px] decoration-0 text-[#555] hover:text-[#4e65ff]">
-                    <i className="bi bi-person text-[1.5em] "></i>
-                    My Profile
+                    <i className={`${item.icon} text-[1.5em]`}></i>
+                    {item.title}
                 </Link>
             </li>
     )
