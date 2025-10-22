fceda520c41185e7e223b6cff5e2fad6feead4b9
amir <amirtaki623@gmail.com>
2025-10-22 21:20:15 +0330

    day 14 part 29

--- src/component/dropDrownMenu.jsx/index.jsx
+++ src/component/dropDrownMenu.jsx/index.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
+
 const DropDrownMenu = () => {
-    
+    const [active, setActive] = useState(false)
     return(
         // navigation
         <div
-            className="w-[120px] h-[60px] bg-white shadow-[10px_25px_35px_rgba(0,0,0,.1)] flex justify-between 
-            transition-[width .5s_height .5s] "
+            className={` ${active ? "w-[300px] h-[400px]" : "w-[120px] h-[60px]"}
+                bg-white shadow-[10px_25px_35px_rgba(0,0,0,.1)] flex justify-between 
+                transition-[width .5s_height .5s]   
+            `}
             >
 
 
