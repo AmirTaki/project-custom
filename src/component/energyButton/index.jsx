2f0599fa05740680ea376ffd6370e5be15ab7567
amir <amirtaki623@gmail.com>
2025-10-22 23:33:53 +0330

    day 14 part 60

--- src/component/energyButton/index.jsx
+++ src/component/energyButton/index.jsx
@@ -1,6 +1,12 @@
 const EnergyButton = () => {
     return(
-        <div className=""></div>
+        <div className="
+            absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 scale-100
+            
+        "
+        >
+            Energy
+        </div>
     )
 }
 export default EnergyButton;
