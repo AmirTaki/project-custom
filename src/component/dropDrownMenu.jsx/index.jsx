aa7eb4bec9ca88ab459d106833f650cf81f2c2ec
amir <amirtaki623@gmail.com>
2025-10-22 21:28:45 +0330

    day 14 part 32

--- src/component/dropDrownMenu.jsx/index.jsx
+++ src/component/dropDrownMenu.jsx/index.jsx
@@ -6,12 +6,13 @@ const DropDrownMenu = () => {
         <>
         {/* navigation */}
         <div
-            className={` ${active ? "w-[300px] h-[400px] transition-[height,width] duration-500  delay-[0ms,750ms]   " : "w-[120px] h-[60px] transition-[width,height] duration-500  delay-[0ms,750ms]  "}
-                bg-white shadow-[10px_25px_35px_rgba(0,0,0,.1)] flex justify-between 
-                
+            className={` ${active ? "w-[300px] h-[400px] transition-[height,width]  delay-[0ms,750ms] " : "w-[120px] h-[60px] transition-[width,height]  delay-[0ms,750ms]  "}
+                bg-white shadow-[10px_25px_35px_rgba(0,0,0,.1)] flex justify-between duration-500   
             `}
             >
-
+            {/* userBx */}
+            <div className="relative w-[60px] h-[60px] bg-white flex items-center duration-500 delay-500"
+            ></div>
 
 
         </div>
