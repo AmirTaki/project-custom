69a2567d96190084ded8d2fa700fb766f9bbd0ad
amir <amirtaki623@gmail.com>
2025-10-22 21:26:57 +0330

    day 14 part 31

--- src/component/dropDrownMenu.jsx/index.jsx
+++ src/component/dropDrownMenu.jsx/index.jsx
@@ -3,16 +3,20 @@ import { useState } from "react";
 const DropDrownMenu = () => {
     const [active, setActive] = useState(false)
     return(
-        // navigation
+        <>
+        {/* navigation */}
         <div
-            className={` ${active ? "w-[300px] h-[400px]" : "w-[120px] h-[60px]"}
+            className={` ${active ? "w-[300px] h-[400px] transition-[height,width] duration-500  delay-[0ms,750ms]   " : "w-[120px] h-[60px] transition-[width,height] duration-500  delay-[0ms,750ms]  "}
                 bg-white shadow-[10px_25px_35px_rgba(0,0,0,.1)] flex justify-between 
-                transition-[width,height] duration-500 delay-[0ms,750ms]   
+                
             `}
             >
 
 
+
         </div>
+        <div className="text-red-500" onClick={() => {setActive(!active)}}>click me</div>
+        </>
     )
 }
 
