c5e0498609c603c1c5b1bfe42ee236ea39fcb8dd
amir <amirtaki623@gmail.com>
2025-10-22 22:26:32 +0330

    day 14 part 51

---
+++ src/component/dropDrownMenu.jsx/listIcon.jsx
@@ -0,0 +1,23 @@
+const ListIcon = [
+    {
+        id : 0,
+        icon : 'bi bi-person',
+        title : 'My Profile'
+    },
+    {
+        id : 1,
+        icon : 'bi bi-chat-right-text',
+        title : 'Messages'
+    },
+    {
+        id : 2,
+        icon : 'bi bi-bell',
+        title : 'Notification'
+    },
+    {
+        id : 3,
+        icon : 'bi bi-gear',
+        title : 'Settings'
+    },
+
+]
