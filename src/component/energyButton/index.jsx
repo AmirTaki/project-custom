b081b58e80f9a2decbe9b89a5129ff4b3e82fa5b
amir <amirtaki623@gmail.com>
2025-10-22 23:36:11 +0330

    day 14 part 61

--- src/component/energyButton/index.jsx
+++ src/component/energyButton/index.jsx
@@ -2,7 +2,8 @@ const EnergyButton = () => {
     return(
         <div className="
             absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 scale-100
-            
+            w-[200px] h-[60px] text-center leading-[60px] font-sans uppercase decoration-0
+            tracking-[6px] text-[24px] text-[#309220] bg-[rgba(80,241,0,1)]
         "
         >
             Energy
