b1bef6d8b1ae290d7d5e1436ff8e974a06f9e502
amir <amirtaki623@gmail.com>
2025-10-22 23:54:22 +0330

    day 14 part 67

--- src/component/energyButton/index.jsx
+++ src/component/energyButton/index.jsx
@@ -1,20 +1,7 @@
 import "./styles.css"
 const EnergyButton = () => {
     return(
-        <div className="
-            absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
-            w-[200px] h-[60px] 
-            text-center leading-[60px] 
-            font-sans uppercase decoration-0
-            tracking-[6px] text-[24px]
-            text-[#309220] bg-[rgba(80,241,0,1)]
-            shadow-[0_0_15px_rgba(90,241,0,1),0_0_25px_rgba(80,241,0,1)]
-            transition-transform duration-500
-            transform-gpu
-            hover:scale-90   
-            hover:animate-pulse-custom
-        "
-        >
+        <div className=" energy">
             Energy
         </div>
     )
