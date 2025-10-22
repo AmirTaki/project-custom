9a77de4cf8dc6a057dea41e884e6deacc2f8b2fa
amir <amirtaki623@gmail.com>
2025-10-22 23:50:28 +0330

    day 14 part 66

--- src/component/energyButton/index.jsx
+++ src/component/energyButton/index.jsx
@@ -6,9 +6,13 @@ const EnergyButton = () => {
             w-[200px] h-[60px] 
             text-center leading-[60px] 
             font-sans uppercase decoration-0
-            tracking-[6px] text-[24px] text-[#309220] bg-[rgba(80,241,0,1)]
-            rounded-[60px] duration-500 
-            energy  
+            tracking-[6px] text-[24px]
+            text-[#309220] bg-[rgba(80,241,0,1)]
+            shadow-[0_0_15px_rgba(90,241,0,1),0_0_25px_rgba(80,241,0,1)]
+            transition-transform duration-500
+            transform-gpu
+            hover:scale-90   
+            hover:animate-pulse-custom
         "
         >
             Energy
