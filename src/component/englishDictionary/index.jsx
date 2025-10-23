c9ec459610674128326107bdc2dcf90c81ec199c
amir <amirtaki623@gmail.com>
2025-10-23 19:16:12 +0330

    day 15 part 4

--- src/component/englishDictionary/index.jsx
+++ src/component/englishDictionary/index.jsx
@@ -2,10 +2,12 @@ const EnglishDictionary = () => {
     return(
         // container
         <div className="bg-[rgba(255,255,255,.3)] p-[28px] rounded-[7px] shadow-[0_10px_10px_rgba(0,0,0,.3)]
-            w-[90%] m-[100px] max-w-[450px] text-center text-[18px] font-[500]
-        "
-        
-        ></div>
+            w-[90%] m-[100px] max-w-[450px] text-center text-[18px] font-[500]  "
+        >
+            {/* heading*/}
+            <h1 className="text-[28px]">English Dictionary</h1>
+
+        </div>
     )
 }
 
