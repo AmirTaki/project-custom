255446916f358ba42800b16d4e22c50e8f2a95ea
amir <amirtaki623@gmail.com>
2025-10-24 19:06:10 +0330

    day 16 part 18

--- src/component/footerAnimated/columnFotter.jsx
+++ src/component/footerAnimated/columnFotter.jsx
@@ -1,6 +1,21 @@
-const ColumnFooter = () => {
+const ColumnFooter = ({}) => {
     return(
-        <div className=""></div>
+           
+        <div className="p-[10px] basis-[15%]">
+            {/* col h3 */}
+            <h3 className="w-fit mb-[40px] relative">
+                {title} 
+                {/* underline */}
+                <div className="w-[100%] h-[5px] bg-[#767676] absolute top-[25px] left-0  rounded-[3px] overflow-hidden">
+                    <span className="w-[15px] h-[100%] bg-[#fff] rounded-[3px] absolute top-0 left-[10px] animate-[movingFooter_2s_linear_infinite]"></span>
+                </div>
+            </h3>
+            {/* col p */}
+            <p>ITPL Road Whitefield, Bangalore Karnataka, PIN 560066, Tehran</p>
+            <p></p>
+            <p className="w-fit border-b-1 border-b-[#ccc] my-[20px]">email@gamil.com</p>
+            <h4>+98-0123456789</h4>
+        </div>
     )
 }
 
--- src/component/footerAnimated/index.jsx
+++ src/component/footerAnimated/index.jsx
@@ -12,28 +12,8 @@ const FooterAnimated  = () => {
                     <img src="https://cdn.pixabay.com/photo/2017/05/31/16/39/windows-2360920_1280.png" alt="" className="w-[80px]"/>
                     <p>Subscribe Easy Tutorials Youtube channel to watch more videos on website development and Press the bell icon to get immediate notification of latest videos.</p>
                 </div>
-                {/* col 2 */}
-                <div className="p-[10px] basis-[15%]">
-                    {/* col h3 */}
-                    <h3 className="w-fit mb-[40px] relative">
-                        Office 
-                        {/* underline */}
-                        <div className="w-[100%] h-[5px] bg-[#767676] absolute top-[25px] left-0  rounded-[3px] overflow-hidden">
-                            <span className="w-[15px] h-[100%] bg-[#fff] rounded-[3px] absolute top-0 left-[10px] animate-[movingFooter_2s_linear_infinite]"></span>
-                        </div>
-                    </h3>
-                    {/* col p */}
-                    <p>ITPL Road Whitefield, Bangalore Karnataka, PIN 560066, Tehran</p>
-                    <p></p>
-                    <p className="w-fit border-b-1 border-b-[#ccc] my-[20px]">email@gamil.com</p>
-                    <h4>+98-0123456789</h4>
-                </div>
-                {/* col 3 */}
-                <div className="p-[10px] basis-[15%]">
-                    <h3 className="w-fit mb-[40px] relative">
-                        Links 
-                    </h3>
-                </div>
+
+           
            </div>
         </footer>
     )
