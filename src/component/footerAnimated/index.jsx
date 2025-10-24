bdf9ddb66d959e75db49db185a6a26c183431e73
amir <amirtaki623@gmail.com>
2025-10-24 18:38:02 +0330

    day 16 part 4

--- src/component/footerAnimated/index.jsx
+++ src/component/footerAnimated/index.jsx
@@ -1,6 +1,11 @@
 const FooterAnimated  = () => {
     return(
-        <div className=""></div>
+        <footer className="w-[100%] absolute bottom-0  bg-[linear-gradient(to_right,#00093c,#2d0b00)]">
+            {/* row */}
+            <div className="w-100 h-[100] ">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quam magnam, illo nihil eligendi, nemo, corporis dolorum voluptas nobis fugiat maxime rem veniam vero. Iusto minima sint quibusdam architecto eum.
+            </div>
+        </footer>
     )
 }
 
