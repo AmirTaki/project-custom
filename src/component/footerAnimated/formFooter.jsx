2a5688f43d21ed7142313bfa7eb50208dd1133cd
amir <amirtaki623@gmail.com>
2025-10-24 19:28:29 +0330

    day 16 part 34

---
+++ src/component/footerAnimated/formFooter.jsx
@@ -0,0 +1,15 @@
+const FormFooter = () => {
+    return (
+        <form className="pb-[15px] flex items-center justify-between border-b-1 border-b-[#ccc] mb-[50px]">
+            <i className="bi bi-envelope text-[18px] mr-[10px]"></i>
+            <input type="email" placeholder="Enter your email id" required 
+                className="w-[100%]! bg-transparent text-[#ccc] border-0 outline-0"
+            />
+            <button type="submit" className="bg-transparent border-0 outline-0 cursor-pointer">
+                <i className="bi bi-arrow-right  text-[16px] text-[#ccc]"></i>
+            </button>
+        </form>
+    )
+}
+
+export default FormFooter;
