988d2816b0e4ed56081515100a7355359aedeaa3
amir <amirtaki623@gmail.com>
2025-10-24 20:52:40 +0330

    day 16 part 50

--- src/component/generateQrCode/index.jsx
+++ src/component/generateQrCode/index.jsx
@@ -7,6 +7,8 @@ const GenerateQrCode = () => {
                 <h1 className="text-[26px] text-center text-white mb-[26px] uppercase">Generate QR Code</h1>
                 <input 
                     className="w-[100%] mb-[12px] p-[15px] outline-0 rounded-[8px]"
+                    type = 'text'
+                    placeholder="Type your text ot URL"
                 />
             </div>
         </div>
