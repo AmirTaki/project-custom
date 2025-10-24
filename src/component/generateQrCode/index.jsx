d7073875ddb201f3b8d2950f5355387cf878cdcf
amir <amirtaki623@gmail.com>
2025-10-25 00:45:34 +0330

    day 16 part 79

--- src/component/generateQrCode/index.jsx
+++ src/component/generateQrCode/index.jsx
@@ -1,8 +1,51 @@
-import { useReducer } from "react"
-import { QRProvider } from "./QRContext"
+import {  useReducer, useRef } from "react"
 import QrHeader from "./qrHeader"
+import QRBody from "./qrBody";
 
 const GenerateQrCode = () => {
+    const qrContainer = useRef(null);
+    const imgRef = useRef(null);
+    const reducerQrCode = (state, action)=>{
+    switch(action.type){    
+        case "input":
+            return {...state, value : action.payload.event}
+            
+            case "size" : 
+                return {...state, size : action.payload.event}
+            
+            case "handerGenerate" :
+                const generateQRCode = () => {
+                    qrContainer.current.innerHTML = ''
+    
+                    new QRCode (qrContainer.current, {
+                    text : state.value,
+                    width : state.size,
+                    height : state.size,
+                    colorDark : '#000000',
+                    colorLight : '#ffffff'
+                    })
+                }
+
+                state.value.length > 0 ? generateQRCode() : window.alert('Enter the text or URL to generate your QR code')
+                return {...state} 
+            
+
+            case "handlerDownload" : 
+                const canvas =  qrContainer.current?.querySelector('canvas')
+                if(!canvas){alert('QR code not generated yet'); return;}
+
+                const dataUrl = canvas.toDataURL('image/png')
+
+                if(imgRef.current) imgRef.current.src = dataUrl;
+
+                const a =  document.createElement('a')
+                a.href = dataUrl;
+                a.download = dataUrl;
+                a.click()
+                a.remove()
+                return {...state}
+        }
+    }
 
     const [state, disptach] =   useReducer(reducerQrCode, {
         value : '',
@@ -11,42 +54,30 @@ const GenerateQrCode = () => {
     })
 
     return (
-        <QRProvider>
-            <div className="w-[400px] bg-[#155e75] p-[30px] rounded-[8px]">
-                {/* qr header */}
-                <QrHeader state = {state} disptach = {disptach}/>
-        
-                {/* qr body */}
-                <div 
-                    ref = {qrContainer}
-                    className="grid place-items-center"
-                >
-                    <img 
-                        ref = {imgRef} 
-                        src="a" alt="" 
-                        className= "max-w-[100%] max-h-[100%] mb-[10px] p-[20px]border-[.5px] border-white rounded-[8px]"
-                    />
-                </div>
-
-                {/* qr footer */}
-                <div className="mt-[30px] flex justify-center">
-                    <button 
-                        onClick={() => {disptach({type : 'handerGenerate'})}}
-                        className="bg-white text-[20px] py-[14px] px-[36px] mx-[2px] text-[#155e75] font-[600] rounded-[8px] cursor-pointer"
-                    >
-                        Generate
-                    </button>
-
-                    <button                     
-                        onClick={() => {disptach({type : "handlerDownload"})}}
-                        className="bg-white text-[20px] py-[14px] px-[36px] mx-[2px] text-[#155e75] font-[600] rounded-[8px] cursor-pointer"
-                    >
-                        Download
-                    </button>
-                </div>
-            </div>
+        <div className="w-[400px] bg-[#155e75] p-[30px] rounded-[8px]">
+            {/* qr header */}
+            <QrHeader state = {state} disptach = {disptach}/>
     
-        </QRProvider>
+            {/* qr body */}
+            <QRBody qrContainer={qrContainer} imgRef = {imgRef} />
+
+            {/* qr footer */}
+            <div className="mt-[30px] flex justify-center">
+                <button 
+                    onClick={() => {disptach({type : 'handerGenerate'})}}
+                    className="bg-white text-[20px] py-[14px] px-[36px] mx-[2px] text-[#155e75] font-[600] rounded-[8px] cursor-pointer"
+                >
+                    Generate
+                </button>
+
+                <button                     
+                    onClick={() => {disptach({type : "handlerDownload"})}}
+                    className="bg-white text-[20px] py-[14px] px-[36px] mx-[2px] text-[#155e75] font-[600] rounded-[8px] cursor-pointer"
+                >
+                    Download
+                </button>
+            </div>
+        </div>
     )
 }
 
---
+++ src/component/generateQrCode/qrBody.jsx
@@ -0,0 +1,16 @@
+const QRBody = ({qrContainer, QRBody}) => {
+    return(
+        <div 
+            ref = {qrContainer}
+            className="grid place-items-center"
+        >
+            <img 
+                ref = {imgRef} 
+                src="a" alt="" 
+                className= "max-w-[100%] max-h-[100%] mb-[10px] p-[20px]border-[.5px] border-white rounded-[8px]"
+            />
+        </div>
+    )
+}
+
+export default QRBody;
