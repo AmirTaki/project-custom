ee00991e43cf17f5f3550a0940ac191b95f1fb2f
amir <amirtaki623@gmail.com>
2025-10-24 21:50:05 +0330

    day 16 part 61

--- src/component/generateQrCode/index.jsx
+++ src/component/generateQrCode/index.jsx
@@ -1,11 +1,24 @@
-import { useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 
 
 const options = ['100', '200', '300', '400', '500', '600', '700', '800', '900', '1000' ]
 const GenerateQrCode = () => {
+
+    const [valueInput, setValueInput] =  useState('')
+
     const downloadBtn =  useRef(null)
     const generateBtn =  useRef(null)
-    const inputQr = useRef(null)
+    const inputQr = useRef(valueInput)
+    const sizesRef = useRef(null)
+
+   
+    const isEmptyInput = () => {
+
+    }
+    const handerGenerate = (event) => {
+        event.preventDefault();
+        isEmptyInput()
+    }
     return(
         // box
         <div className="w-[400px] bg-[#155e75] p-[30px] rounded-[8px]">
@@ -14,6 +27,7 @@ const GenerateQrCode = () => {
                 <h1 className="text-[26px] text-center text-white mb-[26px] uppercase">Generate QR Code</h1>
                 <input 
                     ref = {inputQr}
+                    onChange={(e) => {setValueInput(e.target.value)}}
                     className="w-[100%] mb-[12px] p-[15px] outline-0 rounded-[8px] text-[18px] border-2 border-[#7fb7c9] bg-white"
                     type = 'text'
                     placeholder="Type your text ot URL"
@@ -25,7 +39,11 @@ const GenerateQrCode = () => {
                     >
                         Select Size :
                     </label>
-                    <select name="" id="sizes" className="p-[8px] rounded-[8px] text-[18px] outline-0 border-2 bg-white border-[#7fb7c9]">
+                    <select 
+                        ref = {sizesRef}
+                        name="" 
+                        id="sizes" 
+                        className="p-[8px] rounded-[8px] text-[18px] outline-0 border-2 bg-white border-[#7fb7c9]">
                         {options.map((opt, ind) => {
                             return(
                                 <option value = {opt} key = {ind}>{opt}x{opt}</option>
@@ -44,6 +62,7 @@ const GenerateQrCode = () => {
             {/* qr footer */}
             <div className="mt-[30px] flex justify-center">
                 <button 
+                    onClick={handerGenerate}
                     ref = {generateBtn}
                     className="bg-white text-[20px] py-[14px] px-[36px] mx-[2px] text-[#155e75] font-[600] rounded-[8px] cursor-pointer"
                 >
