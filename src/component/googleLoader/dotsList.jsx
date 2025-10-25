eea87b71748c87e26fe111bb74bf361fb481cbfe
amir <amirtaki623@gmail.com>
2025-10-25 21:30:45 +0330

    day 17 part 42

--- src/component/googleLoader/dotsList.jsx
+++ src/component/googleLoader/dotsList.jsx
@@ -2,13 +2,23 @@ export const DotsList = [
 
     {
         id : 0, 
-        delay : 0,
+        delay : -.25,
         color : "#008ae6",
     },
     {
         id : 1, 
-        delay : -.25,
+        delay : -5,
         color : "#e60000",
+    },
+    {
+        id : 2, 
+        delay : -.75,
+        color : "#ffcc00",
+    },
+    {
+        id : 3, 
+        delay : -1,
+        color : "#008800",
     },
 
 ]
