3c25ad4d41ae0fedc19ea30c14225321c4c2ffbb
amir <amirtaki623@gmail.com>
2025-10-25 21:27:45 +0330

    day 17 part 39

--- src/component/googleLoader/index.jsx
+++ src/component/googleLoader/index.jsx
@@ -5,9 +5,9 @@ const GoogleLoader = () => {
         <div className="flex gap-10">
             {/* dot */}
             <div 
-                style={{'--color-bg':'#008ae6'}}
+                style={{'--color-bg':'#008ae6', '--delay-time' : 0}}
                 className="h-[20px] w-[20px] rounded-[50%] bg-[var(--color-bg)] shadow-[0_5px_10px_rgba(0,0,0,.1)]
-                animate-[loaderGoogle_1s_ease-in-out_infinite_alternate]
+                animate-[loaderGoogle_1s_ease-in-out_infinite_alternate] delay-[var(--delay-time)]
             "></div>
         </div>
     )
