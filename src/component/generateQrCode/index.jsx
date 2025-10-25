2e0e2e0851242b9627edb2f64d9dc1f4b2dc7a05
amir <amirtaki623@gmail.com>
2025-10-25 03:58:35 +0330

    day 17 part 1

--- src/component/generateQrCode/index.jsx
+++ src/component/generateQrCode/index.jsx
@@ -7,49 +7,9 @@ const GenerateQrCode = () => {
     const qrContainer = useRef(null);
     const imgRef = useRef(null);
     
-    const reducerQrCode = (state, action)=>{
-    switch(action.type){    
-        case "input":
-            return {...state, value : action.payload.event}
-            
-            case "size" : 
-                return {...state, size : action.payload.event}
-            
-            case "handerGenerate" :
-                const generateQRCode = () => {
-                    qrContainer.current.innerHTML = ''
-    
-                    new QRCode (qrContainer.current, {
-                    text : state.value,
-                    width : state.size,
-                    height : state.size,
-                    colorDark : '#000000',
-                    colorLight : '#ffffff'
-                    })
-                }
 
-                state.value.length > 0 ? generateQRCode() : window.alert('Enter the text or URL to generate your QR code')
-                return {...state} 
-            
 
-            case "handlerDownload" : 
-                const canvas =  qrContainer.current?.querySelector('canvas')
-                if(!canvas){alert('QR code not generated yet'); return;}
-
-                const dataUrl = canvas.toDataURL('image/png')
-
-                if(imgRef.current) imgRef.current.src = dataUrl;
-
-                const a =  document.createElement('a')
-                a.href = dataUrl;
-                a.download = dataUrl;
-                a.click()
-                a.remove()
-                return {...state}
-        }
-    }
-
-    const [state, disptach] =   useReducer(reducerQrCode, {
+    const [state, disptach] = useReducer(reducerQrCode, {
         value : '',
         size : 100,
         options : ['100', '200', '300', '400', '500', '600', '700', '800', '900', '1000' ]
---
+++ src/component/generateQrCode/reducer.jsx
@@ -0,0 +1,47 @@
+export  const reducerQrCode = (state, action)=>{
+    switch(action.type){    
+        case "input":
+            return {...state, value : action.payload.event}
+            
+            case "size" : 
+                return {...state, size : action.payload.event}
+            
+            case "handerGenerate" :
+                const generateQRCode = () => {
+                    try{
+
+                        qrContainer.current.innerHTML = ''
+
+                        new QRCode (qrContainer.current, {
+                        text : state.value,
+                        width : state.size,
+                        height : state.size,
+                        colorDark : '#000000',
+                        colorLight : '#ffffff'
+                        })
+                    }
+                    catch (err) {
+                        alert('please entert connection check!!!')
+                    }
+                }
+
+                state.value.length > 0 ? generateQRCode() : window.alert('Enter the text or URL to generate your QR code')
+                return {...state} 
+            
+
+            case "handlerDownload" : 
+                const canvas =  qrContainer.current?.querySelector('canvas')
+                if(!canvas){alert('QR code not generated yet'); return;}
+
+                const dataUrl = canvas.toDataURL('image/png')
+
+                if(imgRef.current) imgRef.current.src = dataUrl;
+
+                const a =  document.createElement('a')
+                a.href = dataUrl;
+                a.download = dataUrl;
+                a.click()
+                a.remove()
+                return {...state}
+        }
+    }
