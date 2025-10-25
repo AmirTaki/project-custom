b18d72dc2d807260d51c6fe1539412b7f13b9434
amir <amirtaki623@gmail.com>
2025-10-25 19:20:54 +0330

    day 17 part 16

--- src/component/userLocation/index.jsx
+++ src/component/userLocation/index.jsx
@@ -1,12 +1,17 @@
+import imgUser from "./img/free-location-icon-2955-thumb.png"
 const UserLocation = () => {
     return (
         // container 
         <div className="w-[80vw] max-w-[37.5em] bg-white py-[3em] px-[1.8em] absolute
             -translate-x-1/2 -translate-y-1/2 top-[50%] left-[50%] rounded-[6em] 
-            shadow-[0_.6em_2.5em_rgba(0,7,70,.2)]
-        "
-        
-        ></div>
+            shadow-[0_.6em_2.5em_rgba(0,7,70,.2)]"
+        >
+            {/* img */}
+            <img src={imgUser} alt="" 
+                className="w-[6.25em] block m-auto" 
+            />
+
+        </div>
     )
 }
 export default UserLocation;
