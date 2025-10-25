446c97f916a049083e5de13a0eac92d5075a6757
amir <amirtaki623@gmail.com>
2025-10-25 21:25:26 +0330

    day 17 part 37

--- src/component/googleLoader/index.jsx
+++ src/component/googleLoader/index.jsx
@@ -1,9 +1,13 @@
+import "./styles.css"
 const GoogleLoader = () => {
     return (
         // wrapper
         <div className="flex gap-10">
             {/* dot */}
-            <div className="h-[20px] w-[20px] rounded-[50%] bg-[#008ae6] shadow-[0_5px_10px_rgba(0,0,0,.1)]"></div>
+            <div 
+                className="h-[20px] w-[20px] rounded-[50%] bg-[#008ae6] shadow-[0_5px_10px_rgba(0,0,0,.1)]
+                animate-[loaderGoogle_1s_ease-in-out_infinite_alternate]
+            "></div>
         </div>
     )
 }
