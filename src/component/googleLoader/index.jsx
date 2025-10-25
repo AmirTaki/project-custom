b0dd63c5ee160c96a637bf60f0d3b242569cd48a
amir <amirtaki623@gmail.com>
2025-10-25 21:22:58 +0330

    day 17 part 35

--- src/component/googleLoader/index.jsx
+++ src/component/googleLoader/index.jsx
@@ -1,6 +1,10 @@
 const GoogleLoader = () => {
     return (
-        <div className=""></div>
+        // wrapper
+        <div className="flex gap-10">
+            {/* dot */}
+            <div className="h-[20px] w-[20px] rounded-[50%] bg-[#008ae6] shadow-[0_5px_10px_rgba(0,0,0,.1)]"></div>
+        </div>
     )
 }
 
