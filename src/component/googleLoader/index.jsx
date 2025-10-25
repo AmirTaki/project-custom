e31e2309aafed3e8dd717706b9787964b4184cca
amir <amirtaki623@gmail.com>
2025-10-25 21:33:59 +0330

    day 17 part 44

--- src/component/googleLoader/index.jsx
+++ src/component/googleLoader/index.jsx
@@ -10,7 +10,7 @@ const GoogleLoader = () => {
                 return(
                     <div 
                         key = {dots.id}
-                        style={{'--color-bg': `${dots.color}`, '--delay-time' : `${dots.delay}s`}}
+                        style={{'--color-bg': `${dots.color}`, '--delay-time' : `${dots.delay}`}}
                         className="h-[20px] w-[20px] rounded-[50%] bg-[var(--color-bg)] shadow-[0_5px_10px_rgba(0,0,0,.1)]
                         animate-[loaderGoogle_1s_ease-in-out_infinite_alternate] delay-[var(--delay-time)]
                     "></div>
