232d8f141dd3bd95c05b35ca8300f5f6fdb71379
amir <amirtaki623@gmail.com>
2025-10-25 19:17:30 +0330

    day 17 part 15

--- src/component/userLocation/index.jsx
+++ src/component/userLocation/index.jsx
@@ -1,7 +1,12 @@
 const UserLocation = () => {
     return (
         // container 
-        <div className="w-[80vw] max-w-[37.5em] bg-white py-[3em] px-[1.8em] absolute"></div>
+        <div className="w-[80vw] max-w-[37.5em] bg-white py-[3em] px-[1.8em] absolute
+            -translate-x-1/2 -translate-y-1/2 top-[50%] left-[50%] rounded-[6em] 
+            shadow-[0_.6em_2.5em_rgba(0,7,70,.2)]
+        "
+        
+        ></div>
     )
 }
 export default UserLocation;
