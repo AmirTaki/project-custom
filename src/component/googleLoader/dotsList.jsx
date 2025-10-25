7db8246efaa1099e88bcc61f71508646c1203ad5
amir <amirtaki623@gmail.com>
2025-10-25 21:36:13 +0330

    day 17 part 45

--- src/component/googleLoader/dotsList.jsx
+++ src/component/googleLoader/dotsList.jsx
@@ -2,22 +2,22 @@ export const DotsList = [
 
     {
         id : 0, 
-        delay : '-.25s',
+        delay : -.25,
         color : "#008ae6",
     },
     {
         id : 1, 
-        delay : '-.5s',
+        delay : -.5,
         color : "#e60000",
     },
     {
         id : 2, 
-        delay : '-.75s',
+        delay : -.75,
         color : "#ffcc00",
     },
     {
         id : 3, 
-        delay : '-1s',
+        delay : -1,
         color : "#008800",
     },
 
--- src/component/googleLoader/index.jsx
+++ src/component/googleLoader/index.jsx
@@ -10,9 +10,9 @@ const GoogleLoader = () => {
                 return(
                     <div 
                         key = {dots.id}
-                        style={{'--color-bg': `${dots.color}`, '--delay-time' : `${dots.delay}`}}
+                        style={{'--color-bg': `${dots.color}`, animationDelay : `${dots.delay}s`}}
                         className="h-[20px] w-[20px] rounded-[50%] bg-[var(--color-bg)] shadow-[0_5px_10px_rgba(0,0,0,.1)]
-                        animate-[loaderGoogle_1s_ease-in-out_infinite_alternate] delay-[var(--delay-time)]
+                        animate-[loaderGoogle_1s_ease-in-out_infinite_alternate] 
                     "></div>
                 )  
             })}
