77f3c85ab84b02223e4517d1792a9003666da482
amir <amirtaki623@gmail.com>
2025-10-25 21:32:28 +0330

    day 17 part 43

--- src/component/googleLoader/index.jsx
+++ src/component/googleLoader/index.jsx
@@ -1,14 +1,21 @@
+import { DotsList } from "./dotsList";
 import "./styles.css"
+
 const GoogleLoader = () => {
     return (
         // wrapper
         <div className="flex gap-10">
             {/* dot */}
-            <div 
-                style={{'--color-bg':'#008ae6', '--delay-time' : 0}}
-                className="h-[20px] w-[20px] rounded-[50%] bg-[var(--color-bg)] shadow-[0_5px_10px_rgba(0,0,0,.1)]
-                animate-[loaderGoogle_1s_ease-in-out_infinite_alternate] delay-[var(--delay-time)]
-            "></div>
+            {DotsList.map((dots) => {
+                return(
+                    <div 
+                        key = {dots.id}
+                        style={{'--color-bg': `${dots.color}`, '--delay-time' : `${dots.delay}s`}}
+                        className="h-[20px] w-[20px] rounded-[50%] bg-[var(--color-bg)] shadow-[0_5px_10px_rgba(0,0,0,.1)]
+                        animate-[loaderGoogle_1s_ease-in-out_infinite_alternate] delay-[var(--delay-time)]
+                    "></div>
+                )  
+            })}
         </div>
     )
 }
