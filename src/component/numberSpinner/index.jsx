191e69c8161cefca2ddcf82dbdf081f44f35fa88
amir <amirtaki623@gmail.com>
2025-10-25 21:45:35 +0330

    day 17 part 48

--- src/component/numberSpinner/index.jsx
+++ src/component/numberSpinner/index.jsx
@@ -2,7 +2,10 @@ const NumberSpinner = () => {
     return (
         // box
         <div className="w-[80px] h-[500px] flex justify-center items-center bg-[#333] rounded-[50px] shadow-[0_5px_50px_rgba(0,0,0,.5)]">
-            {/*  */}
+            {/* spinner */}
+            <div className="relative flex items-center justify-center flex-col w-[40px] select-none">
+                
+            </div>
         </div>
     )
 }
