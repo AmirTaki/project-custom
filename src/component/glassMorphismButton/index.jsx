dc2322f3b6a1f77e5901a580bf7c1cb4c04a2028
amir <amirtaki623@gmail.com>
2025-10-26 19:22:40 +0330

    day 18 part 21

--- src/component/glassMorphismButton/index.jsx
+++ src/component/glassMorphismButton/index.jsx
@@ -16,17 +16,19 @@ const GlassMorphism = () => {
                 after:content-[''] after:absolute after:left-[50%] after:-translate-x-1/2
                 after:top-[-5px] after:w-[30px] after:h-[10px] after:bg-[#ff1f71] 
                 after:rounded-[10px] after:duration-500 after:delay-0 
-                btn
-            ">
-                <Link 
+                btn group
+                hover:after:top-0 hover:after:h-[50%] hover:after:w-[80%] hover:after:rounded-[30px] hover:after:delay-[.5s]
+            ">asfs
+                {/* <Link 
                     to = "/" 
                     className = "absolute top-0 left-0 w-[100%] h-[100%] flex items-center justify-center bg-[rgba(255,255,255,.5)] shadow-[0_15px_35px_rgba(0,0,0,.2)] border-t-1 border-t-[rgba(255,255,255,.1)] border-b-1 border-b-[rgba(255,255,255,.1)]  rounded-[30px] text-white  font-[400] leading-[1px] decoration-0 overflow-hidden duration-500 blur-[15px]
                         before:content-[''] before:absolute before:top-0 before:left-0 before:w-[50%] before:h-[100%] before:bg-[linear-gradient(to_left,rgba(255,255,255,.15),transparent]
                         before:skew-x-45 before:translate-x-0 before:duration-500
+                        group-hover:before:skew-x-45 group-hover:before:translate-x-[200%] 
                     "
                 > 
                     Read More
-                </Link>
+                </Link> */}
             </div>
             
             </BrowserRouter>
