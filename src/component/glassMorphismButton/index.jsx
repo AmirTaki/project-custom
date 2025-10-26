dd657bff1fbe697ace0f5e3289a6b22a1139a7be
amir <amirtaki623@gmail.com>
2025-10-26 18:46:00 +0330

    day 18 part 9

--- src/component/glassMorphismButton/index.jsx
+++ src/component/glassMorphismButton/index.jsx
@@ -1,7 +1,10 @@
 const glassMorphism = () => {
     return (
         // container
-        <div className="w-[600px] flex flex-wrap justify-around"></div>
+        <div className="w-[600px] flex flex-wrap justify-around">
+            {/* btn */}
+            <div className="relative w-[155px] h-[50px] m-[20px]"></div>
+        </div>
     )
 }
 export default glassMorphism;
