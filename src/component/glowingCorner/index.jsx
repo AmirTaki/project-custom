cd2c884447dfb60072e1aeb6ddda2fd76b03678b
amir <amirtaki623@gmail.com>
2025-10-26 23:52:26 +0330

    day 18 part 50

--- src/component/glowingCorner/index.jsx
+++ src/component/glowingCorner/index.jsx
@@ -4,9 +4,10 @@ const GlowingCorner = () => {
         <div className="relative flex items-center justify-center min-h-[100vh] gap-[50px] bg-[#222] flex-wrap">
             {/* card */}
             <div 
-                style={{left : "10px", top : '10px', background : "linear-gradient(#0f0, blue, red)"}}
+                style={{'--left-position' : "--left-position", '--top-position' : '', background : "linear-gradient(#0f0, blue, red)"}}
                 className="relative w-[320px] h-[400px] bg-[rgba(45,45,45,1)] rounded-[20px] overflow-hidden
                 before:content-[''] before:absolute before:translate-x-[-115%] before:translate-y-[-100%]
+                before:left-[var(--left-position)] before:top-[var(--top-position)]
                 before:w-[360px] before:h-[360px] before:rounded-[102%] before:opacity-0 before:transition-[left 0s_top 0s] duration-500
             "></div>
         </div>
