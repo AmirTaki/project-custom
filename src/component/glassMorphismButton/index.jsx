e014b582720571683279e724d8a3b1ade21098fa
amir <amirtaki623@gmail.com>
2025-10-26 18:50:33 +0330

    day 18 part 12

--- src/component/glassMorphismButton/index.jsx
+++ src/component/glassMorphismButton/index.jsx
@@ -1,13 +1,21 @@
+import { BrowserRouter } from "react-router-dom";
+
 const glassMorphism = () => {
     return (
         // container
         <div className="w-[600px] flex flex-wrap justify-around">
             {/* btn */}
+            <BrowserRouter >
+            
             <div className="relative w-[155px] h-[50px] m-[20px]
                 before:content-[''] before:absolute before:left-[50%] before:-translate-x-1/2
                 before:bottom-[-5px] before:w-[30px] before:h-[10px] before:bg-[#ff1f71]
                 before:rounded-[10px] before:duration-[.5s] before:delay-0
-            "></div>
+            ">
+                <Link to = "/"> Read More</Link>
+            </div>
+            
+            </BrowserRouter>
         </div>
     )
 }
