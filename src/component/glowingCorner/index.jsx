329c3478048e2bc7e6c5be2d6a96b7cd3373db56
amir <amirtaki623@gmail.com>
2025-10-26 23:45:57 +0330

    day 18 part 45

--- src/component/glowingCorner/index.jsx
+++ src/component/glowingCorner/index.jsx
@@ -1,7 +1,10 @@
 const GlowingCorner = () => {
     return( 
         // container
-        <div className="relative flex items-center justify-center min-h-[100vh] gap-[50px] bg-[#222]"></div>
+        <div className="relative flex items-center justify-center min-h-[100vh] gap-[50px] bg-[#222] flex-wrap">
+            {/* card */}
+            <div className="relative w-[320px] h-[400px] bg-[rgba(45,45,45,1)] rounded-[20px] overflow-hidden"></div>
+        </div>
     )
 }
 
