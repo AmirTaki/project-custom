f6fab261a838a5916887cee7b2bc776b0c9a17d5
amir <amirtaki623@gmail.com>
2025-10-27 00:51:40 +0330

    day 19 part 9

--- src/component/glowingCorner/index.jsx
+++ src/component/glowingCorner/index.jsx
@@ -6,7 +6,7 @@ const GlowingCorner = () => {
     const cardRef =  useRef(null)
     const handlerMouseMove = useCallback((e) => {
 
-        const node = ref.current ;
+        const node = cardRef.current ;
         if(!node) return;
         const rect = node.getBoundingClientRect()
         const x =  e.clientX - rect.left
@@ -28,9 +28,11 @@ const GlowingCorner = () => {
                 ref = {cardRef}
                 onMouseMove={handlerMouseMove}
                 style={{'--left-position' : `${state.top}`, '--top-position' : `${state.left}`, "--color-bg" : "#0f0"}}
-                className="relative w-[320px] h-[400px] bg-[rgba(45,45,45,1)] rounded-[20px] overflow-hidden
+                className="
+                before:bg-[linear-gradient(var(--color-bg),blue,red)]
+                relative w-[320px] h-[400px] bg-[rgba(45,45,45,1)] rounded-[20px] overflow-hidden
                 before:content-[''] before:absolute before:-translate-x-1/2 before:-translate-y-1/2
-                before:left-[var(--left-position)] before:top-[var(--top-position)] before:bg-[linear-gradient(var(--color-bg),blue,red)]
+                before:left-[var(--left-position)] before:top-[var(--top-position)] 
                 before:w-[360px] before:h-[360px] before:rounded-[102%] before:opacity-0 before:transition-[left 0s_top 0s] duration-500
                 after:content-[''] after:absolute after:inset-[2px] after:bg-[rgba(45,45,45,.9)] after:rounded-[18px]
                 hover:before:opacity-100!
