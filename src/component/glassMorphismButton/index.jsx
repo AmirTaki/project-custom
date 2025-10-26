a4bd79670dd11d1c94ea56cc734e7758d877fd67
amir <amirtaki623@gmail.com>
2025-10-26 19:09:19 +0330

    day 18 part 17

--- src/component/glassMorphismButton/index.jsx
+++ src/component/glassMorphismButton/index.jsx
@@ -21,7 +21,7 @@ const GlassMorphism = () => {
                 <Link to = "/" 
                     className = "absolute top-0 left-0 w-[100%] h-[100%] flex items-center justify-center
                         bg-[rgba(255,255,255,.5)] shadow-[0_15px_35px_rgba(0,0,0,.2)] border-t-1 border-t-[rgba(255,255,255,.1)] border-b-1 border-b-[rgba(255,255,255,.1)]
-                        rounded-[30px]
+                        rounded-[30px] text-white z-10 font-[400] leading-[1px] decoration-0 
                         "> Read More
                 </Link>
             </div>
