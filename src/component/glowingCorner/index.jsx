54ebf8517daf313fea2e82d60a2fc1f6ce2efbb3
amir <amirtaki623@gmail.com>
2025-10-27 01:07:00 +0330

    day 19 part 10

--- src/component/glowingCorner/index.jsx
+++ src/component/glowingCorner/index.jsx
@@ -19,7 +19,7 @@ const GlowingCorner = () => {
     useEffect(() => {
         console.log(state)
     }, [state])
-
+// before:bg-[linear-gradient(var(--color-bg),blue,red)]
     return( 
         // container
         <div className="relative flex items-center justify-center min-h-[100vh] gap-[50px] bg-[#222] flex-wrap">
@@ -27,13 +27,14 @@ const GlowingCorner = () => {
             <div 
                 ref = {cardRef}
                 onMouseMove={handlerMouseMove}
-                style={{'--left-position' : `${state.top}`, '--top-position' : `${state.left}`, "--color-bg" : "#0f0"}}
+                style={{'--x' : `${state.top}`, '--y' : `${state.left}`, "--color-bg" : "#0f0"}}
                 className="
-                before:bg-[linear-gradient(var(--color-bg),blue,red)]
+                
+                before:bg-[radial-gradient(circle_at_49px_40px,_var(--color-bg),_transparent_40%)]
                 relative w-[320px] h-[400px] bg-[rgba(45,45,45,1)] rounded-[20px] overflow-hidden
                 before:content-[''] before:absolute before:-translate-x-1/2 before:-translate-y-1/2
                 before:left-[var(--left-position)] before:top-[var(--top-position)] 
-                before:w-[360px] before:h-[360px] before:rounded-[102%] before:opacity-0 before:transition-[left 0s_top 0s] duration-500
+                before:w-[360px] before:h-[360px] before:rounded-[102%] before:opacity-0  duration-500
                 after:content-[''] after:absolute after:inset-[2px] after:bg-[rgba(45,45,45,.9)] after:rounded-[18px]
                 hover:before:opacity-100!
             "></div>
