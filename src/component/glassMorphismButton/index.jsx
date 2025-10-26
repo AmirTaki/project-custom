7d1a4efa9115404a8a3813c485e6cf6a28872db7
amir <amirtaki623@gmail.com>
2025-10-26 18:55:53 +0330

    day 18 part 14

--- src/component/glassMorphismButton/index.jsx
+++ src/component/glassMorphismButton/index.jsx
@@ -10,9 +10,12 @@ const GlassMorphism = () => {
             <div className="relative w-[155px] h-[50px] m-[20px]
                 before:content-[''] before:absolute before:left-[50%] before:-translate-x-1/2
                 before:bottom-[-5px] before:w-[30px] before:h-[10px] before:bg-[#ff1f71]
-                before:rounded-[10px] before:duration-[.5s] before:delay-0
+                before:rounded-[10px] before:duration-500 before:delay-0
+                after:content-[''] after:absolute after:left-[50%] after:-translate-x-1/2
+                after:top-[-5px] after:w-[30px] after:h-[10px] after:bg-[#ff1f71] 
+                after:rounded-[10px] after:duration-500 after:delay-0
             ">
-                <Link to = "/" className = "text-white"> Read More</Link>
+                <Link to = "/" className = ""> Read More</Link>
             </div>
             
             </BrowserRouter>
