f27c38c619c73a19f4060646d9d562928e63d721
amir <amirtaki623@gmail.com>
2025-10-26 21:11:28 +0330

    day 18 part 40

---
+++ src/component/glassMorphismButton/colorsItem.jsx
@@ -0,0 +1,17 @@
+export const ColorsItems = [
+    {
+        id : 0,
+        color : "#ff1f71"
+
+    },
+    {
+        id : 1,
+        color : "#2db2ff"
+
+    },
+    {
+        id : 2,
+        color : "#1eff45"
+
+    },
+]
--- src/component/glassMorphismButton/index.jsx
+++ src/component/glassMorphismButton/index.jsx
@@ -1,44 +1,23 @@
 import { BrowserRouter, Link } from "react-router-dom";
 import "./styles.css"
 
+
 const GlassMorphism = () => {
     return (
         // container
         <div className="w-[600px] flex flex-wrap justify-around">
             {/* btn */}
-            <BrowserRouter >
-            
+            <BrowserRouter >            
             <div
-                style = {{'--color-bg' : "#ff1f71"}}
+                style = {{'--color-bg' : ""}}
                 className=" btn group btn-morphism">
                 <Link 
                     to = "/" 
-                    className ="link-morphism group-hover:before:skew-x-45 group-hover:before:translate-x-[200%] group-hover:tracking-[3px] "                    
+                    className ="link-morphism  "                    
                 > 
                     Read More
                 </Link>
             </div>
-            <div
-                style = {{'--color-bg' : "#2db2ff"}}
-                className=" btn group btn-morphism">
-                <Link 
-                    to = "/" 
-                    className ="link-morphism group-hover:before:skew-x-45 group-hover:before:translate-x-[200%] group-hover:tracking-[3px] "                    
-                > 
-                    Read More
-                </Link>
-            </div>
-            <div
-                style = {{'--color-bg' : "#1eff45"}}
-                className=" btn group btn-morphism "
-            >
-                <Link 
-                    to = "/" 
-                    className ="link-morphism group-hover:before:skew-x-45 group-hover:before:translate-x-[200%] group-hover:tracking-[3px]"> 
-                    Read More
-                </Link>
-            </div>
-            
             </BrowserRouter>
         </div>
     )
