6d77e790a44d70e4c191250c2fab1e0a637d0d1b
amir <amirtaki623@gmail.com>
2025-10-26 21:05:52 +0330

    day 18 part 37

--- src/component/glassMorphismButton/index.jsx
+++ src/component/glassMorphismButton/index.jsx
@@ -27,11 +27,16 @@ const GlassMorphism = () => {
                 </Link>
             </div>
             <div
-                className=" btn group btn-morphism">
+                style = {{'--color-bg' : "#1eff45"}}
+                className=" btn group btn-morphism 
+                        before:shadow-[0_0_5px_var(--color-bg),0_0_15px_var(--color-bg),0_0_30px_var(--color-bg),0_0_60px_var(--color-bg)]!
+                        after:shadow-[0_0_5px_var(--color-bg),0_0_15px_var(--color-bg),0_0_30px_#1eff45,0_0_60px_var(--color-bg)]!
+                        before:bg-[var(--color-bg)]!
+                        after:bg-[var(--color-bg)]!
+                ">
                 <Link 
                     to = "/" 
-                    className ="link-morphism group-hover:before:skew-x-45 group-hover:before:translate-x-[200%] group-hover:tracking-[3px] "                    
-                > 
+                    className ="link-morphism group-hover:before:skew-x-45 group-hover:before:translate-x-[200%] group-hover:tracking-[3px]"> 
                     Read More
                 </Link>
             </div>
