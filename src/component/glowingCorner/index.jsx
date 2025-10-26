d49efefd2fb0352fd8ccf57d2cb90e6052c4742d
amir <amirtaki623@gmail.com>
2025-10-26 23:49:04 +0330

    day 18 part 48

--- src/component/glowingCorner/index.jsx
+++ src/component/glowingCorner/index.jsx
@@ -5,6 +5,7 @@ const GlowingCorner = () => {
             {/* card */}
             <div className="relative w-[320px] h-[400px] bg-[rgba(45,45,45,1)] rounded-[20px] overflow-hidden
                 before:content-[''] before:absolute before:translate-x-[-115%] before:translate-y-[-100%]
+                before:w-[360px] before:h-[360px] before:rounded-[102%] before:opacity-0 before:transition-[left 0s_top 0s] duration-500
             "></div>
         </div>
     )
