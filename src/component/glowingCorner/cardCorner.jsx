860d0d0adc9a535fcd8192868b64e466f7b066de
amir <amirtaki623@gmail.com>
2025-10-27 01:26:35 +0330

    day 19 part 19

--- src/component/glowingCorner/cardCorner.jsx
+++ src/component/glowingCorner/cardCorner.jsx
@@ -1,4 +1,18 @@
+import { useCallback, useRef } from "react"
+
 const CardCorner = () => {
+    const cardRef =  useRef(null)
+    const handlerMouseMove = useCallback((e) => {
+
+        const node = cardRef.current ;
+        if(!node) return;
+        const rect = node.getBoundingClientRect()
+        const x =  e.clientX - rect.left
+        const y =  e.clientY - rect.top   
+        
+        node.style.setProperty('--x', `${x}px`)
+        node.style.setProperty('--y', `${y}px`)
+    }, [] )
     return(
         <div className=""></div>
     )
--- src/component/glowingCorner/index.jsx
+++ src/component/glowingCorner/index.jsx
@@ -1,22 +1,8 @@
-import { useCallback, useRef } from "react"
-import './styles.css'
 import { DataCard } from "./dataCrard"
+import './styles.css'
 
 const GlowingCorner = () => {
-    const cardRef =  useRef(null)
-    const handlerMouseMove = useCallback((e) => {
-
-        const node = cardRef.current ;
-        if(!node) return;
-        const rect = node.getBoundingClientRect()
-        const x =  e.clientX - rect.left
-        const y =  e.clientY - rect.top   
-        
-        node.style.setProperty('--x', `${x}px`)
-        node.style.setProperty('--y', `${y}px`)
-    }, [] )
-
-
+ 
     return( 
         // container
         <div className="relative flex items-center justify-center min-h-[100vh] gap-[50px] bg-[#222] flex-wrap">
