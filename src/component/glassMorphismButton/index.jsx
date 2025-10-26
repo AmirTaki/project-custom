122176c5b324a458562b22d2176bd6974f31c485
amir <amirtaki623@gmail.com>
2025-10-26 18:49:26 +0330

    day 18 part 11

--- src/component/glassMorphismButton/index.jsx
+++ src/component/glassMorphismButton/index.jsx
@@ -6,7 +6,7 @@ const glassMorphism = () => {
             <div className="relative w-[155px] h-[50px] m-[20px]
                 before:content-[''] before:absolute before:left-[50%] before:-translate-x-1/2
                 before:bottom-[-5px] before:w-[30px] before:h-[10px] before:bg-[#ff1f71]
-                before:
+                before:rounded-[10px] before:duration-[.5s] before:delay-0
             "></div>
         </div>
     )
