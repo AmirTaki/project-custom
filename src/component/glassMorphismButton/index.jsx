1bd3ea6a01f9f2a43ae98846da17a263583c3143
amir <amirtaki623@gmail.com>
2025-10-26 19:13:18 +0330

    day 18 part 19

--- src/component/glassMorphismButton/index.jsx
+++ src/component/glassMorphismButton/index.jsx
@@ -15,14 +15,16 @@ const GlassMorphism = () => {
                 before:rounded-[10px] before:duration-500 before:delay-0
                 after:content-[''] after:absolute after:left-[50%] after:-translate-x-1/2
                 after:top-[-5px] after:w-[30px] after:h-[10px] after:bg-[#ff1f71] 
-                after:rounded-[10px] after:duration-500 after:delay-0
+                after:rounded-[10px] after:duration-500 after:delay-0 
                 btn
             ">
-                <Link to = "/" 
-                    className = "absolute top-0 left-0 w-[100%] h-[100%] flex items-center justify-center
-                        bg-[rgba(255,255,255,.5)] shadow-[0_15px_35px_rgba(0,0,0,.2)] border-t-1 border-t-[rgba(255,255,255,.1)] border-b-1 border-b-[rgba(255,255,255,.1)]
-                        rounded-[30px] text-white z-10 font-[400] leading-[1px] decoration-0 overflow-hidden duration-500 blur-[15px]
-                        "> Read More
+                <Link 
+                    to = "/" 
+                    className = "absolute top-0 left-0 w-[100%] h-[100%] flex items-center justify-center bg-[rgba(255,255,255,.5)] shadow-[0_15px_35px_rgba(0,0,0,.2)] border-t-1 border-t-[rgba(255,255,255,.1)] border-b-1 border-b-[rgba(255,255,255,.1)]  rounded-[30px] text-white  font-[400] leading-[1px] decoration-0 overflow-hidden duration-500 blur-[15px]
+                        before:content-[''] before:absolute before:top-0 before:left-0 before:w-[50%] before:h-[100%]
+                    "
+                > 
+                    Read More
                 </Link>
             </div>
             
