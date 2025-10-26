7a102a5c007f124e53a80602080ba996c3188f54
amir <amirtaki623@gmail.com>
2025-10-26 18:48:59 +0330

    day 18 part 10

--- src/component/glassMorphismButton/index.jsx
+++ src/component/glassMorphismButton/index.jsx
@@ -3,7 +3,11 @@ const glassMorphism = () => {
         // container
         <div className="w-[600px] flex flex-wrap justify-around">
             {/* btn */}
-            <div className="relative w-[155px] h-[50px] m-[20px]"></div>
+            <div className="relative w-[155px] h-[50px] m-[20px]
+                before:content-[''] before:absolute before:left-[50%] before:-translate-x-1/2
+                before:bottom-[-5px] before:w-[30px] before:h-[10px] before:bg-[#ff1f71]
+                before:
+            "></div>
         </div>
     )
 }
