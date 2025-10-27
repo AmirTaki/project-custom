906f2f73fd3cab67357a10d6f30f9c2418b58ba1
amir <amirtaki623@gmail.com>
2025-10-27 23:05:41 +0330

    day 19 part 50

--- src/component/gradientGenerator/listButtons.jsx
+++ src/component/gradientGenerator/listButtons.jsx
@@ -1,5 +1,29 @@
 const ListButtons = {
-    buttons : {
-        id :
-    }
+    buttons :[ 
+        {
+            id : 0,
+            value : 'to top',
+            icon : 'fas fa-arrow-up',
+            flag : false,
+        },
+        {
+            id : 1,
+            value : 'to bpttom',
+            icon : 'fas fa-arrow-down',
+            flag : false,
+        },
+        {
+            id : 2,
+            value : 'to right',
+            icon : 'fas fa-arrow-right',
+            flag : false,
+        },
+        {
+            id : 3,
+            value : 'to left',
+            icon : 'fas fa-arrow-left',
+            flag : false,
+        },
+
+    ]
 }
