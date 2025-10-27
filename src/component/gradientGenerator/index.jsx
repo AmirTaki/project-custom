4b122d3322d54f71d66d861a1639c63c020978c7
amir <amirtaki623@gmail.com>
2025-10-27 21:01:50 +0330

    day 19 part 32

--- src/component/gradientGenerator/index.jsx
+++ src/component/gradientGenerator/index.jsx
@@ -1,12 +1,17 @@
 const GradientGenerator = () => {
     return(
         // box
-        <div className="w-[3000px]! h-[500px] bg-amber-200 mx-auto">
+        <div className="w-[3000px]! h-[500px] mx-auto">
             {/* container */}
             <div className="bg-[#fff] w-[400px] px-[50px] p-[30px] absolute 
                 -translate-x-1/2 -translate-y-1/2 top-[50%] left-[50%]
                 rounded-[10px] shadow-[0_20px_25px_rgba(0,0,0,0.25)]
             ">
+                {/* colors */}
+                <div className="w-[100%] flex justify-around">
+                  <input type="colors" className="bg-transparent w-[100px] h-[45px]  cursor-pointer select-none " value = "#1488cc"/>  
+                  <input type="colors"  value = "#2b32b2"/>  
+                </div>
 
             </div>
         </div>
