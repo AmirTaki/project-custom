e315ec227af19faed941385ebbb1dfa5ee86c6cd
amir <amirtaki623@gmail.com>
2025-10-27 20:03:58 +0330

    day 19 part 24

--- src/component/glowingCorner/cardCorner.jsx
+++ src/component/glowingCorner/cardCorner.jsx
@@ -4,9 +4,18 @@ import { useCallback, useEffect, useRef } from "react"
 const CardCorner = ({card}) => {
     const cardRef = useRef(null)
   
-    const handlerMouseMove = useCallback( () => {
+    const handlerMouseMove = useCallback( (e) => {
 
-    })
+        const node =  cardRef.current
+        if(!node) return ;
+
+        const rect = node.getBoundingClientRect()
+        const x = e.clientX - rect.left
+        const y = e.clientY - rect.top
+
+        node.style.setProperty('--x', `${x}px`)
+        node.style.setProperty('--y', `${y}px`)
+    },[])
 
     return(
         <div 
