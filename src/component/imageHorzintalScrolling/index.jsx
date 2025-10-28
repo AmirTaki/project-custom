a53d964cef1c0187792f7dce713003e506b1925f
amir <amirtaki623@gmail.com>
2025-10-28 20:56:35 +0330

    day 20 part 16

--- src/component/imageHorzintalScrolling/index.jsx
+++ src/component/imageHorzintalScrolling/index.jsx
@@ -10,7 +10,7 @@ const HorzinotalScrolling = () => {
 
             {/* gallery */}
             <div className="w-[900px] flex overflow-scroll">
-                <div className=""></div>
+                <div className="w-[100%] grid grid-cols-3 gap-[20px] p-[10px] flex-none"></div>
             </div>
 
         </div>
