dbcf788e29676c337fcadf13d42ff06a825a8053
amir <amirtaki623@gmail.com>
2025-10-28 23:01:57 +0330

    day 20 part 39

--- src/component/componentImg.jsx/index.jsx
+++ src/component/componentImg.jsx/index.jsx
@@ -1,12 +1,23 @@
 const ImgComponent = () => {
     return(
 
-    <div className="bg-white w-[90%] h-[450px] mt-20 flex  ">
-        <div className="bg-red-500 w-[100%] h-[100%] ">
+    <div className="bg-white w-[970px] max-lg:w-[650px]!  h-[500px] mt-20 flex flex-wrap  flex-col  justify-evenly   overflow-x-scroll">
+      
+        <div className="bg-red-500 w-[300px] h-[95%] mx-3 ">
         </div>
-        <div className="bg-blue-500">
+        <div className="bg-blue-500 w-[300px] h-[95%] mx-3 ">
         </div>
-        <div className="bg-green-500">
+        <div className="bg-green-500 w-[300px] h-[95%] mx-3 ">
+        </div>
+        <div className="bg-pink-500 w-[300px] h-[95%] mx-3">
+        </div>
+        <div className="bg-orange-500 w-[300px] h-[95%] mx-3">
+        </div>
+        <div className="bg-yellow-500 w-[300px] h-[95%] mx-3">
+        </div>
+        <div className="bg-gray-500 w-[300px] h-[95%] mx-3">
+        </div>
+        <div className="bg-pink-500 w-[300px] h-[95%] mx-3">
         </div>
     </div>
     )
