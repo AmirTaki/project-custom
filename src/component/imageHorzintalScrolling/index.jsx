eab0bc5a58ad15f3be6b38256e7af57d9f85ec1b
amir <amirtaki623@gmail.com>
2025-10-28 20:55:28 +0330

    day 20 part 15

--- src/component/imageHorzintalScrolling/index.jsx
+++ src/component/imageHorzintalScrolling/index.jsx
@@ -1,7 +1,19 @@
+import imgBack from "./img/back.png"
+
 const HorzinotalScrolling = () => {
     return(
         // gallery wrap
-        <div className="flex justify-items items-center m-[10%_auto]"></div>
+        <div className="flex justify-items items-center m-[10%_auto]">
+            {/* back */}
+            <img src= {imgBack} alt="" className="" />
+
+
+            {/* gallery */}
+            <div className="w-[900px] flex overflow-scroll">
+                <div className=""></div>
+            </div>
+
+        </div>
     )
 }
 
