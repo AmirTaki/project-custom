f389288624c8408f8eae9ab08e80e2c425117c2d
amir <amirtaki623@gmail.com>
2025-10-29 20:44:45 +0330

    dat 21 part 2

--- src/component/imageEditor/index.jsx
+++ src/component/imageEditor/index.jsx
@@ -1,6 +1,7 @@
 const ImageEditor = () => {
     return(
-        <div className=""></div>
+        // container
+        <div className="w-[850px] p-[30px_35px_35px] bg-white rounded-[10px] shadow-[0_10px_20px_rgba(0,0,0,.1)]"></div>
     )
 }
 
