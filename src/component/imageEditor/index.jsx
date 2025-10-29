30e00361414f7fbad00e755844cf73779f3bc520
amir <amirtaki623@gmail.com>
2025-10-29 20:51:44 +0330

    dat 21 part 6

--- src/component/imageEditor/index.jsx
+++ src/component/imageEditor/index.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 const ImageEditor = () => {
+    const [disable, setDisable] = useState(true)
     return(
         // container  // disable
         <div className="w-[850px] p-[30px_35px_35px] bg-white rounded-[10px] shadow-[0_10px_20px_rgba(0,0,0,.1)]">
@@ -6,7 +9,16 @@ const ImageEditor = () => {
                 Easy Image Editor
             </h2>
             {/* wrapper */}
-            <div className="flex my-[20px] mx-0 min-h-[335px]"></div>
+            <div className="flex my-[20px] mx-0 min-h-[335px]">
+                {/* editor-panel */}
+                <div className={`${disable ?"opacity-50 pointer-events-none" : "opacity-100 pointer-events-auto" } 
+                    py-[15px] px-[20px] w-[280px] rounded-[5px] border-1 border-[#ccc]
+                `}>
+
+                </div>
+            </div>
+
+            {/* controls  */}
         </div>
     )
 }
