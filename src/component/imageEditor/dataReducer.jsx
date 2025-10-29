f29b44fe31a4f0ab41a3308188dd516feb4e05f0
amir <amirtaki623@gmail.com>
2025-10-29 23:42:02 +0330

    day 21 part 60

---
+++ src/component/imageEditor/dataReducer.jsx
@@ -0,0 +1,31 @@
+export const initialEditor = {
+    brightness : '100',
+    saturation : '100',
+    inversion : '0',
+    grayscale : '0',
+    rotate : 0,
+    flipHorizontal : 1,
+    flipVertical : 1,
+    buttonsFilter : [
+        {
+            id : 0, 
+            name : 'Brighteness',
+            active : false
+        },
+        {
+            id : 1, 
+            name : 'Saturation',
+            active : false
+        },
+        {
+            id : 2, 
+            name : 'Inversion',
+            active : false
+        },
+        {
+            id : 3, 
+            name : 'Grayscale',
+            active : false
+        },
+    ]
+}
--- src/component/imageEditor/index.jsx
+++ src/component/imageEditor/index.jsx
@@ -16,38 +16,7 @@ const ImageEditor = () => {
     const filterSlider = useRef(null)
     const previewImg = useRef(null)
 
-    const initialEditor = {
-        brightness : '100',
-        saturation : '100',
-        inversion : '0',
-        grayscale : '0',
-        rotate : 0,
-        flipHorizontal : 1,
-        flipVertical : 1,
-        buttonsFilter : [
-            {
-                id : 0, 
-                name : 'Brighteness',
-                active : false
-            },
-            {
-                id : 1, 
-                name : 'Saturation',
-                active : false
-            },
-            {
-                id : 2, 
-                name : 'Inversion',
-                active : false
-            },
-            {
-                id : 3, 
-                name : 'Grayscale',
-                active : false
-            },
-        ]
-    }
-    
+   
     const reducerEditor = (state, action) => {
         switch(action.type){
             case "applyFilter":
