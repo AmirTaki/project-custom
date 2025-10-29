07cfc8db989b9f0c085114908a261feb48487f1c
amir <amirtaki623@gmail.com>
2025-10-29 23:03:17 +0330

    day 21 part 46

--- src/component/imageEditor/index.jsx
+++ src/component/imageEditor/index.jsx
@@ -1,13 +1,15 @@
-import { useReducer, useState } from "react";
+import { createContext, useReducer, useState } from "react";
 import img from "./img/cadillac.jpg"
 import Wrapper from "./wrapper";
 import Controls from "./controls";
-
+const EditorContect =  createContext('')
 const ImageEditor = () => {
  
     const [disable, setDisable] = useState(false)
     const [active, setActive] = useState(false)
     const [text, setText] = useState(false)
+
+    const initialEditor = {}
     
     const reducerEditor = (state, action) => {
         switch(action.type){
@@ -15,21 +17,23 @@ const ImageEditor = () => {
                 return {...state}
         }
     }
-    const [state, dispath] =  useReducer(reducerEditor, {})
+    const [state, dispath] =  useReducer(reducerEditor, initialEditor)
 
     return(
-        // container  // disable
-        <div className="w-[750px] p-[30px_35px_35px] bg-white rounded-[10px] shadow-[0_10px_20px_rgba(0,0,0,.1)] max-md:p-[25px]! max-md:w-[90%]! ">
-            <h2 className="-mt-[8px] text-[22px] font-[500] ">
-                Easy Image Editor
-            </h2>
-            {/* wrapper */}
-                <Wrapper disable = {disable} active = {active} img = {img} />
+        <EditorContect.Provider value={{}}>
+            {/* // container  // disable */}
+            <div className="w-[750px] p-[30px_35px_35px] bg-white rounded-[10px] shadow-[0_10px_20px_rgba(0,0,0,.1)] max-md:p-[25px]! max-md:w-[90%]! ">
+                <h2 className="-mt-[8px] text-[22px] font-[500] ">
+                    Easy Image Editor
+                </h2>
+                {/* wrapper */}
+                    <Wrapper disable = {disable} active = {active} img = {img} />
 
-            {/* controls  */}
-                <Controls disable = {disable}/>
+                {/* controls  */}
+                    <Controls disable = {disable}/>
 
-        </div>
+            </div>
+        </EditorContect.Provider>
     )
 }
 
