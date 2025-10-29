9d093af3541c1aa2b02c079b91ecf454b48ad4bc
amir <amirtaki623@gmail.com>
2025-10-29 21:39:40 +0330

    dat 21 part 20

--- src/component/imageEditor/index.jsx
+++ src/component/imageEditor/index.jsx
@@ -66,11 +66,11 @@ const ImageEditor = () => {
                                 return(
                                     <button
                                         key = {index}
-                                        className={`outline-0 h-[40px] text-[14px]  rounded-[3px] mb-[8px]  w-[calc(50%-4px)] 
+                                        className={`outline-0 h-[40px] text-[14px]  rounded-[3px] mb-[8px]  w-[calc(25%-3px)] 
                                             ${active? "text-white bg-[#5372F0] border-1 border-[#5372F0]" : "text-[#6c757D] bg-white border-1 border-[#aaa] hover:bg-[#f5f5f5]!"}    
                                         `}
                                     >
-                                        {item}
+                                        <i className={item}></i>
                                     </button>
                                     
                                 )
