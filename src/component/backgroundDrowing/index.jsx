a92dce54ee439a3ace326edf1540a6c76703fc8e
amir <amirtaki623@gmail.com>
2025-10-30 21:26:45 +0330

    day 22 part 56

--- src/component/backgroundDrowing/index.jsx
+++ src/component/backgroundDrowing/index.jsx
@@ -1,14 +1,20 @@
+import imgBackGround from "./img/background.png"
+import imgTransparent from "./img/transparent.png"
+
 const BackgroundChangeEffect = () => {
     return(
         //  container
         <div className="w-[100%] h-[100vh] flex justify-center items-center-safe bg-[#f3fbff]">
             {/* img-box */}
             <div 
-                style={{backgroundImage : }}
-                className="w-[60%] m-auto leading-0 "
+                style={{backgroundImage : imgBackGround}}
+                className="w-[60%] m-auto leading-0 overflow-hidden relative"
             >
+                {/* img-box */}
+                <img src={imgTransparent} alt="" className="w-[100%]" />
 
-                </div>
+                {/* img-wrap */}
+            </div>
         </div>
     )
 }
