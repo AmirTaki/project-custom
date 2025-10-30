518195a2eac8c7bd8ceb94160a90315ff302197b
amir <amirtaki623@gmail.com>
2025-10-30 21:12:00 +0330

    day 22 part 54

--- src/component/backgroundDrowing/index.jsx
+++ src/component/backgroundDrowing/index.jsx
@@ -2,7 +2,8 @@ const BackgroundChangeEffect = () => {
     return(
         //  container
         <div className="w-[100%] h-[100vh] flex justify-center items-center-safe bg-[#f3fbff]">
-            
+            {/* img-box */}
+            <div className="w-[60%] m-auto leading-0 "></div>
         </div>
     )
 }
