36a474262ba6443b1909edec78bc3507926d6343
amir <amirtaki623@gmail.com>
2025-10-30 22:04:17 +0330

    day 22 part 62

--- src/component/backgroundDrowing/index.jsx
+++ src/component/backgroundDrowing/index.jsx
@@ -13,7 +13,8 @@ const BackgroundChangeEffect = () => {
         //  container
         <div className="w-[100%] h-[100vh] flex justify-center items-center-safe bg-[#f3fbff]">
             {/* img-box */}
-            <div 
+            <div
+                ref = {imgBox} 
                 style={{backgroundImage : `url(${imgBackGround})` }}
                 className="w-[60%] m-auto leading-0 overflow-hidden relative"
             >
@@ -21,13 +22,13 @@ const BackgroundChangeEffect = () => {
                 <img src={imgTransparent} alt="" className="w-[100%]" />
 
                 {/* img-wrap */}
-                <div className="w-[20px] absolute left-0 top-0 overflow-hidden">
-                    <img src={imgOrginal} alt="" />
+                <div ref = {imgWrap} className="w-[20px] absolute left-0 top-0 overflow-hidden">
+                    <img src={imgOrginal} alt=""  ref = {originalImg}/>
                 </div>
             </div>
 
             {/* line */}
-            <span className="w-[40px]  h-[100%] cursor-ew-resize bg-blue absolute left-[20%] top-[0]">
+            <span ref = {line} className="w-[40px]  h-[100%] cursor-ew-resize bg-blue absolute left-[20%] top-[0]">
                 {/* arrow */}
                 <img src={imgArrwo} alt="" className="w-[100px] absolute top-[50%] ml-[-20px] mt-[-20px]"/>
             </span>
