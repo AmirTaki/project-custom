c5381f74e12af9371ee20ecfe859a8b68e814d98
amir <amirtaki623@gmail.com>
2025-10-31 00:27:01 +0330

    day 23 part 4

--- src/component/backgroundDrowing/index.jsx
+++ src/component/backgroundDrowing/index.jsx
@@ -8,11 +8,21 @@ const BackgroundChangeEffect = () => {
 
 
     return(
-        <div 
-           className="top-10 relative w-full max-w-4xl aspect-[1.5/1] overflow-hidden select-none cursor-ew-resize rounded-lg shadow-2xl bg-slate-900">
+        <>
+            <div 
+            className="top-10 relative w-full max-w-4xl aspect-[1.5/1] overflow-hidden select-none cursor-ew-resize rounded-lg shadow-2xl bg-slate-900">
 
-            <img src={imgTransparent} alt="" className="absolute inset-0 w-full h-full object-cover pointer-events-none" />
-        </div>
+                <img src={imgTransparent} alt="" className="absolute inset-0 w-full h-full object-cover pointer-events-none" />
+
+            </div>
+            <div 
+                className="absolute inset-0 w-full h-full overflow-hidden pointer-events-none"
+                // style = {{clipPath : `inset(0 ${100 - }% 0 0 )`}}
+            >
+                <img src="" alt="" />
+
+            </div>
+        </>
     )
 }
 export default BackgroundChangeEffect
