e30ac9c13305c7807c9bcf75352b5f1d2a331153
amir <amirtaki623@gmail.com>
2025-10-31 19:54:30 +0330

    day 23 part 25

--- src/component/backgroundDrowing/index.jsx
+++ src/component/backgroundDrowing/index.jsx
@@ -9,7 +9,16 @@ import { useCallback, useEffect, useRef, useState } from "react"
 const BackgroundChangeEffect = () => {
   
     return(
-        <div className=""></div>
+        // container
+        <div className="">
+            {/* img box */}
+            <div 
+                style={{backgroundImage : `url(${imgBackGround})`}}
+                className="top-20 relative w-[700px] h-[500px] bg-white"
+            >
+
+            </div>
+        </div>
     )
 }
 export default BackgroundChangeEffect
