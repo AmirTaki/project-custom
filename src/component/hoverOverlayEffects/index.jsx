c40d91fe8478b6c899928aa4ab6cb7cb7da4442c
amir <amirtaki623@gmail.com>
2025-10-31 21:58:46 +0330

    day 23 part 47

--- src/component/hoverOverlayEffects/index.jsx
+++ src/component/hoverOverlayEffects/index.jsx
@@ -3,6 +3,7 @@ const ImageHoverOverlayEffects = () => {
         // container
         <div className="relative w-[1100px] flex flex-wrap">
             {/* box */}
+            <div className="relative w-[275px] h-[275px] duration-500"></div>
         </div>
     )
 }
