37f4642338b28d5ca366c86fa2f0902e7a987e2b
amir <amirtaki623@gmail.com>
2025-10-31 20:01:20 +0330

    day 23 part 26

--- src/component/backgroundDrowing/index.jsx
+++ src/component/backgroundDrowing/index.jsx
@@ -10,13 +10,17 @@ const BackgroundChangeEffect = () => {
   
     return(
         // container
-        <div className="">
+        <div className="bg-amber-50 ">
             {/* img box */}
             <div 
                 style={{backgroundImage : `url(${imgBackGround})`}}
-                className="top-20 relative w-[700px] h-[500px] bg-white"
+                className="top-20 relative w-[700px] h-[100%] max-w-4xl aspect-[1.5/1] overflow-hidden select-none cursor-ew-resize rounded-2xl bg-white shadow-2xl"
             >
-
+                {/* img */}
+                <img src={imgTransparent} alt=""
+                    draggable = {false}
+                    className="absolute inset-0 w-full h-full object-cover pointer-events-none"
+                />
             </div>
         </div>
     )
