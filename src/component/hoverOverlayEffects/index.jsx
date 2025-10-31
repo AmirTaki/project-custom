76ee104e246adcdb4ae0d83c56d535c96e3b9669
amir <amirtaki623@gmail.com>
2025-10-31 22:58:21 +0330

    day 23 part 62

--- src/component/hoverOverlayEffects/index.jsx
+++ src/component/hoverOverlayEffects/index.jsx
@@ -4,7 +4,7 @@ import { DataBoxImage } from "./dataBoxImage";
 const ImageHoverOverlayEffects = () => {
     return(
         // container
-        <div className="relative w-[90%] flex flex-wrap bg-amber-100">
+        <div className="relative w-[90%] flex flex-wrap items-center justify-center gap-1.5 ">
             {DataBoxImage.map((box) => {
                 return(
                     <BoxImage key = {box.id} box = {box}/>
