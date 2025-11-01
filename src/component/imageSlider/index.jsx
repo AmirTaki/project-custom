61feb33480111e2dcf3173ef997078dc74911a9c
amir <amirtaki623@gmail.com>
2025-11-01 19:44:50 +0330

    day 24 part 6

--- src/component/imageSlider/index.jsx
+++ src/component/imageSlider/index.jsx
@@ -1,16 +1,19 @@
 import img1 from "./image/img1.jpg"
 const ImageSlider = () => {
     return(
-    <div className="w-full h-full bg-red-200 overflow-hidden">
+    <div className="w-full h-[100vh] bg-red-200 overflow-hidden">
         
         {/*slider  overflow-hidden*/}
-        <div className="h-[100vh] w-[100vw] relative overflow-hidden ">
+        <div className="h-[100vh] w-[100vw] relative overflow-hidden -mt-[50px]">
             {/* list */}
             <div className="">
                 {/* item */}
                 <div className="w-full h-full absolute inset-0">
                     {/* img */}
                     <img src = {img1} alt="" className="w-full h-full object-cover" />
+                    {/* content */}
+                    <div className="absolute top-[20%] w-[1140px] max-w-[80%] left-[50%] h-10 bg-amber-600 transform -translate-x-1/2"></div>
+
                 </div>
             </div>
         </div>
