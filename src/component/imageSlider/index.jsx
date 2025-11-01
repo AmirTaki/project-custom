d72816a804638d8611151de657d0a26c8acbc7cb
amir <amirtaki623@gmail.com>
2025-11-01 19:36:38 +0330

    day 24 part 4

--- src/component/imageSlider/index.jsx
+++ src/component/imageSlider/index.jsx
@@ -3,8 +3,13 @@ const ImageSlider = () => {
     <div className="w-full h-[600px] bg-red-200">
         
         {/*slider  overflow-hidden*/}
-        <div className="h-[100vh] w-[100vw] relative 
-        "></div>
+        <div className="h-[100vh] w-[100vw] relative ">
+            {/* list */}
+            <div className="">
+                {/* item */}
+                <div className="w-full h-full absolute inset-0"></div>
+            </div>
+        </div>
     </div>
     )
 }
