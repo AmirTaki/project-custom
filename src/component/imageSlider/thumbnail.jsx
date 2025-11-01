d2bed6f9e6eb768f3c0dde87979811b61bc8b94b
amir <amirtaki623@gmail.com>
2025-11-01 20:40:50 +0330

    day 24 part 26

--- src/component/imageSlider/thumbnail.jsx
+++ src/component/imageSlider/thumbnail.jsx
@@ -2,7 +2,8 @@ const Thumbnail = () => {
     return(
         // thumbnail
         <div className="absolute bottom-[50px] left-[50%] w-max z-[100] flex gap-[20px]">
-
+            {/* item */}
+            <div className="w-[150px] h-[220px] shrink-0 reative"></div>
         </div>
     )
 }
