270505ade0007b38e22d02a8560f86dbc3e1ee77
amir <amirtaki623@gmail.com>
2025-11-01 21:10:48 +0330

    day 24 part 36

--- src/component/imageSlider/itemImage.jsx
+++ src/component/imageSlider/itemImage.jsx
@@ -7,17 +7,19 @@ const ItemImage = ({item}) => {
             {/* content */}
             <div className="absolute top-[20%] w-[1140px] max-w-[80%] left-[50%]  transform -translate-x-1/2 pr-[30%] max-md:pr-[0]! box-border text-white text-shadow-[0_5px_10px_#0004] ">
                 {/* title */}
-                <div className="text-[5em] font-bold leading-[1.3em] max-md:text-[50px]! sliderAnimation ">
+                <div className="text-[5em] font-bold leading-[1.3em] max-md:text-[50px]! sliderAnimation titleContent">
                     {item.title}
                 </div>
                 {/* type */}
-                <div className="text-[5em] font-bold leading-[1.3em] text-[#14ff72cb] sliderAnimation ">{item.type}</div>
+                <div className="text-[5em] font-bold leading-[1.3em] text-[#14ff72cb] sliderAnimation  typeContent">
+                    {item.type}
+                </div>
                 {/* discription */}
-                <div className="sliderAnimation ">
+                <div className="sliderAnimation descriptionContent ">
                     {item.description}
                 </div>
                 {/* button */}
-                <div className="grid grid-cols-[130px_130px] grid-rows-[40px] gap-[5px] mt-[20px] sliderAnimation ">
+                <div className="grid grid-cols-[130px_130px] grid-rows-[40px] gap-[5px] mt-[20px] sliderAnimation  buttonsContent">
                     <button 
                         className="border-0  text-black bg-[#eee] font-[Poppins]! font-[500]! cursor-pointer transition-all duration-500 tracking-[2px] hover:tracking-[3px] "
                     >SEE MORE</button>
