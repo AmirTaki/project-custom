05ebdb92535d18a2b6a4a2d821daedc84636dc15
amir <amirtaki623@gmail.com>
2025-11-01 19:35:33 +0330

    day 24 part 3

--- src/component/imageSlider/index.jsx
+++ src/component/imageSlider/index.jsx
@@ -1,7 +1,11 @@
 const ImageSlider = () => {
     return(
-        // slider
-        <div className=""></div>
+    <div className="w-full h-[600px] bg-red-200">
+        
+        {/*slider  overflow-hidden*/}
+        <div className="h-[100vh] w-[100vw] relative 
+        "></div>
+    </div>
     )
 }
 
