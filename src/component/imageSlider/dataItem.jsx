5708e001d05e8a9fdb9a602bd633a3df1eca6ffd
amir <amirtaki623@gmail.com>
2025-11-01 20:26:34 +0330

    day 24 part 16

--- src/component/imageSlider/dataItem.jsx
+++ src/component/imageSlider/dataItem.jsx
@@ -1,3 +1,8 @@
+import img1 from "./image/img1.jpg"
+import img2 from "./image/img2.jpg"
+import img3 from "./image/img3.jpg"
+import img4 from "./image/img4.jpg"
+
 export const DataImage = [
     {
         id : 0,
@@ -5,5 +10,26 @@ export const DataImage = [
         type : '',
         description : '',
         image : ''
-    }
+    },
+    {
+        id : 1,
+        title : '',
+        type : '',
+        description : '',
+        image : ''
+    },
+    {
+        id : 2,
+        title : '',
+        type : '',
+        description : '',
+        image : ''
+    },
+    {
+        id : 3,
+        title : '',
+        type : '',
+        description : '',
+        image : ''
+    },
 ]
