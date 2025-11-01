42442f4a799832a46d6f0998a0cf02c0b732181c
amir <amirtaki623@gmail.com>
2025-11-01 23:34:31 +0330

    day 24 part 54

--- src/component/imageSlider/thumbnail.jsx
+++ src/component/imageSlider/thumbnail.jsx
@@ -6,18 +6,18 @@ const Thumbnail = () => {
     return(
         // thumbnail
         <div className="absolute bottom-[50px] left-[50%] w-max z-[100] flex gap-[20px]">
-            <div  className="w-[150px] h-[220px] shrink-0 reative">
-                <img src={item.image[item.value].image} 
-                    className={`${item.flag ? 'block' : 'hidden'} w-full h-full object-cover rounded-[20px] shadow-[5px_0_15px_rgba(0,0,0,.3)]`}
+            {/* <div  className="w-[150px] h-[220px] shrink-0 reative">
+                <img src={state.image[state.value].image} 
+                    className={`${state.flag ? 'block' : 'hidden'} w-full h-full object-cover rounded-[20px] shadow-[5px_0_15px_rgba(0,0,0,.3)]`}
                 alt="" />
-            </div>
+            </div> */}
 
             {/* item */}
             {state.image.map((item) => {
                 return(
                     <div key = {item.id} className="w-[150px] h-[220px] shrink-0 reative">
                         <img src={item.image} 
-                            className={`${item.flag ? 'hidden' : 'block'} w-full h-full object-cover rounded-[20px] shadow-[5px_0_15px_rgba(0,0,0,.3)]`}
+                            className={`w-full h-full object-cover rounded-[20px] shadow-[5px_0_15px_rgba(0,0,0,.3)]`}
                         alt="" />
                     </div>
                 )
