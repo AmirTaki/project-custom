fc128f8b1b2261faceb938b6b08cd1622436d16d
amir <amirtaki623@gmail.com>
2025-11-01 20:29:11 +0330

    day 24 part 18

--- src/component/imageSlider/dataItem.jsx
+++ src/component/imageSlider/dataItem.jsx
@@ -7,29 +7,29 @@ export const DataImage = [
     {
         id : 0,
         title : 'MAGIC SLIDER',
-        type : '',
-        description : '                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur velit provident pariatur optio sapiente et mollitia, in ut odit placeat accusantium officia, ipsa quae, fugit sequi expedita dignissimos cum doloremque.',
+        type : 'FLOWER',
+        description : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur velit provident pariatur optio sapiente et mollitia, in ut odit placeat accusantium officia, ipsa quae, fugit sequi expedita dignissimos cum doloremque.',
         image : img1
     },
     {
         id : 1,
-        title : '',
-        type : '',
-        description : '                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur velit provident pariatur optio sapiente et mollitia, in ut odit placeat accusantium officia, ipsa quae, fugit sequi expedita dignissimos cum doloremque.',
+        title : 'MAGIC SLIDER',
+        type : 'NATURE',
+        description : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur velit provident pariatur optio sapiente et mollitia, in ut odit placeat accusantium officia, ipsa quae, fugit sequi expedita dignissimos cum doloremque.',
         image : img2
     },
     {
         id : 2,
-        title : '',
-        type : '',
-        description : '                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur velit provident pariatur optio sapiente et mollitia, in ut odit placeat accusantium officia, ipsa quae, fugit sequi expedita dignissimos cum doloremque.',
+        title : 'MAGIC SLIDER',
+        type : 'NATURE',
+        description : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur velit provident pariatur optio sapiente et mollitia, in ut odit placeat accusantium officia, ipsa quae, fugit sequi expedita dignissimos cum doloremque.',
         image : img3
     },
     {
         id : 3,
-        title : '',
-        type : '',
-        description : '                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur velit provident pariatur optio sapiente et mollitia, in ut odit placeat accusantium officia, ipsa quae, fugit sequi expedita dignissimos cum doloremque.',
+        title : 'MAGIC SLIDER',
+        type : 'PLANT',
+        description : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur velit provident pariatur optio sapiente et mollitia, in ut odit placeat accusantium officia, ipsa quae, fugit sequi expedita dignissimos cum doloremque.',
         image : img4
     },
 ]
