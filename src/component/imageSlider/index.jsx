7a9d4e4f5a55c61a8dcfa1397e7bfb1d2cf47e56
amir <amirtaki623@gmail.com>
2025-11-01 20:24:21 +0330

    day 24 part 14

--- src/component/imageSlider/index.jsx
+++ src/component/imageSlider/index.jsx
@@ -8,28 +8,7 @@ const ImageSlider = () => {
             {/* list */}
             <div className="">
                 {/* item */}
-                <div className="w-full h-full absolute inset-0">
-                    {/* img */}
-                    <img src = {img1} alt="" className="w-full h-full object-cover" />
-                    {/* content */}
-                    <div className="absolute top-[20%] w-[1140px] max-w-[80%] left-[50%]  transform -translate-x-1/2 pr-[30%] max-md:pr-[0]! box-border text-white text-shadow-[0_5px_10px_#0004] ">
-                        {/* title */}
-                        <div className="text-[5em] font-bold leading-[1.3em] max-md:text-[50px]!">MAGIC SLIDER</div>
-                        {/* type */}
-                        <div className="text-[5em] font-bold leading-[1.3em] text-[#14ff72cb]">FLOWER</div>
-                        {/* discription */}
-                        <div className="">
-                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur velit provident pariatur optio sapiente et mollitia, in ut odit placeat accusantium officia, ipsa quae, fugit sequi expedita dignissimos cum doloremque.
-                        </div>
-                        {/* button */}
-                        <div className="grid grid-cols-[130px_130px] grid-rows-[40px] gap-[5px] mt-[20px]">
-                            <button 
-                                className="border-0  text-black bg-[#eee] font-[Poppins]! font-[500]! cursor-pointer transition-all duration-500 tracking-[2px] hover:tracking-[3px] "
-                            >SEE MORE</button>
-                        </div>
-                    </div>
-
-                </div>
+      
             </div>
         </div>
     </div>
