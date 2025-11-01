85240d57b1b306a7444e680bd5c5788f9107927e
amir <amirtaki623@gmail.com>
2025-11-01 20:37:39 +0330

    day 24 part 25

--- src/component/imageSlider/thumbnail.jsx
+++ src/component/imageSlider/thumbnail.jsx
@@ -1,6 +1,9 @@
 const Thumbnail = () => {
     return(
-        <div className=""></div>
+        // thumbnail
+        <div className="absolute bottom-[50px] left-[50%] w-max z-[100] flex gap-[20px]">
+
+        </div>
     )
 }
 export default Thumbnail;
