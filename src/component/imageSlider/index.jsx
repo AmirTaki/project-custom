2609ec9c6f3c3fe587da6168e9ea308a3ffc5a07
amir <amirtaki623@gmail.com>
2025-11-01 20:30:26 +0330

    day 24 part 19

--- src/component/imageSlider/index.jsx
+++ src/component/imageSlider/index.jsx
@@ -1,3 +1,6 @@
+import { DataImage } from "./dataItem";
+import ItemImage from "./itemImage";
+
 const ImageSlider = () => {
     return(
     <div className="w-full h-[100vh] bg-red-200 overflow-hidden">
@@ -6,6 +9,11 @@ const ImageSlider = () => {
         <div className="h-[100vh] w-[100vw] relative overflow-hidden -mt-[50px]">
             {/* list */}
             <div className="">
+                {DataImage.map((item) => {
+                    return(
+                        <ItemImage key = {item.id} item = {item} />
+                    )
+                })}
                 {/* item */}
       
             </div>
