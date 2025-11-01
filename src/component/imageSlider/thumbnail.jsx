c4c6063dd9ec133d7e881ce582b1a5841d1af965
amir <amirtaki623@gmail.com>
2025-11-01 22:31:54 +0330

    day 24 part 43

--- src/component/imageSlider/thumbnail.jsx
+++ src/component/imageSlider/thumbnail.jsx
@@ -1,11 +1,13 @@
-import { DataImage } from "./dataItem";
+import { useContext } from "react";
+import { imageSliderContext } from ".";
 
 const Thumbnail = () => {
+    const {state, dispatch} = useContext(imageSliderContext)
     return(
         // thumbnail
         <div className="absolute bottom-[50px] left-[50%] w-max z-[100] flex gap-[20px]">
             {/* item */}
-            {DataImage.map((item) => {
+            {state.image.map((item) => {
                 return(
                     <div key = {item.id} className="w-[150px] h-[220px] shrink-0 reative">
                         <img src={item.image} 
