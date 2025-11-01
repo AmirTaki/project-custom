7f25f5a02428b8a2f326f600408a5cf06499389b
amir <amirtaki623@gmail.com>
2025-11-01 20:56:35 +0330

    day 24 part 32

--- src/component/imageSlider/buttonPrevArrows.jsx
+++ src/component/imageSlider/buttonPrevArrows.jsx
@@ -4,11 +4,16 @@ const NextPrevButton = () => {
         <div 
             className="absolute top-[80%] right-[52%] z-[100] w-[300px] max-w-[30%] flex gap-[10px] items-center"
         >
-            <button className="w-[40px] h-[40px] rounded-[50%] text-white bg-[#14ff72cb] border-0 font-[monospace] font-bold
-                transition-all duration-500 cursor-pointer hover:bg-white hover:text-[#000]
-            " >
-                {}
-            </button>
+            {bt.map((b, i) => {
+                return(
+                    <button key = {i} 
+                    className="w-[40px] h-[40px] rounded-[50%] text-white bg-[#14ff72cb] border-0 font-[monospace] font-bold
+                        transition-all duration-500 cursor-pointer hover:bg-white! hover:text-[#000]
+                    ">
+                        {b}
+                    </button>
+                )
+            })}
 
         </div>
     )
