7f34d2fd07f5aeb5cffae33750667f860059482f
amir <amirtaki623@gmail.com>
2025-11-01 21:13:08 +0330

    day 24 part 37

--- src/component/imageSlider/index.jsx
+++ src/component/imageSlider/index.jsx
@@ -1,21 +1,27 @@
+import { createContext } from "react";
 import NextPrevButton from "./buttonPrevArrows";
 import ListSlider from "./listSlider";
 import Thumbnail from "./thumbnail";
 
+export const imageSliderContect =  createContext()
 const ImageSlider = () => {
     return(
     <div className="w-full h-[100vh] bg-red-200 overflow-hidden">
         
         {/*slider  overflow-hidden*/}
         <div className="h-[100vh] w-[100vw] relative overflow-hidden -mt-[10px]">
-            {/* list */}
-            <ListSlider />
+            <imageSliderContect.Provider value ={{}}>
 
-            {/* thumbnail */}
-            <Thumbnail />
+                {/* list */}
+                <ListSlider />
 
-            {/* next prev Arrows */}
-            <NextPrevButton />
+                {/* thumbnail */}
+                <Thumbnail />
+
+                {/* next prev Arrows */}
+                <NextPrevButton />
+                
+            </imageSliderContect.Provider>
         </div>
     </div>
     )
