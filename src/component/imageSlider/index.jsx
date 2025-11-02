ed3b42ecc69799dfcb5556da292a547f0c53ec26
amir <amirtaki623@gmail.com>
2025-11-02 19:19:37 +0330

    day 25 part 40

--- src/component/imageSlider/index.jsx
+++ src/component/imageSlider/index.jsx
@@ -1,21 +1,36 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useReducer, useRef, useState } from "react";
 import "./styles.css"
 
 
 const ImageSlider = () => {
+
+    const reducer = (state, action) => {
+        switch(action.type){
+            case "" :
+                return {...state}
+        }
+    }
+    const [state, dispatch]  = useReducer(reducer, {
+
+    })
     const [move, setMove] =  useState(false)
+    const [value, setValue] = useState({})
     const slider = useRef(null)
     const Next = () => {
         const slide =  slider.current
         if(slide){
             const slideNext  = slide.querySelectorAll('.item')
             slide.appendChild(slideNext[0])
+            setValue({...value, 0 : true})
+            
             setMove(true)
         }   
     }
     useEffect(() => {
        const time = setInterval(() => {
             setMove(false)
+            setValue({...value, 0 : false})
+
             
         }, 500);
         return() => [
@@ -27,10 +42,10 @@ const ImageSlider = () => {
         {/* // slider */}
         <div className={`flex flex-row gap-2 duration-500 ${move? "translate-x-10" : "translate-x-0"}`} ref = {slider}>
             {/* item */}
-            <div className = {`bg-white w-30 h-30 flex justify-center items-center text-6xl item `}>1</div>
-            <div className = {`bg-white w-30 h-30 flex justify-center items-center text-6xl item `}>2</div>
-            <div className = {`bg-white w-30 h-30 flex justify-center items-center text-6xl item `}>3</div>
-            <div className = {`bg-white w-30 h-30 flex justify-center items-center text-6xl item `}>4</div>
+            <div className = {`bg-white w-30 h-30 flex justify-center items-center text-6xl item duration-500 ${value[0] ? "scale-200" : "scale-100"}`}>1</div>
+            <div className = {`bg-white w-30 h-30 flex justify-center items-center text-6xl item duration-500 ${value[1] ? "scale-200" : "scale-100"}`}>2</div>
+            <div className = {`bg-white w-30 h-30 flex justify-center items-center text-6xl item duration-500 ${value[2] ? "scale-200" : "scale-100"}`}>3</div>
+            <div className = {`bg-white w-30 h-30 flex justify-center items-center text-6xl item duration-500 ${value[3] ? "scale-200" : "scale-100"}`}>4</div>
         </div>
 
 
