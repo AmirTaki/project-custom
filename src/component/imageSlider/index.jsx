24709ab441182351cdb362b65027e8f776978e72
amir <amirtaki623@gmail.com>
2025-11-02 23:05:26 +0330

    day 25 part 57

--- src/component/imageSlider/index.jsx
+++ src/component/imageSlider/index.jsx
@@ -20,7 +20,11 @@ const ImageSlider = () => {
                 thumbnail.current.appendChild(thumbnailItems[0])
                 slider.current.classList.add('next')
                 return {...state};
+                
             case "prev" : 
+                sliderList.current.prepend(sliderItems[sliderItems.length - 1])
+                thumbnail.current.prepend(thumbnailItems[thumbnailItems.length - 1])
+                slider.current.classList.add('prev')
                 return {...state}
         }
     }
