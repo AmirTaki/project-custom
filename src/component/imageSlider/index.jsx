e62852175e1dc26ff57e9262a46c06d1085b94bf
amir <amirtaki623@gmail.com>
2025-11-02 19:58:02 +0330

    day 25 part 44

--- src/component/imageSlider/index.jsx
+++ src/component/imageSlider/index.jsx
@@ -8,20 +8,20 @@ const ImageSlider = () => {
     const reducer = (state, action) => {
         switch(action.type){
             case "next" :
-                
-                const shift = state.item.shift()
-                state.item.push(shift)
+                // const shift = state.item.shift()
+                state.item.push(state.item.shift())
+                return {...state, moveNext : true, }
 
-                const newItem = state.item.map((item) => {({...item, scale : item.id == shift.id ? true : false})})
-                return {...state, move : true, item : newItem}
-
+            case "prev":
+                state.item.unshift(state.item.pop())
+                return  {...state, movePrev : true}
             case "moveFalse" :
-                const newState =  state.item.map((item) => {({...item, scale : false})}) 
-                return {...state, move : false, item : newState}
+                return {...state, moveNext : false, movePrev:false }
         }
     }
     const [state, dispatch]  = useReducer(reducer, {
-        move : false,
+        moveNext : false,
+        movePrev : false,
         item : [
             {id : 0, scale : false},
             {id : 1, scale : false},
@@ -44,7 +44,7 @@ const ImageSlider = () => {
     return(
         <>
         {/* // slider */}
-        <div className={`flex flex-row gap-2 duration-500 ${state.move ? "translate-x-10" : "translate-x-0"}`} ref = {slider}>
+        <div className={`flex flex-row gap-2 duration-500 ${state.moveNext ? "translate-x-10" : "translate-x-0"} ${state.movePrev ?  '-translate-x-10!': 'translate-x-0' }`} ref = {slider}>
             {/* item */}
             {state.item.map((item) => (
                 <div key = {item.id} className = {`bg-white w-30 h-30 flex justify-center items-center text-6xl item duration-500 ${item.scale ? "scale-200" : "scale-100"}` }>{item.id}</div>
@@ -53,7 +53,8 @@ const ImageSlider = () => {
 
 
         <button onClick = {() => {dispatch ({type : 'next'})}} className="bg-white mt-10 w-10 cursor-pointer">next</button>
-        <button></button>
+        <button onClick = {() => {dispatch ({type : 'prev'})}} className="bg-white mt-10 w-10 cursor-pointer">prev</button>
+        
         </>
 
 
