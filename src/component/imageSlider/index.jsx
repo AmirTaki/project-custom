d1e0b2c5f6a1bd91b892f7ed2efd700921bf460e
amir <amirtaki623@gmail.com>
2025-11-02 03:58:41 +0330

    day 25 part 17

--- src/component/imageSlider/index.jsx
+++ src/component/imageSlider/index.jsx
@@ -6,15 +6,19 @@ const ImageSlider = () => {
 
     return(
     //    slider
-    <div className="w-full h-[100vh] bg-red-200 overflow-hidden ">
+    <div className="w-[100%]  h-[50vh] bg-red-200 relative ">
         {/* item */}
         {DataImage.image.map((item) => {
             return(
-                <div style={{width : `%`}} key = {item.id}>
-                    <img src={item.image} alt="" />
+                <div 
+                    className="h-full w-full absolute"
+                    style={{width : `${item.width}%`, zIndex : item.zidex }} key = {item.id}>
+                    <img src={item.image} alt="" className="w-full h-full"/>
                 </div>      
             )
         }) }
+
+        <div className="bg-blue-500 z-[1000]! absolute -bottom-10 w-20 cursor-pointer">click</div>
     </div>
     )
 }
