6adfe12aa41da66fc2108f6e64ca18d192a8ca71
amir <amirtaki623@gmail.com>
2025-11-02 03:40:14 +0330

    day 25 part 15

--- src/component/imageSlider/thumbnail.jsx
+++ src/component/imageSlider/thumbnail.jsx
@@ -4,16 +4,16 @@ import './styles.css'
 
 
 const Thumbnail = () => {
-    const {DataImage} = useContext(imageSliderContext)
+    const {DataImage, thumbnailRef} = useContext(imageSliderContext)
     return(
         // thumbnail
-        <div className="absolute bottom-[50px] left-[50%] w-max z-[100] flex gap-[20px] ">
+        <div className="absolute bottom-[50px] left-[50%] w-max z-[100] flex gap-[20px] " ref = {thumbnailRef}>
           
 
             {/* item */}
             {DataImage.image.map((item) => {
                 return(
-                    <div key = {item.id} className="w-[150px] h-[220px] shrink-0 reative">
+                    <div key = {item.id} className="w-[150px] h-[220px] shrink-0 reative item">
                         <img src={item.image} 
                             className={`w-full h-full object-cover rounded-[20px] shadow-[5px_0_15px_rgba(0,0,0,.3)]`}
                         alt="" />
