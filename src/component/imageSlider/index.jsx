02d32801de6add9f213d86d665697722675d709e
amir <amirtaki623@gmail.com>
2025-11-02 04:06:07 +0330

    day 25 part 18

--- src/component/imageSlider/index.jsx
+++ src/component/imageSlider/index.jsx
@@ -1,14 +1,22 @@
+import { useEffect, useReducer, useState } from "react";
 import { DataImage } from "./dataItem";
 
 
 
 const ImageSlider = () => {
+    const reducer = (state, action)=>{
+        switch(action.type){
+            case "":
+                return {...state}
+        }
+    }
+    const [Data, dispatch] = useReducer(reducer, DataImage)
 
     return(
     //    slider
     <div className="w-[100%]  h-[50vh] bg-red-200 relative ">
         {/* item */}
-        {DataImage.image.map((item) => {
+        {Data.image.map((item) => {
             return(
                 <div 
                     className="h-full w-full absolute"
@@ -18,7 +26,9 @@ const ImageSlider = () => {
             )
         }) }
 
-        <div className="bg-blue-500 z-[1000]! absolute -bottom-10 w-20 cursor-pointer">click</div>
+        <div 
+            onClick={() => {handlerClick()}}
+        className="bg-blue-500 z-[1000]! absolute -bottom-10 w-20 cursor-pointer">click</div>
     </div>
     )
 }
