462f4266ca0050043a73d9cb2a4b8fe6b93bf537
amir <amirtaki623@gmail.com>
2025-11-02 19:31:19 +0330

    day 25 part 41

--- src/component/imageSlider/index.jsx
+++ src/component/imageSlider/index.jsx
@@ -1,13 +1,21 @@
 import { useEffect, useReducer, useRef, useState } from "react";
-import "./styles.css"
+// import "./styles.css"
 
 
 const ImageSlider = () => {
+    const slider = useRef(null)
 
     const reducer = (state, action) => {
         switch(action.type){
             case "next" :
-                return {...state}
+                const slide =  slider.current
+                if(slide){
+                    const slideNext  = slide.querySelectorAll('.item')
+                    slide.appendChild(slideNext[0])
+                }
+                return {...state, move : true}
+            case "moveFalse" :
+                return {...state, move : false}
         }
     }
     const [state, dispatch]  = useReducer(reducer, {
@@ -16,43 +24,30 @@ const ImageSlider = () => {
             {id : 0},{id : 1},{id : 2},{id : 3},
         ]
     })  
-    const [move, setMove] =  useState(false)
-    
-    const slider = useRef(null)
-    const Next = () => {
-        const slide =  slider.current
-        if(slide){
-            const slideNext  = slide.querySelectorAll('.item')
-            slide.appendChild(slideNext[0])
-            
-            setMove(true)
-        }   
-    }
+  
     useEffect(() => {
        const time = setInterval(() => {
-            setMove(false)
-
-            
+            dispatch({type : 'moveFalse'})
+        
         }, 500);
         return() => [
             clearInterval(time)
         ]
-    }, [move])
+    }, [state.item])
 
 
     return(
         <>
         {/* // slider */}
-        <div className={`flex flex-row gap-2 duration-500 ${move? "translate-x-10" : "translate-x-0"}`} ref = {slider}>
+        <div className={`flex flex-row gap-2 duration-500 ${state.move ? "translate-x-10" : "translate-x-0"}`} ref = {slider}>
             {/* item */}
-            <div className = {`bg-white w-30 h-30 flex justify-center items-center text-6xl item duration-500 ${move ? "scale-200" : "scale-100"}`}>1</div>
-            <div className = {`bg-white w-30 h-30 flex justify-center items-center text-6xl item duration-500 ${move ? "scale-200" : "scale-100"}`}>2</div>
-            <div className = {`bg-white w-30 h-30 flex justify-center items-center text-6xl item duration-500 ${move ? "scale-200" : "scale-100"}`}>3</div>
-            <div className = {`bg-white w-30 h-30 flex justify-center items-center text-6xl item duration-500 ${move ? "scale-200" : "scale-100"}`}>4</div>
+            {state.item.map((item) => (
+                <div key = {item.id} className = {`bg-white w-30 h-30 flex justify-center items-center text-6xl item duration-500 `}>{item.id}</div>
+            ))}
         </div>
 
 
-        <button onClick = {Next} className="bg-white mt-10 w-10 cursor-pointer">next</button>
+        <button onClick = {() => {dispatch ({type : 'next'})}} className="bg-white mt-10 w-10 cursor-pointer">next</button>
         <button></button>
         </>
 
