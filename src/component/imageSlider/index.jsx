22f38a1fe2414b9099608509888fe753e680f856
amir <amirtaki623@gmail.com>
2025-11-02 18:49:55 +0330

    day 25 part 38

--- src/component/imageSlider/index.jsx
+++ src/component/imageSlider/index.jsx
@@ -1,6 +1,25 @@
+import { useRef } from "react";
+import "./styles.css"
+
 const ImageSlider = () => {
+    const slider = useRef(null)
     return(
-        <div className=""></div>
+        <>
+        {/* // slider */}
+        <div className="flex flex-row gap-2 " ref = {slider}>
+            {/* item */}
+            <div className="bg-white w-30 h-30 flex justify-center items-center text-6xl item">1</div>
+            <div className="bg-white w-30 h-30 flex justify-center items-center text-6xl item">2</div>
+            <div className="bg-white w-30 h-30 flex justify-center items-center text-6xl item">3</div>
+            <div className="bg-white w-30 h-30 flex justify-center items-center text-6xl item">4</div>
+        </div>
+
+
+        <button className="bg-white mt-10 w-10 cursor-pointer">next</button>
+        <button></button>
+        </>
+
+
     )
 }
 
