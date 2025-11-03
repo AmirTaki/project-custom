d2b3b2ae088230d9b957c10c9a504c6b26ed6430
amir <amirtaki623@gmail.com>
2025-11-03 22:10:47 +0330

    day 26 part 43

--- src/component/cardComponent/index.jsx
+++ src/component/cardComponent/index.jsx
@@ -1,6 +1,6 @@
 const imageCardHover = () => {
     // container
-    <div className=""></div>
+    <div className="min-w-[400px] h-[400px] flex items-end "></div>
 }
 
 export default imageCardHover;
