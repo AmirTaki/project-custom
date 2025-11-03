57ec7c0821c5ba6b6157b3f1ba101e63feab427e
amir <amirtaki623@gmail.com>
2025-11-03 22:18:07 +0330

    day 26 part 45

--- src/component/cardComponent/index.jsx
+++ src/component/cardComponent/index.jsx
@@ -1,9 +1,13 @@
 const ImageCardHover = () => {
     return(
         // container
-        <div className="min-w-[400px] h-[400px] flex items-end ">
+        <div className="min-w-[400px] h-[400px] flex items-end bg-amber-500">
             {/* icon-image */}
             <div className="relative bg-white rounded-[50%] h-[70px] w-[70px] my-0 mx-[5px] shadow-[0_5px_10px_rgba(0,0,0,.25)]">
+                {/* icon */}
+                <div className="">
+                    <img src="" alt="" />
+                </div>
             </div>
         </div>
     )
