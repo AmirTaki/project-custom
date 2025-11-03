656db2b2027c8f5ac9e2fc435bc5357d9bdb35ec
amir <amirtaki623@gmail.com>
2025-11-03 23:10:45 +0330

    day 26 part 62

---
+++ src/component/cardComponent/imageData.jsx
@@ -0,0 +1,13 @@
+import img1 from "./image/image1.avif"
+import img2 from "./image/image2.avif"
+import img3 from "./image/image3.avif"
+import img4 from "./image/image4.avif"
+import img5 from "./image/image5.avif"
+import img6 from "./image/image6.avif"
+
+export const ImageData = [
+    {
+        id : 0,
+        image : 
+    }
+]
