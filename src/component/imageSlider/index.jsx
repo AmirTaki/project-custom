e73a7aa7e0587034c4cebd5bd6b2c47f4acef3f6
amir <amirtaki623@gmail.com>
2025-11-03 19:22:35 +0330

    day 26 part 35

--- src/component/imageSlider/index.jsx
+++ src/component/imageSlider/index.jsx
@@ -118,11 +118,17 @@ const ImageSlider = () => {
             </div>
 
             {/* next prev arrows */}
-            <div className="nextPrevArrows">
+            <div className="nextPrevArrows
+                absolute top-[80%] right-[52%] z-[100] w-[300px] max-w-[30%] flex gap-[10px] items-center
+            ">
                 {state.buttons.map((item) => {
                     return(
                         <button 
-                            className={`${item.name}`} key = {item.id}
+                            className={`${item.name}
+                                w-[40px] h-[40px] rounded-[50%] bg-[#14ff72cb] border-0 text-white font-[monospace]
+                                font-bold duration-500 cursor-pointer hover:bg-white! hover:text-black    
+                            `} 
+                            key = {item.id}
                             onClick={() => {dispatch({type : `${item.name}`})}}
                         >
                             {item.symbol}
