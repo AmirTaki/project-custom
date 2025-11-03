ed4dee93f5c8e5fcbad6163df06cec799b037b02
amir <amirtaki623@gmail.com>
2025-11-03 23:12:02 +0330

    day 26 part 63

--- src/component/cardComponent/imageData.jsx
+++ src/component/cardComponent/imageData.jsx
@@ -8,6 +8,38 @@ import img6 from "./image/image6.avif"
 export const ImageData = [
     {
         id : 0,
-        image : 
-    }
+        image : img1,
+        name : "David Carls",
+        job : 'Designer || Developer'
+    },
+    {
+        id : 1,
+        image : img2,
+        name : "David Carls",
+        job : 'Designer || Developer'
+    },
+    {
+        id : 2,
+        image : img3,
+        name : "David Carls",
+        job : 'Designer || Developer'
+    },
+    {
+        id : 3,
+        image : img4,
+        name : "David Carls",
+        job : 'Designer || Developer'
+    },
+    {
+        id : 4,
+        image : img5,
+        name : "David Carls",
+        job : 'Designer || Developer'
+    },
+    {
+        id : 5,
+        image : img6,
+        name : "David Carls",
+        job : 'Designer || Developer'
+    },
 ]
