673a32f921ac07387636be719f033a1846437f5f
amir <amirtaki623@gmail.com>
2025-11-04 00:06:02 +0330

    day 27 part

--- src/component/cardComponent/iconImage.jsx
+++ src/component/cardComponent/iconImage.jsx
@@ -20,7 +20,11 @@ const IconImage = ({item}) => {
                         />
                     </div>
                     {/* content */}
-                    <div className="absolute  w-full bottom-[-10px] left-0 py-0 px-[10px] ">
+                    <div className="absolute  w-full bottom-[-10px] left-0 py-0 px-[10px]
+                        before:content before:absolute before:h-[20px] before:w-[20px] before:bg-white 
+                        before:left-[50%] before:bottom-[1px] before:transform before:-translate-x-1/2 
+                        before:rotate-45 before:-z-1
+                    ">
                         {/* details */}
                         <div className="details">
                             {/* name */}
