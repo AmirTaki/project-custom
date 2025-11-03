ab532421f1b466ea414271b40a86a91ef6339574
amir <amirtaki623@gmail.com>
2025-11-03 04:32:02 +0330

    day 26 part 10

--- src/component/navbar/index.jsx
+++ src/component/navbar/index.jsx
@@ -44,42 +44,49 @@ const Navbar = () => {
 
 
     return(
-        <div className="flex justify-between items-center  px-[5rem]! max-lg:px-[2rem]! text-white
-        w-[100%]! fixed! top-0 bg-[rgba(0,0,0,.7)] h-[60px] z-[2000]! 
+        <div className="  text-white
+        w-[100%]! fixed! top-0 bg-[rgba(0,0,0,.7)]  z-[2000]! 
         ">
             <BrowserRouter>
-                <div className={`items-center justify-center flex `}>
-                    {/* logo */}
-                    <div className="">
-                        <a className="text-[1.5rem] font-bold  cursor-pointer duration-300 hover:text-[orange]" href="">Web developer </a>
-                    </div>
-                </div>
-                <ul className={`${state.resize ? "bg-blue-500 overflow-hidden  flex! flex-col items-center justify-center fixed right-[2rem] max-md:right-[5%] max-md:left-[5%]  max-md:w-[90%]! w-[300px] rounded-2xl top-[60px]" : "bg-red-500 flex justify-center items-center gap-10 "}`}>
-                    <li  className={`${state.resize ? " p-[.7rem] text-md" : "text-md"} cursor-pointer duration-300 hover:text-[orange] `} ><Link to = '/'>Home</Link></li>
-                    <li  className={`${state.resize ? " p-[.7rem] text-md" : "text-md"} cursor-pointer duration-300 hover:text-[orange] `} ><Link to = '/'>About</Link></li>
-                    <li  className={`${state.resize ? " p-[.7rem] text-md" : "text-md"} cursor-pointer duration-300 hover:text-[orange] `} ><Link to = '/'>Services</Link></li>
-                    <li  className={`${state.resize ? " p-[.7rem] text-md" : "text-md"} cursor-pointer duration-300 hover:text-[orange] `} ><Link to = '/'>Contact</Link></li>
-                    <div className="border-0 w-[80%] text-center oumdine-0 px-[1rem] py-[.5rem] rounded-[20px] cursor-pointer text-[.8rem] font-bold bg-[orange] hover:scale-111 duration-200 active:scale-95 lg:hidden!">
-                        Get Started
-                    </div>
-                </ul>
+                <div className="flex justify-between items-center  h-[60px] px-[5rem]! max-lg:px-[2rem]!">
 
-                <div 
-                    className={`flex items-center justify-center  max-lg:hidden!  } `}
-                >
-                    <div className="border-0 outline-0 px-[1rem] py-[.5rem]
-                        rounded-[20px] cursor-pointer text-[.8rem] font-bold bg-[orange]
-                        hover:scale-111 duration-200 active:scale-95 
-                        ">
-                        Get Started
+                    <div className={`items-center justify-center flex `}>
+                        {/* logo */}
+                        <div className="">
+                            <a className="text-[1.5rem] font-bold  cursor-pointer duration-300 hover:text-[orange]" href="">Web developer </a>
+                        </div>
                     </div>
-                </div>
+                    <ul className=
+                        {`${state.resize ? " overflow-hidden  duration-500 h-0 flex! flex-col items-center justify-center fixed right-[2rem] max-md:right-[5%] max-md:left-[5%]  max-md:w-[90%]! w-[300px] rounded-2xl top-[60px]" : " flex justify-center items-center gap-10 "}
+                            ${state.resize && state.dropMenu ? "h-[260px] duration-500 " : ""} bg-[rgba(0,0,0,.7)]
+                        `}    
+                    >
+                        <li  className={`${state.resize ? " p-[.7rem] text-md" : "text-md"} cursor-pointer duration-300 hover:text-[orange] `} ><Link to = '/'>Home</Link></li>
+                        <li  className={`${state.resize ? " p-[.7rem] text-md" : "text-md"} cursor-pointer duration-300 hover:text-[orange] `} ><Link to = '/'>About</Link></li>
+                        <li  className={`${state.resize ? " p-[.7rem] text-md" : "text-md"} cursor-pointer duration-300 hover:text-[orange] `} ><Link to = '/'>Services</Link></li>
+                        <li  className={`${state.resize ? " p-[.7rem] text-md" : "text-md"} cursor-pointer duration-300 hover:text-[orange] `} ><Link to = '/'>Contact</Link></li>
+                        <div className="border-0 w-[80%] text-center oumdine-0 px-[1rem] py-[.5rem] rounded-[20px] cursor-pointer text-[.8rem] font-bold bg-[orange] hover:scale-111 duration-200 active:scale-95 lg:hidden!">
+                            Get Started
+                        </div>
+                    </ul>
 
-                <div className={`${state.resize ? 'block' : 'hidden'}`}>
-                       <div 
-                        onClick={()=>{dispatch({type : 'hadlerDropMenu'})}}
-                        className="text-white text-[1.5rem] cursor-pointer">
-                        <i className={`${state.dropMenu ? "fa-solid fa-xmark" : "fa-solid fa-bars"}`}></i>
+                    <div 
+                        className={`flex items-center justify-center  max-lg:hidden!  } `}
+                    >
+                        <div className="border-0 outline-0 px-[1rem] py-[.5rem]
+                            rounded-[20px] cursor-pointer text-[.8rem] font-bold bg-[orange]
+                            hover:scale-111 duration-200 active:scale-95 
+                            ">
+                            Get Started
+                        </div>
+                    </div>
+
+                    <div className={`${state.resize ? 'block' : 'hidden'}`}>
+                        <div 
+                            onClick={()=>{dispatch({type : 'hadlerDropMenu'})}}
+                            className="text-white text-[1.5rem] cursor-pointer">
+                            <i className={`${state.dropMenu ? "fa-solid fa-xmark" : "fa-solid fa-bars"}`}></i>
+                        </div>
                     </div>
                 </div>
                    
