8c1240dc0f27a5f83b40d3cb1b9c25d95f378a16
amir <amirtaki623@gmail.com>
2025-11-04 21:34:18 +0330

    day 27 part 29

--- src/component/gallery/index.jsx
+++ src/component/gallery/index.jsx
@@ -26,10 +26,11 @@ const GalleryImage = () => {
             className="w-[80%] h-[750px]  overflow-y-scroll m-[100px_auto_50px] grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-[30px] scrollbarGarllery "
         >
             {state.map((item) => {
+                //  img 
                 return(
-
-                    //  img 
-                    <img key = {item.id} src={item.image} alt="" 
+                    <img 
+                       onClick={() => {disptach({type : 'handlerClick', payload : {id : item.id}})}} 
+                        key = {item.id} src={item.image} alt="" 
                         className='w-[99%] h-full cursor-pointer transition duration-1000 ease-in-out hover:scale-75 hover:shadow-[0_32px_75px_rgba(68,77,136,.2)] ' 
                     />
                 )
