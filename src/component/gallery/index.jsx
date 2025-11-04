7badcd3e5ec8b4ed94f7690de4c4b6e947a38c80
amir <amirtaki623@gmail.com>
2025-11-04 20:56:45 +0330

    day 27 part 18

--- src/component/gallery/index.jsx
+++ src/component/gallery/index.jsx
@@ -3,7 +3,7 @@ const GalleryImage = () => {
     return(
         // imageWrpper
         <div className="w-full h-screen bg-[rgba(0,0,0,.9)] fixed top-0 left-0 flex justify-center items-center z-[200]">
-            <img src={img1} alt="" />
+            <img src={img1} alt="" className='w-[90%] max-w-[500px]'/>
         </div>
     )
 }
