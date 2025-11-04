78e47ce6a8fb42296462e67e46d132b2c6b87201
amir <amirtaki623@gmail.com>
2025-11-04 23:05:30 +0330

    day 27 part 58

--- src/component/login.jsx/boxItem.jsx
+++ src/component/login.jsx/boxItem.jsx
@@ -2,6 +2,30 @@ export const BoxItem = [
     box = [
         {
            id : 0,
+           color : '#0f0' 
+        },
+        {
+           id : 1,
+           color : '#ff0' 
+        },
+        {
+           id : 2,
+           color : '#f00' 
+        },
+        {
+           id : 3,
+           color : '#f0f' 
+        },
+        {
+           id : 4,
+           color : '' 
+        },
+        {
+           id : 5,
+           color : '' 
+        },
+        {
+           id : 6,
            color : '' 
         },
     ]
