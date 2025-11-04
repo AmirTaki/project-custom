7b41b6533933de74295255a7c15575e77d7b4e5a
amir <amirtaki623@gmail.com>
2025-11-04 23:02:15 +0330

    day 27 part 56

--- src/component/login.jsx/index.jsx
+++ src/component/login.jsx/index.jsx
@@ -1,7 +1,10 @@
 const LoginForm = () => {
     return(
         // container
-        <div className="grid grid-cols-[repeat(3,150px)] gap-[4px]"></div>
+        <div className="grid grid-cols-[repeat(3,150px)] gap-[4px]">
+            {/* box */}
+            <div className="relative w-[150px] h-[150px] bg-[rgba(45,4545,1) flex overflow-hidden justify-center items-center]"></div>
+        </div>
     )
 }
 
