fe0835140d9786b53a89649e2126f7286fbecc71
amir <amirtaki623@gmail.com>
2025-11-04 23:19:29 +0330

    day 27 part 67

--- src/component/login.jsx/index.jsx
+++ src/component/login.jsx/index.jsx
@@ -6,7 +6,6 @@ const LoginForm = () => {
             {/* box */}
             <div 
                 className="boxContainer
-                after:content after:absolute after:inset-[2px] after:bg-[rgba(45,45,45,.9)]
                 before:content before:absolute before:transform before:-translate-x-1/2 before:-translate-y-1/2 before:bg-red-500
                 before:w-[300px] before:h-[300px] before:transition before:duration-[.5s,top_.1s,left_.1s]
                 "
