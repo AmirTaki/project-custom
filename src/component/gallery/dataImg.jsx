4d616b915ab3139c93a97e0c30eefb68d0624ada
amir <amirtaki623@gmail.com>
2025-11-04 20:50:05 +0330

    day 27 part 15

---
+++ src/component/gallery/dataImg.jsx
@@ -0,0 +1,43 @@
+import img1 from "./image/img1.jpg"
+import img2 from "./image/img2.jpg"
+import img3 from "./image/img3.jpg"
+import img4 from "./image/img4.jpg"
+import img5 from "./image/img5.jpg"
+import img6 from "./image/img6.jpg"
+import img7 from "./image/img7.jpg"
+import img8 from "./image/img8.jpg"
+const DataGallery = [
+    {
+        id : 0,
+        image : img1
+    },
+    {
+        id : 1,
+        image : img2
+    },
+    {
+        id : 2,
+        image : img3
+    },
+    {
+        id : 3,
+        image : img4
+    },
+    {
+        id : 4,
+        image : img5
+    },
+    {
+        id : 5,
+        image : img6
+    },
+    {
+        id : 6,
+        image : img7
+    },
+    {
+        id : 7,
+        image : img8
+    },
+  
+]
