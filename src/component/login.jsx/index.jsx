a6729580f47d45877155d9d312abff44d7b0b9bf
amir <amirtaki623@gmail.com>
2025-11-04 23:37:16 +0330

    day 27 part 74

--- src/component/login.jsx/index.jsx
+++ src/component/login.jsx/index.jsx
@@ -4,11 +4,15 @@ import "./styles.css"
 const LoginForm = () => {
     return(
         // container
-        <div className="grid grid-cols-[repeat(3,150px)] gap-[4px]">
+        <div className="grid grid-cols-[repeat(3,150px)] gap-[4px] mt-5">
             {/* box */}
             {BoxItem.box.map((box) => {
                 return(
-                    <div className="boxContainer"></div>
+                    <div 
+                        style = {{"--clr" : box.color}}
+                        key = {box.id} 
+                        className="boxContainer"
+                    ></div>
                 )
             })}
         </div>
