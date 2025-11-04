f398ccf9c7551d5f4679470c9853833772d5167d
amir <amirtaki623@gmail.com>
2025-11-04 20:57:50 +0330

    day 27 part 19

--- src/component/gallery/index.jsx
+++ src/component/gallery/index.jsx
@@ -4,6 +4,9 @@ const GalleryImage = () => {
         // imageWrpper
         <div className="w-full h-screen bg-[rgba(0,0,0,.9)] fixed top-0 left-0 flex justify-center items-center z-[200]">
             <img src={img1} alt="" className='w-[90%] max-w-[500px]'/>
+            <span className='absolute top-[5%] right-[5%] text-[30px] font-[sans-serif]  text-white cursor-pointer'>
+                X
+            </span>
         </div>
     )
 }
