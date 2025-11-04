78e03d54a28224e551bc1414d8a5cd4e1a1ce0f4
amir <amirtaki623@gmail.com>
2025-11-04 23:13:17 +0330

    day 27 part 63

--- src/component/login.jsx/index.jsx
+++ src/component/login.jsx/index.jsx
@@ -6,7 +6,8 @@ const LoginForm = () => {
             <div 
                 className="relative w-[150px] h-[150px] bg-[rgba(45,45,45,1)]  flex overflow-hidden justify-center items-center
                 after:content after:absolute after:inset-[2px] after:bg-[rgba(45,45,45,.9)]
-                before:content before:absolute before:transform before:-translate-x-1/2 before:-translate-y-1/2
+                before:content before:absolute before:transform before:-translate-x-1/2 before:-translate-y-1/2 before:bg-red-500
+                before:w-[300px] before:h-[300px] before:transition before:duration-[.5s,top_.1s,left_.1s]
                 "
             >
                     
