253b514f2b78320cdf385505baa918fa45d20af0
amir <amirtaki623@gmail.com>
2025-11-04 21:38:05 +0330

    day 27 part 40

--- src/component/gallery/dataImg.jsx
+++ src/component/gallery/dataImg.jsx
@@ -7,38 +7,41 @@ import img6 from "./image/img6.jpg"
 import img7 from "./image/img7.jpg"
 import img8 from "./image/img8.jpg"
 
-export const DataGallery = [
-    {
-        id : 0,
-        image : img1
-    },
-    {
-        id : 1,
-        image : img2
-    },
-    {
-        id : 2,
-        image : img3
-    },
-    {
-        id : 3,
-        image : img4
-    },
-    {
-        id : 4,
-        image : img5
-    },
-    {
-        id : 5,
-        image : img6
-    },
-    {
-        id : 6,
-        image : img7
-    },
-    {
-        id : 7,
-        image : img8
-    },
+export const DataGallery = {
+    displayWarpper : false,
+    image:[
+        {
+            id : 0,
+            image : img1
+        },
+        {
+            id : 1,
+            image : img2
+        },
+        {
+            id : 2,
+            image : img3
+        },
+        {
+            id : 3,
+            image : img4
+        },
+        {
+            id : 4,
+            image : img5
+        },
+        {
+            id : 5,
+            image : img6
+        },
+        {
+            id : 6,
+            image : img7
+        },
+        {
+            id : 7,
+            image : img8
+        }
+    ]
   
-]
+}
