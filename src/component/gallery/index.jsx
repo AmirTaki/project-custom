0838b11d0e0a6c6b81a41b5be9dcd2c37b8d5912
amir <amirtaki623@gmail.com>
2025-11-04 22:09:38 +0330

    day 27 part 47

--- src/component/gallery/index.jsx
+++ src/component/gallery/index.jsx
@@ -1,18 +1,20 @@
-import { act, useReducer, useRef } from 'react';
+import {useReducer, useRef } from 'react';
 import { DataGallery } from './dataImg';
-import img1 from './image/img8.jpg'
+
 import './styles.css'
 const GalleryImage = () => {
     const wrapperImage =  useRef(null)
     const reducer = (state, action) => {
         switch(action.type){
             case "handlerClick":
+                document.documentElement.style.overflow = "hidden";
                 if(wrapperImage.current ){      
                     wrapperImage.current.src = action.payload.src
                 }
             return{...state, displayWarpper : true}
 
             case "handlerCross":
+                document.documentElement.style.overflow = "auto";
             return{...state, displayWarpper : false}
         }
     }
@@ -22,7 +24,7 @@ const GalleryImage = () => {
         <>
         {/* imageWrpper */}
         <div className={`${state.displayWarpper ? 'flex' : 'hidden'} w-full h-screen bg-[rgba(0,0,0,.9)] fixed top-0 left-0  justify-center items-center z-[200]`}  >
-            <img src = {img1} alt="" className='w-[90%] max-w-[500px]' ref = {wrapperImage}/>
+            <img src = {state.image[0]} alt="" className='w-[90%] max-w-[500px]' ref = {wrapperImage}/>
             <span 
                 onClick={() => {disptach({type : 'handlerCross'})}}
                 className='absolute top-[5%] right-[5%] text-[30px] font-[sans-serif]  text-white cursor-pointer!'
