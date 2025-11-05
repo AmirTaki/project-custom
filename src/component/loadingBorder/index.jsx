53cc771d8b7f6ad4ae8c3e93575aaca209cc7541
amir <amirtaki623@gmail.com>
2025-11-05 23:36:34 +0330

    day 28 part 57

--- src/component/loadingBorder/index.jsx
+++ src/component/loadingBorder/index.jsx
@@ -7,14 +7,14 @@ const LoadingBorder = () => {
 
     useEffect(() => {
         if(!active) return;
-        
     }, [active]) 
 
     return(
         // container
-        <div className="flex items-center justify-center relative h-[450px] w-[350px] rounded-[16px] bg-white overflow-hidden
+        <div className={`${active ? "" : ""}
+            flex items-center justify-center relative h-[450px] w-[350px] rounded-[16px] bg-white overflow-hidden
             before:content before:absolute before:h-[650px] before:w-[650px] before:bg-[conic-gradients(transparent,transparent,transparent,#fff)] 
-        ">
+        `}>
             {/* overlay */}
             <span
                 className="flex items-center justify-center absolute h-[440px] w-[340px] text-[40px] font-[500] text-white rounded-[12px] bg-[#10131c] font-[Poopins]"
