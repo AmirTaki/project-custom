6de27499c4397c29655de086b8eb49ece9c5852d
amir <amirtaki623@gmail.com>
2025-11-05 23:22:38 +0330

    day 28 part 49

--- src/component/loadingBorder/index.jsx
+++ src/component/loadingBorder/index.jsx
@@ -1,7 +1,9 @@
 const LoadingBorder = () => {
     return(
         // container
-        <div className="flex items-center justify-center relative h-[450px] w-[350px] rounded-[16px] bg-white overflow-hidden ">
+        <div className="flex items-center justify-center relative h-[450px] w-[350px] rounded-[16px] bg-white overflow-hidden
+            before:content before:absolute before:h-[650px] before:w-[650px] before:bg-amber-400
+        ">
             {/* overlay */}
             <span
                 className="flex items-center justify-center"
