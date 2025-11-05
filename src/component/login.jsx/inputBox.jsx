884f8aebc5e6e932a6c787e594fa75e049474cb9
amir <amirtaki623@gmail.com>
2025-11-05 22:27:57 +0330

    day 28 part 40

--- src/component/login.jsx/inputBox.jsx
+++ src/component/login.jsx/inputBox.jsx
@@ -1,8 +1,8 @@
-const InputBox = ({input, type}) => {
+const InputBox = ({input, type, value}) => {
     return(
         // inputBox 
         <div className="relative w-full mt-[30px]">
-            <input type={type}  required id = {input}
+            <input type={type}  required id = {input} value = {value}
                 className="w-full py-[20px] px-[25px] outline-0 rounded-[10px] bg-[rgba(0,0,0,.25)]  border-1 border-[#000] text-white text-[1em] uppercase tracking-[.1em] backdrop-blur-[10px] peer pointer-events-auto    "
             />
             {input !== 'submit' && (       
