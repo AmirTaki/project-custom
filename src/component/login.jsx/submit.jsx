7c7de4535bda2a8697b71cee73430a17fe37386f
amir <amirtaki623@gmail.com>
2025-11-05 22:43:15 +0330

    day 28 part 44

--- src/component/login.jsx/submit.jsx
+++ src/component/login.jsx/submit.jsx
@@ -2,7 +2,12 @@ const SubmitButton = ({value}) => {
     return(
         // inputBox 
         <div className="relative w-full mt-[30px]">
-            <input type="submit" value = {value} />
+            <input 
+                type="submit" value = {value} 
+                className="w-full py-[20px] px-[25px] outline-0 rounded-[10px] bg-[rgba(0,0,0,.25)]  border-1 border-[#000] text-white  uppercase tracking-[.1em] backdrop-blur-[10px] peer pointer-events-auto 
+                    cursor-pointer! text-[1.1em]! font-[500]! duration-300! text-center! hover:bg-[#0f0] hover:text-[#222] hover:border-[1px] hover:border-transparent!
+                "    
+            />
         </div>
     )
 
