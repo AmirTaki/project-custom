6f0a9d773116a32ba8e4b89438eba54b3868dd4b
amir <amirtaki623@gmail.com>
2025-11-05 23:43:34 +0330

    day 28 part 61

--- src/component/loadingBorder/index.jsx
+++ src/component/loadingBorder/index.jsx
@@ -13,7 +13,7 @@ const LoadingBorder = () => {
         // container
         <div className={`${active ? "bg-[#000]! container" : ""}
             flex items-center justify-center relative h-[450px] w-[350px] rounded-[16px] bg-white overflow-hidden
-            before:content before:absolute before:h-[650px] before:w-[650px] 
+            before:content before:absolute before:h-[650px] before:w-[650px] before:bg-[conic-gradient(transparent,transparent,transparent,#fff)]
         `}>
             {/* overlay */}
             <span
