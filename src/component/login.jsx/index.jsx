973d3d6b0a7f015f072eda4b68584931e6730fc5
amir <amirtaki623@gmail.com>
2025-11-05 20:32:43 +0330

    day 28 part 33

--- src/component/login.jsx/index.jsx
+++ src/component/login.jsx/index.jsx
@@ -24,13 +24,28 @@ const LoginForm = () => {
                         className="w-full py-[20px] px-[25px] outline-0 rounded-[10px] bg-[rgba(0,0,0,.25)]  border-1 border-[#000] text-white text-[1em] uppercase tracking-[.1em] backdrop-blur-[10px] peer pointer-events-auto    "/>
                     <label 
                         htmlFor="username"
-                        className="absolute text-[1em] left-0 py-[20px] px-[25px] text-white uppercase rounded-[4px] duration-300 pointer-events-none
+                        className="absolute text-[1em] left-0 py-[20px] px-[25px] text-white uppercase rounded-[4px] duration-300 pointer-events-none tracking-[.2em]
                             peer-focus:translate-x-[15px] peer-focus:-translate-y-[10px] peer-focus:text-[.75em]! peer-focus:bg-white peer-focus:text-[#222]
-                            peer-focus:py-[2px] peer-focus:px-[7px] peer-focus:border-[1px] peer-focus:border-black  peer-focus:tracking-[.3em] peer-focus:font-[500]
+                            peer-focus:py-[2px] peer-focus:px-[7px] peer-focus:border-[1px] peer-focus:border-black  peer-focus:tracking-[.1em] 
                             peer-valid:translate-x-[15px] peer-valid:-translate-y-[10px] peer-valid:text-[.75em]! peer-valid:bg-white peer-valid:text-[#222]
-                            peer-valid:py-[2px] peer-valid:px-[7px] peer-valid:border-[1px] peer-valid:border-black peer-valid:tracking-[.3em] peer-valid:font-[500]
+                            peer-valid:py-[2px] peer-valid:px-[7px] peer-valid:border-[1px] peer-valid:border-black peer-valid:tracking-[.1em] 
                     ">      
                         Username
+                    </label>
+                </div>
+                {/* inputBox */}
+                <div className="relative w-full mt-[30px]">
+                    <input type="password"  required id = 'password'
+                        className="w-full py-[20px] px-[25px] outline-0 rounded-[10px] bg-[rgba(0,0,0,.25)]  border-1 border-[#000] text-white text-[1em] uppercase tracking-[.1em] backdrop-blur-[10px] peer pointer-events-auto    "/>
+                    <label 
+                        htmlFor="password"
+                        className="absolute text-[1em] left-0 py-[20px] px-[25px] text-white uppercase rounded-[4px] duration-300 pointer-events-none tracking-[.2em]
+                            peer-focus:translate-x-[15px] peer-focus:-translate-y-[10px] peer-focus:text-[.75em]! peer-focus:bg-white peer-focus:text-[#222]
+                            peer-focus:py-[2px] peer-focus:px-[7px] peer-focus:border-[1px] peer-focus:border-black  peer-focus:tracking-[.1em] 
+                            peer-valid:translate-x-[15px] peer-valid:-translate-y-[10px] peer-valid:text-[.75em]! peer-valid:bg-white peer-valid:text-[#222]
+                            peer-valid:py-[2px] peer-valid:px-[7px] peer-valid:border-[1px] peer-valid:border-black peer-valid:tracking-[.1em]
+                    ">      
+                        Passwrod
                     </label>
                 </div>
             </div>
