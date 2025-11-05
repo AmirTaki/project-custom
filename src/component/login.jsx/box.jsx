5345c9a24520de097b2e185fb1ce7cadbbd6acd3
amir <amirtaki623@gmail.com>
2025-11-05 19:19:24 +0330

    day 28 part 8

--- src/component/login.jsx/box.jsx
+++ src/component/login.jsx/box.jsx
@@ -1,6 +1,27 @@
 const Box = () => {
+    const boxRef = useRef(null)
+    const handlerAnimation = useCallback(() => {
+    
+        }, [])
+        const  handlerMouseMove = ((e) => {
+            const box = boxRef.current
+            if(!box) return;
+    
+            const rect = box.getBoundingClientRect()
+            const x = e.clientX - rect.left
+            const y = e.clientY - rect.top        
+    
+            box.style.setProperty('--x', `${x}px`)
+            box.style.setProperty('--y', `${y}px`)
+        })
     return(
-
+        <div 
+            ref = {boxRef}
+            onMouseMove={handlerMouseMove}
+            style = {{"--clr" : box.color,  }}
+            key = {box.id} 
+            className="boxContainer"
+        ></div>
     )
 }
 
--- src/component/login.jsx/index.jsx
+++ src/component/login.jsx/index.jsx
@@ -1,36 +1,18 @@
 import { useCallback, useRef } from "react"
 import { BoxItem } from "./boxItem"
 import "./styles.css"
+import Box from "./box"
+
 
 const LoginForm = () => {
-    const boxRef = useRef(null)
-    const handlerAnimation = useCallback(() => {
-
-    }, [])
-    const  handlerMouseMove = ((e) => {
-        const box = boxRef.current
-        if(!box) return;
-
-        const rect = box.getBoundingClientRect()
-        const x = e.clientX - rect.left
-        const y = e.clientY - rect.top        
-
-        box.style.setProperty('--x', `${x}px`)
-        box.style.setProperty('--y', `${y}px`)
-    })
+  
     return(
         // container
         <div className="grid grid-cols-[repeat(3,150px)] gap-[4px] mt-5">
             {/* box */}
             {BoxItem.box.map((box) => {
                 return(
-                    <div 
-                        ref = {boxRef}
-                        onMouseMove={handlerMouseMove}
-                        style = {{"--clr" : box.color,  }}
-                        key = {box.id} 
-                        className="boxContainer"
-                    ></div>
+                    <Box />
                 )
             })}
         </div>
