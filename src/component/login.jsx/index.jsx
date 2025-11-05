1ffdb8acfd464e345a4c989f6d237416c6221efa
amir <amirtaki623@gmail.com>
2025-11-05 20:09:22 +0330

    day 28 part 230

--- src/component/login.jsx/index.jsx
+++ src/component/login.jsx/index.jsx
@@ -20,16 +20,18 @@ const LoginForm = () => {
                 </h2>
                 {/* inputBox */}
                 <div className="relative w-full mt-[30px]">
-                    <input type="text"   required
+                    <input type="text"   required id = 'username'
                         className="w-full py-[20px] px-[25px] outline-0 rounded-[10px] bg-[rgba(0,0,0,.25)] border-1 border-[#000]
                         backdrop-blur-[10px] text-white text-[1em] uppercase leading-[.1em] pointer-events-auto group
                     "/>
-                    <span className="absolute text-[1em] left-0 py-[20px] px-[25px] text-white uppercase rounded-4px duration-300 pointer-events-none
-                        peer-focus:translate-x-[15px] peer-focus:-translate-y-[10px] peer-focus:text-[.75em] peer-focus:bg-white peer-focus:text-[#222]
-                        peer-focus:py-[2px] peer-focus:px-[5px] peer-focus:border-[1px] peer-focus:border-black
+                    <label 
+                        htmlFor="username"
+                        className="absolute text-[1em] left-0 py-[20px] px-[25px] text-white uppercase rounded-4px duration-300 pointer-events-none
+                            peer-focus:translate-x-[15px] peer-focus:-translate-y-[10px] peer-focus:text-[.75em] peer-focus:bg-white peer-focus:text-[#222]
+                            peer-focus:py-[2px] peer-focus:px-[5px] peer-focus:border-[1px] peer-focus:border-black
                     ">
                         Username
-                    </span>
+                    </label>
                 </div>
             </div>
         </div>
