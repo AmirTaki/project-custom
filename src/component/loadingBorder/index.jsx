588f1bb696efdff7b7e45791e4de8f354f401a3a
amir <amirtaki623@gmail.com>
2025-11-05 23:42:26 +0330

    day 28 part 60

--- src/component/loadingBorder/index.jsx
+++ src/component/loadingBorder/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-
+import "./styles.css"
 const LoadingBorder = () => {
     // const [precent, setPercent] = useState(0)
     const precent =  useRef(0)
@@ -11,9 +11,9 @@ const LoadingBorder = () => {
 
     return(
         // container
-        <div className={`${active ? "bg-[#000]!" : ""}
+        <div className={`${active ? "bg-[#000]! container" : ""}
             flex items-center justify-center relative h-[450px] w-[350px] rounded-[16px] bg-white overflow-hidden
-            before:content before:absolute before:h-[650px] before:w-[650px] before:bg-[conic-gradients(transparent,transparent,transparent,#fff)] 
+            before:content before:absolute before:h-[650px] before:w-[650px] 
         `}>
             {/* overlay */}
             <span
