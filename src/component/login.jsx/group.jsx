820f2c8003e5bd09e837647ce8107aab2bf9f15c
amir <amirtaki623@gmail.com>
2025-11-05 22:57:55 +0330

    day 28 part 41

--- src/component/login.jsx/group.jsx
+++ src/component/login.jsx/group.jsx
@@ -3,8 +3,11 @@ import { Link } from "react-router-dom"
 const GroupForm = () => {
     return(
         <div className="relative w-full mt-[50px] flex justify-between">
-            <Link to = "/" className = "text-[#222] bg-white py-2 px-5 decoration-0 leading-[.5em] rounded-[4px] pointer-events-auto">
+            <Link to = "/" className = "text-[#222] bg-white py-[2px] px-[5px] decoration-0 tracking-[.05em] rounded-[4px] pointer-events-auto">
                 Forget Password
+            </Link>
+            <Link to = "/" className = "text-white bg-[#f0f] font-[500] py-[2px] px-[5px] decoration-0 tracking-[.05em] rounded-[4px] pointer-events-auto">
+                Sign up
             </Link>
         </div>
     )
