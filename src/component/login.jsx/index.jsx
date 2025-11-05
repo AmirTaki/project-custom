5697fe89392794ef04ba91471f924f821065cfd9
amir <amirtaki623@gmail.com>
2025-11-05 19:41:43 +0330

    day 28 part 22

--- src/component/login.jsx/index.jsx
+++ src/component/login.jsx/index.jsx
@@ -20,7 +20,12 @@ const LoginForm = () => {
                     Login
                 </h2>
                 {/* inputBox */}
-                <div className="relative w-full mt-[30px]"></div>
+                <div className="relative w-full mt-[30px]">
+                    <input type="text"  required 
+                        className="w-full py-[20px] px-[25px] outline-0 rounded-[10px] bg-[rgba(0,0,0,.25)] border-1 border-[#000]
+                        backdrop-blur-[10px]
+                    "/>
+                </div>
             </div>
         </div>
     )
