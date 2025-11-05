b3f7ada79711604ba4a10da47a42dfab97f0f76e
amir <amirtaki623@gmail.com>
2025-11-05 22:59:23 +0330

    day 28 part 42

--- src/component/login.jsx/group.jsx
+++ src/component/login.jsx/group.jsx
@@ -6,7 +6,7 @@ const GroupForm = () => {
             <Link to = "/" className = "text-[#222] bg-white py-[2px] px-[5px] decoration-0 tracking-[.05em] rounded-[4px] pointer-events-auto">
                 Forget Password
             </Link>
-            <Link to = "/" className = "text-white bg-[#f0f] font-[500] py-[2px] px-[5px] decoration-0 tracking-[.05em] rounded-[4px] pointer-events-auto">
+            <Link to = "/" className = "text-white bg-[#f0f] font-[500] py-[2px] px-[5px] decoration-0 tracking-[.05em] rounded-[4px] pointer-events-auto shadow-[0_0_15px_#f0f,0_0_30px_#f0f,0_0_60px_#f0f]">
                 Sign up
             </Link>
         </div>
