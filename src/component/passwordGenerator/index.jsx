e1daf3cce64f40296b9dd81ce43e94310675857d
amir <amirtaki623@gmail.com>
2025-11-06 20:38:49 +0330

    day 29 part 27

--- src/component/passwordGenerator/index.jsx
+++ src/component/passwordGenerator/index.jsx
@@ -7,6 +7,8 @@ const PasswordGenerator = () => {
         switch(action.type){
             case "handlerGenerate" :
                 return {...state};
+            case "handlerRange": 
+                return{...state, value : action.payload.event}
         }
     }
 
