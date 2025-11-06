ec36b74404f2c88af478ebfc0af8a27b19b69179
amir <amirtaki623@gmail.com>
2025-11-06 19:39:00 +0330

    day 29 part 3

--- src/component/passwordGenerator/index.jsx
+++ src/component/passwordGenerator/index.jsx
@@ -1,6 +1,7 @@
 const PasswordGenerator = () => {
     return(
-        <div className=""></div>
+        // container
+        <div className="border-[.5px] border-white rounded-[10px] py-[28px] px-[32px] flex flex-col bg-transparent shadow-[8px_8px_4px_#909090,8px_8px_0_#575757]"></div>
     )
 }
 
