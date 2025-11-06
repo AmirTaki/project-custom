30a15b2edc07be00bcf9c2736e3009684cdc5106
amir <amirtaki623@gmail.com>
2025-11-06 21:39:20 +0330

    day 29 part 36

--- src/component/passwordGenerator/initailState.jsx
+++ src/component/passwordGenerator/initailState.jsx
@@ -1,30 +1,35 @@
 export const DataInitial = {
     value : 15,
-      
+    
+
     inputCheckBox : [
         {
             key : 0, 
             id : 'lowercase',
             type : 'checkbox',
-            text : 'Include Lowercase Letters (a-z)' 
+            text : 'Include Lowercase Letters (a-z)',
+            checked : false
         },
         {
             key : 1, 
             id : 'uppercase',
             type : 'checkbox',
-            text : 'Include Uppercase Letters (A-Z)' 
+            text : 'Include Uppercase Letters (A-Z)',
+            checked : false 
         },
         {
             key : 2, 
             id : 'numbers',
             type : 'checkbox',
-            text : 'Include Numbers (0-9)' 
+            text : 'Include Numbers (0-9)',
+            checked : false 
         },
         {
             key : 3, 
             id : 'symbols',
             type : 'checkbox',
-            text : 'Include Symbols (@-*)' 
+            text : 'Include Symbols (@-*)' ,
+            checked : false
         },
     ]
 }
