c87522a7d600516a0489e2fa668f53efa158a6ba
amir <amirtaki623@gmail.com>
2025-11-07 23:16:08 +0330

    day 30 part 54

--- src/component/shareButton/index.jsx
+++ src/component/shareButton/index.jsx
@@ -1,7 +1,14 @@
 const ShareButton = () => {
     return(
         // main-box
-        <div className="absolute left-[50%] top-[60%] transform -translate-x-1/2 -translate-y-1/2"></div>
+        <div className="absolute left-[50%] top-[60%] transform -translate-x-1/2 -translate-y-1/2">
+            {/* check */}
+            <input type="checkbox" />
+            {/*  */}
+            <label htmlFor="check"
+                className="reltaive bg-white h-[50px] w-[50px]"
+            >Share</label>
+        </div>
     )
 }
 export default ShareButton;
