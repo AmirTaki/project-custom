53e3fa91a5b9f1c798ec6145c2e2a5b0f9ff1700
amir <amirtaki623@gmail.com>
2025-11-07 23:21:07 +0330

    day 30 part 58

--- src/component/shareButton/index.jsx
+++ src/component/shareButton/index.jsx
@@ -8,7 +8,9 @@ const ShareButton = () => {
             <label htmlFor="check"
                 className="reltaive bg-white h-[50px] w-[150px] rounded-[35px] block text-center leading-[50px]
                     text-[22px] font-[500] uppercase text-[#0984ac] cursor-pointer shadow-[0_0_4px_rgba(0,0,0,.5)] 
-                    transition-all duration-300 ease-in
+                    transition-all duration-300 ease-in 
+                    before:content-['Cancel'] before:absolute before:h-full before:w-full before:left-0 before:top-0 before:bg-white 
+                    before:opacity-0 before:pointer-events-none
                 "
             >Share</label>
         </div>
