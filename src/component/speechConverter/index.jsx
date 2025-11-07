c1c6ec69b095faf83ce4b2854a577216b960eb5e
amir <amirtaki623@gmail.com>
2025-11-07 20:14:04 +0330

    day 30 part 12

--- src/component/speechConverter/index.jsx
+++ src/component/speechConverter/index.jsx
@@ -14,7 +14,10 @@ const TextSpeech = () => {
             {/* row */}
             <div className="w-[600px] flex text-center gap-[20px]">
                 <select 
-                    className="flex-1 text-white bg-[#403d84] h-[50px] px-[20px] outline-none border-0 rounded-[35px] appearance-none bg-[url('./image/dropdown.png')] bg-no-repeat  bg-[length:15px]"
+                    style={{backgroundPositionX : "calc(100%-20px)", backgroundPositionY : "20px"}}
+                    className="flex-1 text-white bg-[#403d84] h-[50px] px-[20px] outline-none border-0 rounded-[35px] appearance-none bg-[url('./image/dropdown.png')] bg-no-repeat  bg-[length:15px]
+                
+                    "
                 ></select>
             </div>
         </div>
