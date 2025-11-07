f3050db85c7c3058d13cf0582aab63aa3b003de0
amir <amirtaki623@gmail.com>
2025-11-07 20:21:20 +0330

    day 30 part 15

--- src/component/speechConverter/index.jsx
+++ src/component/speechConverter/index.jsx
@@ -15,7 +15,13 @@ const TextSpeech = () => {
             <div className="w-[600px] flex text-center gap-[20px]">
                 <select 
                     style={{backgroundPositionX : "calc(100%-20px)", backgroundPositionY : "20px",backgroundImage: "url('./image/dropdown.png')"}}
-                    className="flex-1 text-white bg-[#403d84] h-[50px] px-[20px] outline-none border-0 rounded-[35px] appearance-none bg-[url('./image/dropdown.png')] bg-no-repeat  bg-[length:15px]"></select>
+                    className="flex-1 text-white bg-[#403d84] h-[50px] px-[20px] outline-none border-0 rounded-[35px] appearance-none bg-[url('./image/dropdown.png')] bg-no-repeat  bg-[length:15px]"
+                >
+                </select>
+                {/* button */}
+                <button className="bg-[#ff296c] text-white text-[16px] py-[10px_30px] rounded-[35px] border-0 outline-none cursor-pointer flex items-center">
+                    Listen
+                </button>
             </div>
         </div>
     )
