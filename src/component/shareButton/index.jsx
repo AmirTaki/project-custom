4ed0cf1aa73a82290b2b1589c867e5fc06d5d88e
amir <amirtaki623@gmail.com>
2025-11-07 23:22:01 +0330

    day 30 part 59

--- src/component/shareButton/index.jsx
+++ src/component/shareButton/index.jsx
@@ -10,7 +10,7 @@ const ShareButton = () => {
                     text-[22px] font-[500] uppercase text-[#0984ac] cursor-pointer shadow-[0_0_4px_rgba(0,0,0,.5)] 
                     transition-all duration-300 ease-in 
                     before:content-['Cancel'] before:absolute before:h-full before:w-full before:left-0 before:top-0 before:bg-white 
-                    before:opacity-0 before:pointer-events-none
+                    before:opacity-0 before:pointer-events-none hover:tracking-[1px]
                 "
             >Share</label>
         </div>
