724c9a5176262777f35586dd64ef21cbbb462eec
amir <amirtaki623@gmail.com>
2025-11-07 19:45:56 +0330

    day 30 part 5

--- src/component/speechConverter/index.jsx
+++ src/component/speechConverter/index.jsx
@@ -1,6 +1,9 @@
 const TextSpeech = () => {
     return(
-        <div className=""></div>
+        // hero
+        <div className="w-full min-h-screen bg-radial-[45deg_#010758_#490d61] text-white flex justify-center items-center flex-col">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est ipsam animi nesciunt voluptatem, voluptate sapiente necessitatibus quia magnam enim deserunt, blanditiis voluptas sit porro nulla, ea quaerat minus dolor ratione.
+        </div>
     )
 }
 export default TextSpeech;
