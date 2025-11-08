27f33f6625c4f312b5f71cca27225c558377a293
amir <amirtaki623@gmail.com>
2025-11-08 20:57:55 +0330

    day 31 part 40

--- src/component/rotatingCards/index.jsx
+++ src/component/rotatingCards/index.jsx
@@ -2,10 +2,10 @@ import './styles.css'
 const RotatingCard = () => {
     const list = [1, 2, 3, 4, 5, 6, 7, 8]
     return(
-        <div className="grid place-items-center h-[50vh] bg-amber-300 pt-40">
+        <div className="grid place-items-center h-[50vh] pt-40  ">
 
         {/* // container */}
-        <div className="  w-[200px] h-[350px]  relative transform-3d  rotate3D">
+        <div className="  w-[200px] h-[350px]   relative transform-3d  rotate3D">
             {list.map((item) => (
                 <div
                     key = {item} 
@@ -13,7 +13,7 @@ const RotatingCard = () => {
                     className="absolute left-0 top-0 transform-3d duration-1000  rotateSpan"
                 >
                     <span
-                        className='bg-[silver] w-[200px] h-[320px] text-[10rem] text-white grid place-items-center font-[600]
+                        className='bg-[rgba(0,0,0,.4)] w-[200px] h-[320px] text-[10rem] text-white grid place-items-center font-[600]
                         text-shadow-[10px_10px_20px_crimson] rounded-[24px] shadow-[0_15px_25px_rgba(0,0,0,.5)]
                         max-lg:w-[150px]! max-lg:h-[220px]! max-lg:text-[7rem]!
                         max-sm:w-[120px]! max-sm:h-[170px]! max-sm:text-[5rem]!
