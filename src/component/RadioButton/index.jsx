9717d9b6c45bebfb860e2d0a40c7261f09fef593
amir <amirtaki623@gmail.com>
2025-11-08 21:50:42 +0330

    day 31 part 53

--- src/component/RadioButton/index.jsx
+++ src/component/RadioButton/index.jsx
@@ -2,23 +2,23 @@ import { useState } from "react";
 
 const PureRadio = () => {
 
-    const [radio, setRadio] = useState(true)
+    const [radio, setRadio] = useState(false)
     return(
         // container
         <div className="flex justify-center items-center">
             {/* radio-title-group */}
             <div className="flex flex-wrap justify-center">
                 {/* input-container */}
-                <div className="relative h-[7rem] w-[7rem] m-[.5rem] hover:shadow-[0_0_12px_#07afd9]! rounded-[8px] duration-300 cursor-pointer">
-                    {/* <input type="radio"  className="absolute w-full h-full m-0 cursor-pointer z-20 opacity-0 "/> */}
+                <div
+                    onClick={() => {setRadio(!radio)}} 
+                    className="relative h-[7rem] w-[7rem] m-[.5rem] hover:shadow-[0_0_12px_#07afd9]! rounded-[8px] duration-300 cursor-pointer">
                     {/* radio-title */}
                     <div 
                         className={`${radio ? "bg-[#07afd9] shadow-[0_0_12px_#07afd9] scale-110" : "bg-transparent"}
                         flex flex-col items-center justify-center h-full border-2 border-[#07afd9]   rounded-[8px] transition-all duration-300! ease-in-out
                         `}
                     >
-                        
-                        <ion-icon name="walk" className = {`${radio ? "text-white" : "text-[#07afd9] "} text-[3rem]`}></ion-icon>
+                        <ion-icon name="walk" style = {{color : `${radio ? "white" : "#07afd9"}`}} className = {` text-[3rem]`}></ion-icon>
                         <label htmlFor="walk" className={`${radio ? "text-white" : "text-[#07afd9] "} text-[.8rem] font-[600] uppercase tracking-[1px]`}>Walk</label>
                     </div>
                 </div>
