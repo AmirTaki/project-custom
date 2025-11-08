ef7d664645bdd9ebb59c048135c859727ca71389
amir <amirtaki623@gmail.com>
2025-11-08 20:26:07 +0330

    day 31 part 25

--- src/component/rotatingCards/index.jsx
+++ src/component/rotatingCards/index.jsx
@@ -1,8 +1,10 @@
 import './styles.css'
 const RotatingCard = () => {
+    const list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     return(
         // container
         <div className=" w-[200px] h-[350px] mt-[10%] relative transform-3d perspective-[1000px] rotate3D">
+            {}
             <div 
                 style = {{"--i" : 0 , transform : "rotateY(calc(var(--i) * 45deg)) translateZ(400px);" }}
                 className="absolute left-0 top-0 transform-3d duration-1000 "
