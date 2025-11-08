9a517d9bfbc3c28adb972b3e6c839e7ba205ed93
amir <amirtaki623@gmail.com>
2025-11-08 22:22:11 +0330

    day 31 part 61

--- src/component/RadioButton/inputContainer.jsx
+++ src/component/RadioButton/inputContainer.jsx
@@ -1,6 +1,19 @@
-const InputContainer = () => {
+const InputContainer = ({item}) => {
     return(
-
+        // {/* input-container */}
+        <div
+            onClick={() => {setRadio(!radio)}} 
+            className="relative h-[7rem] w-[7rem] m-[.5rem] hover:shadow-[0_0_12px_#07afd9]! rounded-[8px] duration-300 cursor-pointer">
+            {/* radio-title */}
+            <div 
+                className={`${radio ? "bg-[#07afd9] shadow-[0_0_12px_#07afd9] scale-110" : "bg-transparent"}
+                flex flex-col items-center justify-center h-full border-2 border-[#07afd9]   rounded-[8px] transition-all duration-300! ease-in-out
+                `}
+            >
+                <ion-icon name="walk" style = {{color : `${radio ? "white" : "#07afd9"}`}} className = {`text-[3rem]`}></ion-icon>
+                <label htmlFor="walk" className={`${radio ? "text-white" : "text-[#07afd9] "} text-[.8rem] font-[600] uppercase tracking-[1px]`}>Walk</label>
+            </div>
+        </div>
     )
 }
 export default InputContainer;
