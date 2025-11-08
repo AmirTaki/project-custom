611db727946087e8e49d8528271978d63b1fb20f
amir <amirtaki623@gmail.com>
2025-11-08 21:33:05 +0330

    day 31 part 4

--- src/component/RadioButton/index.jsx
+++ src/component/RadioButton/index.jsx
@@ -8,7 +8,9 @@ const PureRadio = () => {
                 <div className="relative h-[7rem] w-[7rem] m-[.5rem]">
                     <input type="radio"  className="absolute w-full h-full m-0 cursor-pointer z-20 opacity-0"/>
                     {/* radio-title */}
-                    <div className="flex flex-col items-center justify-center h-full border-2 border-[#07afd9] rounded-[8px] transition-all duration-300 ease-in-out"></div>
+                    <div className="flex flex-col items-center justify-center h-full border-2 border-[#07afd9] rounded-[8px] transition-all duration-300 ease-in-out">
+                        <ion-icon name="walk" className = "text-[##07afd9] text-[3rem]"></ion-icon>
+                    </div>
                 </div>
             </div>
         </div>
