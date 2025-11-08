cddceb0e22f7539d08656fcbc68630d7712ee1b6
amir <amirtaki623@gmail.com>
2025-11-08 20:33:37 +0330

    day 31 part 27

--- src/component/rotatingCards/index.jsx
+++ src/component/rotatingCards/index.jsx
@@ -2,20 +2,23 @@ import './styles.css'
 const RotatingCard = () => {
     const list = [1, 2, 3, 4, 5, 6, 7, 8]
     return(
-        // container
-        <div className=" w-[200px] h-[350px] mt-[10%] relative transform-3d perspective-[1000px] rotate3D">
+        <div className="grid place-items-center h-[10vh]">
+
+        {/* // container */}
+        <div className="  w-[200px] h-[350px] mt-[10%] relative transform-3d  rotate3D">
             {list.map((item) => (
                 <div
                     key = {item} 
-                    style = {{"--i" : item , transform : "rotateY(calc(var(--i) * 45deg)) translateZ(400px);" }}
+                    // style = {{"--i" : item , transform : "rotateY(calc(var(--i) * 45deg)) translateZ(400px)" }}
                     className="absolute left-0 top-0 transform-3d duration-1000 "
                 >
                     <span
-                        className='bg-[red] w-[200px] h-[350px] text-[10rem] text-white grid place-items-center font-[600]
-                        text-shadow-[10px_10px_20px_blue] rounded-[24px] shadow-[0_15px_25px_rgba(0,0,0,.5)]'
+                        className='bg-[silver] w-[200px] h-[350px] text-[10rem] text-white grid place-items-center font-[600]
+                        text-shadow-[10px_10px_20px_crimson] rounded-[24px] shadow-[0_15px_25px_rgba(0,0,0,.5)]'
                     >{item}</span>
                 </div>
             ))}
+        </div>
         </div>
     )
 }
