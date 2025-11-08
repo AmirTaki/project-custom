f30f976be94afc8d8c0b1ea276712962cfa47557
amir <amirtaki623@gmail.com>
2025-11-08 03:39:47 +0330

    day 31 part 9

--- src/component/shareButton/index.jsx
+++ src/component/shareButton/index.jsx
@@ -14,12 +14,13 @@ const ShareButton = () => {
             <label 
                 onClick={() => {setCheck((prevCheck) => (prevCheck = true))}}
                 htmlFor="check"
-                className="reltaive bg-white h-[50px] w-[150px] rounded-[35px] block text-center leading-[50px]
+                className={`
+                    reltaive bg-white h-[50px] w-[150px] rounded-[35px] block text-center leading-[50px]
                     text-[22px] font-[500] uppercase text-[#0984ac] cursor-pointer shadow-[0_0_4px_rgba(0,0,0,.5)] 
                     transition-all duration-300 ease-in 
                     before:content-['Cancel'] before:absolute before:h-full before:w-full before:left-0 before:top-0 before:bg-white 
-                    before:opacity-0 before:pointer-events-none hover:tracking-[1px]
-                "
+                    before:opacity-0 before:pointer-events-none hover:tracking-[1px]    
+                `}
             >Share</label>
             {/* icons */}
             <div className={`${check ? "opacity-100 pointer-events-auto -top-[64px]" : "opacity-0 pointer-events-none -top-[120px]"}
