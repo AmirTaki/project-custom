7443061982c82f5b1adcf8b21a7b0f870b9a8106
amir <amirtaki623@gmail.com>
2025-11-08 21:31:28 +0330

    day 31 part 45

--- src/component/RadioButton/index.jsx
+++ src/component/RadioButton/index.jsx
@@ -7,6 +7,8 @@ const PureRadio = () => {
                 {/* input-container */}
                 <div className="relative h-[7rem] w-[7rem] m-[.5rem]">
                     <input type="radio"  className="absolute w-full h-full m-0 cursor-pointer z-20 opacity-0"/>
+                    {/* radio-title */}
+                    <div className="flex flex-col items-center justify-center h-full border-2 border-[#07afd9] rounded-[8px] transition-all duration-300 ease-in-out"></div>
                 </div>
             </div>
         </div>
