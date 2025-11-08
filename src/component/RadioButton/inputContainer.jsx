8cd0a4ea24e742706ca409add406894d58779ee3
amir <amirtaki623@gmail.com>
2025-11-08 22:23:37 +0330

    day 31 part 62

--- src/component/RadioButton/inputContainer.jsx
+++ src/component/RadioButton/inputContainer.jsx
@@ -2,16 +2,16 @@ const InputContainer = ({item}) => {
     return(
         // {/* input-container */}
         <div
-            onClick={() => {setRadio(!radio)}} 
+            // onClick={() => {setRadio(!radio)}} 
             className="relative h-[7rem] w-[7rem] m-[.5rem] hover:shadow-[0_0_12px_#07afd9]! rounded-[8px] duration-300 cursor-pointer">
             {/* radio-title */}
             <div 
-                className={`${radio ? "bg-[#07afd9] shadow-[0_0_12px_#07afd9] scale-110" : "bg-transparent"}
+                className={`${item.flag ? "bg-[#07afd9] shadow-[0_0_12px_#07afd9] scale-110" : "bg-transparent"}
                 flex flex-col items-center justify-center h-full border-2 border-[#07afd9]   rounded-[8px] transition-all duration-300! ease-in-out
                 `}
             >
-                <ion-icon name="walk" style = {{color : `${radio ? "white" : "#07afd9"}`}} className = {`text-[3rem]`}></ion-icon>
-                <label htmlFor="walk" className={`${radio ? "text-white" : "text-[#07afd9] "} text-[.8rem] font-[600] uppercase tracking-[1px]`}>Walk</label>
+                <ion-icon name={item.icon} style = {{color : `${item.flag ? "white" : "#07afd9"}`}} className = {`text-[3rem]`}></ion-icon>
+                <label htmlFor={item.name} className={`${item.flag ? "text-white" : "text-[#07afd9] "} text-[.8rem] font-[600] uppercase tracking-[1px]`}>{item.name}</label>
             </div>
         </div>
     )
