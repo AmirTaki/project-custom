ab26428019289b601476f11f66851e3aebccefb2
amir <amirtaki623@gmail.com>
2025-11-08 03:31:50 +0330

    day 31 part 5

--- src/component/shareButton/index.jsx
+++ src/component/shareButton/index.jsx
@@ -1,15 +1,19 @@
 import { BrowserRouter, Link } from "react-router-dom";
 import { DataShare } from "./dataShaer";
 import './styles.css'
+import { useState } from "react";
 
 const ShareButton = () => {
+    const [check, setCheck] = useState(false)
     return(
         // main-box
         <div className="absolute left-[50%] top-[60%] transform -translate-x-1/2 -translate-y-1/2">
             {/* check */}
             <input type="checkbox" />
             {/*  */}
-            <label htmlFor="check"
+            <label 
+                onClick={() => {setCheck((prevCheck) => (prevCheck = true))}}
+                htmlFor="check"
                 className="reltaive bg-white h-[50px] w-[150px] rounded-[35px] block text-center leading-[50px]
                     text-[22px] font-[500] uppercase text-[#0984ac] cursor-pointer shadow-[0_0_4px_rgba(0,0,0,.5)] 
                     transition-all duration-300 ease-in 
