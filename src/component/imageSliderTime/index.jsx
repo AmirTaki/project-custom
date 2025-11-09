160bafeb7ccb97c49b72648725fa8fb0771d25e5
amir <amirtaki623@gmail.com>
2025-11-10 03:06:54 +0330

    day 33 part 7

--- src/component/imageSliderTime/index.jsx
+++ src/component/imageSliderTime/index.jsx
@@ -14,12 +14,14 @@ const ImgSliderTime = () => {
         <div className="w-[1300px] max-w-[100vw] h-[700px] m-auto relative overflow-hidden max-md:h-[400px]!">
             {/* list */}
             <div className="absolute top-0 left-0 h-full flex duration-1000 max-w-max">
-                {/* item */}
-                <div className="">
-                    <img 
-                        className="w-[1300px] max-w-[100vw] h-full object-cover"
-                        src={img1} alt="" />
-                </div>
+                {state.images.map((item) => (
+                    // item
+                    <div key = {item.id}  className="">
+                        <img 
+                            className="w-[1300px] max-w-[100vw] h-full object-cover"
+                            src={item.img} alt="" />
+                    </div>
+                ))}
             </div>
         </div>
     )
