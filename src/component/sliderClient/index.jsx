76860f909aa8b735355ca2202e91a3698cf488e7
amir <amirtaki623@gmail.com>
2025-11-09 22:09:24 +0330

    day 32 part 29

--- src/component/sliderClient/index.jsx
+++ src/component/sliderClient/index.jsx
@@ -1,22 +1,33 @@
+import { Swiper } from 'swiper/react';
+import 'swiper/css';
 import { DataSlider } from "./dataSlider";
 import SliderItem from "./sliderItem";
 
 const SliderClientSwiper = () => {
     return(
-        // section
-        <section className="relative w-full min-h-screen flex justify-center items-center bg-[#2197f3] overflow-hidden ">
-            {/* swiper-container   -> h */}
-            <div className="w-[400px]  pt-[50px] pb-[50px]">
-                {/* swiper-wrapper */}
-                <div className="">
-                    {DataSlider.Slider.map((item) => {
-                        return(
-                            <SliderItem key = {item.id} item = {item} />
-                        )
-                    })}
+        <Swiper
+            effect = 'coverflow'
+            grabCursor = {true}
+            centeredSlides = {true}
+            slidesPerView="auto"
+            loop = {true}
+
+        >
+
+            <section className="relative w-full min-h-screen flex justify-center items-center bg-[#2197f3] overflow-hidden ">
+                {/* swiper-container   -> h */}
+                <div className="w-[400px]  pt-[50px] pb-[50px]">
+                    {/* swiper-wrapper */}
+                    <div className="">
+                        {DataSlider.Slider.map((item) => {
+                            return(
+                                <SliderItem key = {item.id} item = {item} />
+                            )
+                        })}
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </Swiper>
     )
 }
 
