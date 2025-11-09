b8bfeb5bf084e1bcc367738643ac7f03df9c6981
amir <amirtaki623@gmail.com>
2025-11-09 22:57:32 +0330

    day 32 part 39

--- src/component/sliderClient/index.jsx
+++ src/component/sliderClient/index.jsx
@@ -2,8 +2,8 @@ import SwiperComponent from "./sliderItem";
 
 const SliderClientSwiper = () => {
     return(
-    
-        <div className="">
+        // section
+        <div className="relative w-full min-h-screen flex justify-center items-center bg-[#2196f3] overflow-hidden!">
             <SwiperComponent />
         </div>
  
