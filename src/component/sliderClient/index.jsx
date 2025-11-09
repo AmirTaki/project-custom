055d0877ed5144a125c892502a55f7e809e1f716
amir <amirtaki623@gmail.com>
2025-11-09 21:06:35 +0330

    day 32 part 16

--- src/component/sliderClient/index.jsx
+++ src/component/sliderClient/index.jsx
@@ -30,6 +30,8 @@ const SliderClientSwiper = () => {
                                             src="https://plus.unsplash.com/premium_photo-1726873337697-b60c68e6f515?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cGVyc29uJTIwbXVzaWN8ZW58MHx8MHx8fDA%3D" alt="" 
                                         />
                                     </div>
+                                    <h3 className="text-[16px] font-[400] tracking-[1px] text-[#2196f3] leading-[1.1em]">
+                                        Someone Famous<br /> <span>Creative Designer</span></h3>
                                 </div>
                             </div>
                         </div>
