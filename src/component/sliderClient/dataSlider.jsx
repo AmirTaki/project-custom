180bd4fdce1c5f274cf495e917081342c94e1d9e
amir <amirtaki623@gmail.com>
2025-11-09 21:35:35 +0330

    day 32 part 19

--- src/component/sliderClient/dataSlider.jsx
+++ src/component/sliderClient/dataSlider.jsx
@@ -2,6 +2,7 @@ export const DataSlider = {
     Slider: [
         {
             id: 0,
+            img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHj-4qt5ZMM1om-SSRsIe50XzOyznYOz0ZTQ&s",
             
         }
     ]
