7913c3517e1bebb82a6090cb7aad368ac8c97d10
amir <amirtaki623@gmail.com>
2025-11-10 03:03:18 +0330

    day 33 part 3

--- src/component/imageSliderTime/dataImage.jsx
+++ src/component/imageSliderTime/dataImage.jsx
@@ -1,3 +1,8 @@
+import img1 from "./image/img1.jpg"
+import img2 from "./image/img2.jpg"
+import img3 from "./image/img3.jpg"
+import img4 from "./image/img4.jpg"
+import img5 from "./image/img5.jpg"
 export const DataImage = {
     images : [
         {
