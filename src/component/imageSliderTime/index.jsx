ea5feb0199f395fbd704ef494432793a6460e946
amir <amirtaki623@gmail.com>
2025-11-09 23:39:19 +0330

    day 32 part 46

--- src/component/imageSliderTime/index.jsx
+++ src/component/imageSliderTime/index.jsx
@@ -1,7 +1,10 @@
 const imgSliderTime = () => {
     return(
         // slider
-        <div className="w-[1300px] max-w-[100vw] h-[700px] m-auto relative overflow-hidden"></div>
+        <div className="w-[1300px] max-w-[100vw] h-[700px] m-auto relative overflow-hidden">
+            {/* list */}
+            <div className="absolute top-0 left-0 h-full flex duration-1000"></div>
+        </div>
     )
 }
 export default imgSliderTime;
