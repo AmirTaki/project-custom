35a3043d8587a6c7351c33e5683ae0e46fff1870
amir <amirtaki623@gmail.com>
2025-11-09 20:37:02 +0330

    day 32 part 5

--- src/component/sliderClient/index.jsx
+++ src/component/sliderClient/index.jsx
@@ -1,9 +1,10 @@
 const SliderClientSwiper = () => {
     return(
         // section
-        <section
-            className="relative w-full min-h-screen flex justify-center items-center bg-[#2197f3] overflow-hidden "
-        ></section>
+        <section className="relative w-full min-h-screen flex justify-center items-center bg-[#2197f3] overflow-hidden ">
+            {/* swiper-container */}
+            <div className="w-[400px] h-[500px] pt-[50px] pb-[50px]"></div>
+        </section>
     )
 }
 
