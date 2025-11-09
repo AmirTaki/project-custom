a472e4725a5b9826c62fc07b7231176a4b3d0f14
amir <amirtaki623@gmail.com>
2025-11-09 22:45:27 +0330

    day 32 part 36

--- src/component/sliderClient/dataSlider.jsx
+++ src/component/sliderClient/dataSlider.jsx
@@ -1,5 +1,5 @@
 export const DataSlider = {
-    Slider: [
+    SliderGroup: [
         {
             id: 0,
             img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHj-4qt5ZMM1om-SSRsIe50XzOyznYOz0ZTQ&s",
--- src/component/sliderClient/index.jsx
+++ src/component/sliderClient/index.jsx
@@ -26,9 +26,38 @@ const SliderClientSwiper = () => {
             modules={[EffectCoverflow, Pagination]}
             className="swiper-container bg-white"
         >
-            <SwiperSlide>Slide 1</SwiperSlide>
-            <SwiperSlide>Slide 2</SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
+            {DataSlider.SliderGroup.map((item) => (
+            <SwiperSlide className="w-[320px]  bg-[#d1ebff] shadow-[0_15px_50px_rgba(0,0,0,.2)] backdrop-blur-[4px] rounded-[10px] bg-center bg-cover">
+            {/* testimonialBox */}
+                <div className="relative w-full p-[40px] pt-[90px] text-[#999]">
+                    {/* img -> quote */}
+                    <img 
+                        className="absolute bottom-[5px] right-[3px] opacity-20"
+                        src={item.img} alt=""
+                    />
+                    {/* content */}
+                    <div className="">
+                        <p>
+                            {item.content}
+                        </p>
+                        {/* details */}
+                        <div className="flex items-center mt-[20px]">
+                            {/* imgBx */}
+                            <div className="relative w-[60px] h-[60px] rounded-full overflow-hidden mr-[10px]">
+                                <img 
+                                    className="absolute top-0 left-0 w-full h-full object-center"
+                                    src={item.imgDetails} alt="" 
+                                />
+                            </div>
+                            <h3 className="text-[16px] font-[400] tracking-[1px] text-[#2196f3] leading-[1.1em]">
+                                {item.title}<br /> <span className="text-[#666] text-[12px]">{item.job}</span>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+            </SwiperSlide>
+            ))}
+    
             {/* Add more slides as needed */}
             <div className="swiper-pagination" />
         </Swiper>
