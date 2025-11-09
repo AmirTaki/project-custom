a14b3a0bf30a249929df5427e89feaca8abb9747
amir <amirtaki623@gmail.com>
2025-11-09 23:10:16 +0330

    day 32 part 40

--- src/component/sliderClient/index.jsx
+++ src/component/sliderClient/index.jsx
@@ -3,8 +3,11 @@ import SwiperComponent from "./sliderItem";
 const SliderClientSwiper = () => {
     return(
         // section
-        <div className="relative w-full min-h-screen flex justify-center items-center bg-[#2196f3] overflow-hidden!">
-            <SwiperComponent />
+        <div className="relative w-full min-h-screen flex justify-center items-center bg-[#2196f3] ">
+            {/* swiper-wrapper */}
+            <div className="">
+                <SwiperComponent />
+            </div>
         </div>
  
     )
--- src/component/sliderClient/sliderItem.jsx
+++ src/component/sliderClient/sliderItem.jsx
@@ -2,10 +2,11 @@ import { Swiper,SwiperSlide } from 'swiper/react';
 import { DataSlider, } from "./dataSlider";
 import { EffectCoverflow, Pagination } from 'swiper/modules';
 import 'swiper/css';
+import './styles.css'
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 
-const SwiperComponent = ({item}) => {
+const SwiperComponent = () => {
     return(
         <Swiper
             effect="coverflow"
@@ -22,11 +23,11 @@ const SwiperComponent = ({item}) => {
                 slideShadows: true,
             }}
             modules={[EffectCoverflow, Pagination]}
-            className="swiper-container  "
+            className="swiper-container w-[400px] pt-[50px] pb-[50px] "
         >
             {DataSlider.SliderGroup.map((item) => {
                 return(
-                    <SwiperSlide className="w-[320px]  bg-[#d1ebff] shadow-[0_15px_50px_rgba(0,0,0,.2)] backdrop-blur-[4px] rounded-[10px] bg-center bg-cover">
+                    <SwiperSlide className="w-[320px]  bg-[#d1ebff] shadow-[0_15px_50px_rgba(0,0,0,.2)] blu rounded-[10px] bg-center! bg-cover!">
                     {/* testimonialBox */}
                         <div className="relative w-full p-[40px] pt-[90px] text-[#999]">
                             {/* img -> quote */}
