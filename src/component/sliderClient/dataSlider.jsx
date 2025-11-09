844b786ac794e77521774bf1936fee1a31e238c9
amir <amirtaki623@gmail.com>
2025-11-09 21:38:05 +0330

    day 32 part 20

--- src/component/sliderClient/dataSlider.jsx
+++ src/component/sliderClient/dataSlider.jsx
@@ -3,7 +3,19 @@ export const DataSlider = {
         {
             id: 0,
             img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHj-4qt5ZMM1om-SSRsIe50XzOyznYOz0ZTQ&s",
-            
-        }
+            content : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi similique optio ipsam reprehenderit, voluptatem iusto fuga vitae eligendi. Reprehenderit impedit sapiente eos laborum voluptatum soluta magni dicta alias. Saepe, illo.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia eius dolore officiis consequuntur voluptatum neque provident? Iure ducimus saepe eos, dolor magni reprehenderit in accusantium illo nobis laboriosam quae consequuntur.',
+            imgDetails : "https://media.istockphoto.com/id/917742572/photo/music-and-this-scenery-is-all-she-needs.webp?a=1&b=1&s=612x612&w=0&k=20&c=pldBK3XgkP3jnCijJDMY8pu10rvCnN3dx7EXi4Ft9O0=",
+            title : 'Someone Famous',
+            job : 'Creative Designer'
+        },
+        {
+            id: 0,
+            img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHj-4qt5ZMM1om-SSRsIe50XzOyznYOz0ZTQ&s",
+            content : 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi similique optio ipsam reprehenderit, voluptatem iusto fuga vitae eligendi. Reprehenderit impedit sapiente eos laborum voluptatum soluta magni dicta alias. Saepe, illo.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia eius dolore officiis consequuntur voluptatum neque provident? Iure ducimus saepe eos, dolor magni reprehenderit in accusantium illo nobis laboriosam quae consequuntur.',
+            imgDetails : "https://media.istockphoto.com/id/917742572/photo/music-and-this-scenery-is-all-she-needs.webp?a=1&b=1&s=612x612&w=0&k=20&c=pldBK3XgkP3jnCijJDMY8pu10rvCnN3dx7EXi4Ft9O0=",
+            title : 'Someone Famous',
+            job : 'Creative Designer'
+        },
+
     ]
 }
