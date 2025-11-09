592a8f070df635e2ce442c594969b9b7153b83cb
amir <amirtaki623@gmail.com>
2025-11-09 22:14:10 +0330

    day 32 part 31

--- src/component/sliderClient/index.jsx
+++ src/component/sliderClient/index.jsx
@@ -1,6 +1,10 @@
 import { Swiper } from 'swiper/react';
 import 'swiper/css';
 import { DataSlider } from "./dataSlider";
+import { EffectCoverflow, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
 import SliderItem from "./sliderItem";
 
 const SliderClientSwiper = () => {
@@ -12,21 +16,26 @@ const SliderClientSwiper = () => {
             slidesPerView="auto"
             loop = {true}
             pagination = {{el : '.swiper-pagination', clickable: true}}
-            
-
+            coverflowEffect={{
+                rotate : 0,
+                stretch: 0,
+                depth: 100,
+                modifier: 2,
+                slideShadows: true,
+            }}
+            modules={[EffectCoverflow, Pagination]}
         >
-
             <section className="relative w-full min-h-screen flex justify-center items-center bg-[#2197f3] overflow-hidden ">
                 {/* swiper-container   -> h */}
                 <div className="w-[400px]  pt-[50px] pb-[50px]">
                     {/* swiper-wrapper */}
-                    <div className="">
-                        {DataSlider.Slider.map((item) => {
-                            return(
-                                <SliderItem key = {item.id} item = {item} />
-                            )
-                        })}
-                    </div>
+                        <div className="swiper-container">
+                            {DataSlider.Slider.map((item) => {
+                                return(
+                                    <SliderItem key = {item.id} item = {item} />
+                                )
+                            })}
+                        </div>
                 </div>
             </section>
         </Swiper>
