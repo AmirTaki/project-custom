a4e09c07f667a1a36656d1997754af67af3c4d47
amir <amirtaki623@gmail.com>
2025-11-10 19:36:38 +0330

    day 33 part 34

--- src/component/imageSliderTime/index.jsx
+++ src/component/imageSliderTime/index.jsx
@@ -2,7 +2,7 @@ import { useEffect, useReducer, useRef } from "react";
 import { DataImage } from "./dataImage";
 
 const ImgSliderTime = () => {
-    const listRef = useRef(null)
+    const sliderRef = useRef(null)
     const reducer = (state, action) => {
         switch(action.type){
             case "right":
@@ -16,9 +16,9 @@ const ImgSliderTime = () => {
                 return {...state}
 
             case "changeImage":
-                if(listRef.current){
-                    listRef.current.style.scrollBehavior = "smooth"
-                    listRef.current.scrollLeft = state.index * listRef.current.offsetWidth;
+                if(sliderRef.current){
+                    sliderRef.current.style.scrollBehavior = "smooth"
+                    sliderRef.current.scrollLeft = state.index * sliderRef.current.offsetWidth;
                 }
 
                 return {...state}
@@ -33,7 +33,7 @@ const ImgSliderTime = () => {
         <div  className=" w-[1300px] max-w-[97vw]  h-[600px] m-auto relative bg-yellow-500 overflow-hidden top-10 max-md:h-[400px]! ">
            {/* list */}
            <div
-                ref = {listRef} 
+                ref = {sliderRef} 
                 className="absolute top-0 left-0 w-full h-full bg-amber-600 flex flex-col flex-wrap overflow-x-scroll"
             >
                {state.images.map((item) => (
@@ -46,20 +46,9 @@ const ImgSliderTime = () => {
                 ))}
         
            </div>
-           
-            {/* list */}
-            {/* <div className="absolute top-0 left-0 w-full h-full bg-red-500 flex flex-col flex-wrap  duration-1000 max-w-max">
-                {state.images.map((item) => (
-                    // item
-                    <div key = {item.id}  className="w-full h-full bg-blue-500">
-                        <img 
-                            className=" w-full h-full"
-                            src={item.img} alt="" />
-                    </div>
-                ))}
-            </div> */}
+
             {/* buttons */}
-            <div className="absolute top-[45%] left-[5%] w-[90%] flex justify-between ">
+            <div className="absolute top-[45%] left-[5%] w-[90%] flex justify-between cursor-pointer! ">
                 {state.buttons.map((item) => (
                     <button 
                         key = {item.id}  
@@ -74,8 +63,9 @@ const ImgSliderTime = () => {
             <ul className="absolute bottom-[10px] text-white left-0 w-full m-0 p-0 flex justify-center">
                 {state.images.map((item) => (
                     <li 
+                        onClick={() => {dispatch({type : 'dots', payload : {place : state.index}})}}
                         key = {item.id}
-                        className={`${item.id == state.index? "w-[30px] bg-[rgba(255,255,255,.6)]" : "w-[10px] bg-white"} list-none  h-[10px]  m-[20px] rounded-[20px] duration-1000 hover:scale-110! cursor-pointer`}    
+                        className={`${item.id == state.index? "w-[30px] bg-[rgba(255,255,255,.6)]" : "w-[10px] bg-white"} list-none  h-[10px]  m-[20px] rounded-[20px] duration-1000 hover:scale-110! cursor-pointer!`}    
                     ></li>
                 ))}
             </ul>
