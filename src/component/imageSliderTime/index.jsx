9b00943d6227c3b97af40bf2a47808d4d0fdec40
amir <amirtaki623@gmail.com>
2025-11-10 19:17:03 +0330

    day 33 part 28

--- src/component/imageSliderTime/index.jsx
+++ src/component/imageSliderTime/index.jsx
@@ -2,10 +2,11 @@ import { useReducer, useRef } from "react";
 import { DataImage } from "./dataImage";
 
 const ImgSliderTime = () => {
+    const listRef = useRef(null)
     const reducer = (state, action) => {
         switch(action.type){
             case "right":
-                state.index = index + 1;
+                state.index = state.index + 1;
                 if(listRef.current){
                     listRef.current.style.scrollBehavior = "smooth"
                     listRef.current.scrollLeft = state.index * listRef.current.offsetWidth;
@@ -14,13 +15,12 @@ const ImgSliderTime = () => {
         }
     }
     const [state, dispatch] = useReducer (reducer, DataImage) ;
-    const listRef = useRef(null)
     return(
         // slider
-        <div className=" w-[1300px] max-w-[97vw]  h-[600px] m-auto relative bg-yellow-500 overflow-hidden top-10 max-md:h-[400px]! ">
+        <div  className=" w-[1300px] max-w-[97vw]  h-[600px] m-auto relative bg-yellow-500 overflow-hidden top-10 max-md:h-[400px]! ">
            {/* list */}
            <div
-                ref = {listRef}    
+                ref = {listRef} 
                 className="absolute top-0 left-0 w-full h-full bg-amber-600 flex flex-col flex-wrap overflow-x-scroll"
             >
                {state.images.map((item) => (
