1d20b6636761c5956b0c4fdb166a4cd3eb60cb61
amir <amirtaki623@gmail.com>
2025-11-10 19:11:49 +0330

    day 33 part 26

--- src/component/imageSliderTime/index.jsx
+++ src/component/imageSliderTime/index.jsx
@@ -1,19 +1,23 @@
-import { useReducer } from "react";
+import { useReducer, useRef } from "react";
 import { DataImage } from "./dataImage";
 
 const ImgSliderTime = () => {
     const reducer = (state, action) => {
         switch(action.type){
-            case "":
+            case "right":
                 return {...state}
         }
     }
     const [state, dispatch] = useReducer (reducer, DataImage) ;
+    const listRef = useRef(null)
     return(
         // slider
         <div className=" w-[1300px] max-w-[97vw]  h-[600px] m-auto relative bg-yellow-500 overflow-hidden top-10 max-md:h-[400px]! ">
            {/* list */}
-           <div className="absolute top-0 left-0 w-full h-full bg-amber-600 flex flex-col flex-wrap overflow-x-scroll">
+           <div
+                ref = {listRef}    
+                className="absolute top-0 left-0 w-full h-full bg-amber-600 flex flex-col flex-wrap overflow-x-scroll"
+            >
                {state.images.map((item) => (
                     // item
                     <div key = {item.id}  className="w-full h-full bg-blue-500">
