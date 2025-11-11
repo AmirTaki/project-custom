df14622863803d4ec8466ac9b0355251f338c782
amir <amirtaki623@gmail.com>
2025-11-11 21:21:12 +0330

    day 34 part 16

--- src/component/changeSlider.jsx/index.jsx
+++ src/component/changeSlider.jsx/index.jsx
@@ -2,6 +2,12 @@ import { useReducer } from "react";
 import { InitailState } from "./initialState";
 
 const ChangeSlider = () => {
+    const reduce = (state, action) => {
+        switch(action.type){
+            case "": 
+                return {...state}
+        }
+    }
     const [state, dispatch] =  useReducer(reducer, InitailState)
     return(
         <div className=""></div>
