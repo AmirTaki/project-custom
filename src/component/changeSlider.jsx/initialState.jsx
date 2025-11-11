861c6366798d6346f5d2442249c5b3817819277b
amir <amirtaki623@gmail.com>
2025-11-11 21:19:17 +0330

    day 34 part 14

--- src/component/changeSlider.jsx/initialState.jsx
+++ src/component/changeSlider.jsx/initialState.jsx
@@ -1,8 +1,35 @@
+import c1 from "./image/c1.jpg"
+import c2 from "./image/c2.jpg"
+import c3 from "./image/c3.jpg"
+import c4 from "./image/c4.jpg"
+import c5 from "./image/c5.jpg"
+import c6 from "./image/c6.jpg"
 export const InitailState = {
     images : [
-        {id : ,
-        img : ,
+        {
+            id : 0,
+            img : c1,
         },
-        
+        {
+            id : 1,
+            img : c2,
+        },
+        {
+            id : 2,
+            img : c3 ,
+        },
+        {
+            id : 3,
+            img : c4,
+        },
+        {
+            id : 4,
+            img : c5,
+        },
+        {
+            id : 5,
+            img : c6,
+        },
+
     ]
 }
