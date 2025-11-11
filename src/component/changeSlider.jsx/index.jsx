22ae19c17b868eb6705168cb0e243a39421c33d6
amir <amirtaki623@gmail.com>
2025-11-11 22:43:39 +0330

    day 34 part 38

--- src/component/changeSlider.jsx/index.jsx
+++ src/component/changeSlider.jsx/index.jsx
@@ -38,10 +38,10 @@ const ChangeSlider = () => {
 
         // </div>
 
-        <div className="relative bg-gray-300 w-full h-[600px] top-20">
-            <div className="absolute w-[300px] h-[300px] top-50 translate-x-1/2 bg-blue-500"></div>
-            <div className="absolute w-[300px] h-[300px] top-50 rotate-y-60 -left-18.5 bg-red-500"></div>
-            <div className="absolute w-[300px] h-[300px] top-50 rotate-y-60 left-93 bg-green-500"></div>
+        <div className="relative bg-gray-300 w-full h-[600px] top- flex flex-row justify-center items-center">
+            <div className="bg-blue-500 w-[300px] h-[300px]"></div>
+            <div className="bg-gray-500 w-[300px] h-[300px]"></div>
+            <div className="bg-red-500 w-[300px] h-[300px]"></div>
         </div>
     )
 }
