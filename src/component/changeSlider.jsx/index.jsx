6f29bd1326217d50af91175f20e2a6615fea162c
amir <amirtaki623@gmail.com>
2025-11-11 21:33:59 +0330

    day 34 part 21

--- src/component/changeSlider.jsx/index.jsx
+++ src/component/changeSlider.jsx/index.jsx
@@ -10,18 +10,12 @@ const ChangeSlider = () => {
     }
     const [state, dispatch] =  useReducer(reducer, InitailState)
     return(
-        <div className="w-full h-[500px] bg-gray-600 ">
-            <div className="w-full h-full flex flex-col flex-wrap overlfow-x-scroll">
-                <div className="w-[300px] h-full bg-blue-600"></div>
-                <div className="w-[300px] h-full bg-green-600"></div>
-                <div className="w-[300px] h-full bg-yellow-600"></div>
-                <div className="w-[300px] h-full bg-red-600"></div>
-                <div className="w-[300px] h-full bg-pink-600"></div>
-            </div>
-            {/* <img src={state.images[0].img} alt="" />
-            <img src={state.images[1].img} alt="" />
-            <img src={state.images[2].img} alt="" /> */}
+        <div className="w-full h-[500px] bg-gray-600 relative top-10 flex justify-center items-center">
+            <div className="w-full h-[300px] bg-red-500"></div>
         </div>
     )
 }
 export default ChangeSlider;
+{/* <img src={state.images[0].img} alt="" />
+<img src={state.images[1].img} alt="" />
+<img src={state.images[2].img} alt="" /> */}
