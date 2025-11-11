8bcad18f092603907df592821fa6ef843258774e
amir <amirtaki623@gmail.com>
2025-11-12 00:11:33 +0330

    day 34 part 55

--- src/component/changeSlider.jsx/index.jsx
+++ src/component/changeSlider.jsx/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useReducer, useRef, useState } from "react";
 import { InitailState } from "./initialState";
 import './styles.css'
+import img1 from "./image/c1.jpg"
 
 const ChangeSlider = () => {
     const imageRef =  useRef(null)
@@ -8,18 +9,12 @@ const ChangeSlider = () => {
     const reducer = (state, action) => {
         switch(action.type){
             case "changeBgImg": 
-                imageRef.current.style.background = url(state.images[0].img)
+                // imageRef.current.style.background = url(state.images[0].img)
                 return {...state}
         }
     }
     const [state, dispatch] =  useReducer(reducer, InitailState)
-    const handlerClick = () => {
-        setIndex((prev) => (prev + 1))
-        if(imageRef.current){
-                imageRef.current.style.scrollBehavior = "smooth";
-                imageRef.current.scrollLeft = index * imageRef.current.offsetWidth;
-        }
-    }
+  
 
     useEffect(() => {
         const timer =  setInterval(() => {
@@ -33,7 +28,9 @@ const ChangeSlider = () => {
         
         <div className="relative">
 
-        <div ref = {imageRef} className="absolute bg-gray-300 w-[300px] h-[600px]  top-[50%] left-[50%] ml-[-150px] mt-[-150px] flex flex-col flex-wrap overflow-x-scroll justify-center items-center">
+        <div
+            style={{background : `url(${img1})`}}
+            ref = {imageRef} className="absolute bg-gray-300 w-[300px] h-[600px]  top-[50%] left-[50%] ml-[-150px] mt-[-150px] flex flex-col flex-wrap overflow-x-scroll justify-center items-center">
             
         
 
