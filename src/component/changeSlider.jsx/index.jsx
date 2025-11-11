671bd25f70a882edd31a7e2fe0e4683b0951cb1b
amir <amirtaki623@gmail.com>
2025-11-11 21:20:29 +0330

    day 34 part 15

--- src/component/changeSlider.jsx/index.jsx
+++ src/component/changeSlider.jsx/index.jsx
@@ -1,4 +1,8 @@
+import { useReducer } from "react";
+import { InitailState } from "./initialState";
+
 const ChangeSlider = () => {
+    const [state, dispatch] =  useReducer(reducer, InitailState)
     return(
         <div className=""></div>
     )
