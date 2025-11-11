5f39483f9ee8332d48af18248ccea31379fb7e7d
amir <amirtaki623@gmail.com>
2025-11-11 21:26:11 +0330

    day 34 part 18

--- src/component/changeSlider.jsx/index.jsx
+++ src/component/changeSlider.jsx/index.jsx
@@ -10,7 +10,18 @@ const ChangeSlider = () => {
     }
     const [state, dispatch] =  useReducer(reducer, InitailState)
     return(
-        <div className="w-full h-[500px] bg-gray-600"></div>
+        <div className="w-full h-[500px] bg-gray-600">
+            <div className="w-full h-full flex">
+                <div className="w-[300px] h-full bg-blue-600"></div>
+                <div className="w-[300px] h-full bg-green-600"></div>
+                <div className="w-[300px] h-full bg-yellow-600"></div>
+                <div className="w-[300px] h-full bg-red-600"></div>
+                <div className="w-[300px] h-full bg-pink-600"></div>
+            </div>
+            {/* <img src={state.images[0].img} alt="" />
+            <img src={state.images[1].img} alt="" />
+            <img src={state.images[2].img} alt="" /> */}
+        </div>
     )
 }
 export default ChangeSlider;
