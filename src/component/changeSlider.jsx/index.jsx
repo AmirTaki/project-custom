850f4edd45278faf35be4a29ed8b02560ae8a0ac
amir <amirtaki623@gmail.com>
2025-11-11 22:22:13 +0330

    day 34 part 28

--- src/component/changeSlider.jsx/index.jsx
+++ src/component/changeSlider.jsx/index.jsx
@@ -2,6 +2,7 @@ import { useReducer } from "react";
 import { InitailState } from "./initialState";
 
 const ChangeSlider = () => {
+    const imageRef =  useRef(null)
     const reducer = (state, action) => {
         switch(action.type){
             case "": 
@@ -11,14 +12,17 @@ const ChangeSlider = () => {
     const [state, dispatch] =  useReducer(reducer, InitailState)
     return(
         <div className="w-full h-[500px] bg-gray-600 relative top-10 flex justify-center items-center">
-            <div className="w-full h-[300px] bg-white overflow-x-scroll flex flex-col flex-wrap">
-                <div className="w-[10%] h-full bg-green-500 rotate-y-45">
+            <div
+                ref = {imageRef}
+                className="w-full h-[300px] bg-white overflow-x-scroll flex flex-col flex-wrap"
+            >
+                <div className="w-[10%] h-full bg-green-500 rotate-y-45 item">
                     <img src={state.images[0].img} className="w-full h-full" alt="" />
                 </div>
                 <div className="w-[80%] h-full bg-blue-500">
-                    <img src={state.images[1].img} className="w-full h-full" alt="" />
+                    <img src={state.images[1].img} className="w-full h-full item" alt="" />
                 </div>
-                <div className="w-[10%] h-full bg-orange-500 rotate-y-45">
+                <div className="w-[10%] h-full bg-orange-500 rotate-y-45 item">
                     <img src={state.images[2].img} className="w-full h-full" alt="" />
                 </div>
             
