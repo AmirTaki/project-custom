a4a2112072731a5c3eba58b5057ddf9ad412e986
amir <amirtaki623@gmail.com>
2025-11-12 23:26:29 +0330

    day 35 part 48

--- src/component/sliderSwiper/index.jsx
+++ src/component/sliderSwiper/index.jsx
@@ -3,7 +3,10 @@ const SliderSwiper = () => {
         // cubeBox
         <div className="w-full h-screen bg-blue-600 overflow-hidden relative">
             {/* container */}
-            <div className="w-[600px] h-[600px] bg-red-500 absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 flex justify-center items-center perspective-[1000px]"></div>
+            <div className="w-[600px] h-[600px] bg-red-500 absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 flex justify-center items-center perspective-[1000px]">
+                {/* box */}
+                <div className="absolute w-[350px] h-[350px] bg-green-600 transform-3d duration-1000 ease-in-out"></div>
+            </div>
         </div>
     )
 }
