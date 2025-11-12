b296ec31736d098422f97d7bfddbb778f1f0d1a6
amir <amirtaki623@gmail.com>
2025-11-12 21:23:21 +0330

    day 35 part 31

--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -62,7 +62,7 @@ const Home =  ()  => {
     const isDraggingNavigation = useRef(false)
     
     // veiw projects 
-    const  [view, setView] =   useState(false)   
+    const  [view, setView] =   useState(true)   
     return(
         <div 
         className={`bg-[#10131c]   min-h-[100vh] ${view ? "" : "flex! flex-col justify-center! items-center! "}`} 
@@ -200,14 +200,15 @@ const Home =  ()  => {
                 {/* change slider swiper */}
                 <ChangeSlider />
 
+                {/* phonto cube 3D */}
+                <PhotoCube />
+
                 {/* footer animated */}
                 <FooterAnimated />
 
             </div>
 
             <SliderSwiper   /> 
-            {/* phonto cube 3D */}
-            <PhotoCube />
             {/* <SwiperTest /> */}
             <div className="h-100 w-100 text-white"></div>
         </div>   
--- src/component/photoCube/index.jsx
+++ src/component/photoCube/index.jsx
@@ -4,6 +4,7 @@ import './styles.css'
 
 const PhotoCube = () => {   
     const box =  useRef(null)
+    const cubeBox = useRef(null)
     const reducer = (state, action) => {
         switch(action.type){
             case "":
@@ -14,35 +15,38 @@ const PhotoCube = () => {
 
 
     useEffect(() => {
-        const rotateCube = (x, y) => {
-            let Xvalue = Math.floor((x / 2) + 100)
-            let Yvalue = Math.floor((y / 2) + 100)
-            if(box.current) {
-                box.current.style.transform = `rotateX(${Yvalue}deg) rotateY(${Xvalue}deg)`
+        if(cubeBox.current){
+
+            const rotateCube = (x, y) => {
+                let Xvalue = Math.floor((x / 2) + 100)
+                let Yvalue = Math.floor((y / 2) + 100)
+                if(box.current) {
+                    box.current.style.transform = `rotateX(${Yvalue}deg) rotateY(${Xvalue}deg)`
+                }
+    
             }
-
-        }
-        document.documentElement.addEventListener('mousemove', (e) => {
-            rotateCube(e.clientX, e.clientY)
-        })
-
-        document.documentElement.addEventListener("mousedown", (e) => {
-            rotateCube(105, -150)
-
-        })
-
-        return() => {
-            document.documentElement.removeEventListener('mousemove', (e) => {
-            rotateCube(e.clientX, e.clientY)
-
-            document.documentElement.removeEventListener("mousedown", (e) => {
-                rotateCube(-105, 100)
+            cubeBox.current.addEventListener('mousemove', (e) => {
+                rotateCube(e.clientX, e.clientY)
             })
-        })
+    
+           cubeBox.current.addEventListener("mousedown", (e) => {
+                rotateCube(105, -150)
+    
+            })
+    
+            return() => {
+                cubeBox.removeEventListener('mousemove', (e) => {
+                rotateCube(e.clientX, e.clientY)
+    
+                cubeBox.removeEventListener("mousedown", (e) => {
+                    rotateCube(-105, 100)
+                })
+            })
+            }
         }
     }, [])
     return(
-        <div className="cubeBox">
+        <div ref = {cubeBox} className="cubeBox">
             {/* container */}
             <div className="container">
                 <div ref = {box} className="box">
@@ -61,8 +65,5 @@ const PhotoCube = () => {
     )
 }
 export default PhotoCube;
-// {cube.lists.map((list) => (
-//     <span
-//         className='h-[calc(350px/ 3 - 6px)] bg-white'
-//     ></span>
-// ))}
+
+///  https://www.youtube.com/watch?v=JT8IqoCbcyo
