7c3c0daa93329e607dd11d808d2b0ea4488c1581
amir <amirtaki623@gmail.com>
2025-11-12 23:39:40 +0330

    day 35 part 56

--- src/component/sliderSwiper/index.jsx
+++ src/component/sliderSwiper/index.jsx
@@ -5,11 +5,11 @@ const SliderSwiper = () => {
    const [cubes, dispatch] =  useReducer(ReducerSwiperCube, InitialStateCubeSiwper )
     return(
         // cubeBox
-        <div className="w-full h-screen bg-blue-600 overflow-hidden relative">
+        <div className="w-full h-screen  overflow-hidden relative">
             {/* container */}
-            <div className="w-[600px] h-[600px] bg-red-500 absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 flex justify-center items-center perspective-[1000px]">
+            <div className="w-[600px] h-[600px] absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 flex justify-center items-center perspective-[1000px]">
                 {/* box */}
-                <div className="absolute w-[350px] h-[350px] bg-green-600 transform-3d duration-1000 ease-in-out">
+                <div className="absolute w-[350px] h-[350px]  transform-3d duration-1000 ease-in-out rotate-x-360 rotate-y-180 -rotate-z-45">
                     {/* cubs images */}
                     {cubes.images.map((cube) => (
                         <div 
