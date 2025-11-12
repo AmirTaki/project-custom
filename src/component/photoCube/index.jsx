687237fe7cae05ee2f030461fa0173a4264f9a82
amir <amirtaki623@gmail.com>
2025-11-12 20:50:25 +0330

    day 35 part 32

--- src/component/photoCube/index.jsx
+++ src/component/photoCube/index.jsx
@@ -19,11 +19,13 @@ const PhotoCube = () => {
                         <div 
                             style={{backgroundColor : `#${item.color}`}}
                             key = {item.id} 
-                            className={`${item.name} w-[350px]  h-[350px] absolute grid grid-cols-[repeat(auto-fill,minmax(calc(350px / 4), 1fr))] gap-[4px]`}
+                            className={`${item.name} w-[350px]  h-[350px] absolute grid grid-cols-[repeat(auto-fill,minmax(calc(350px / 4), 1fr))] gap-[4px] p-[4px]`}
                         >
                             {item.name}
                             {cube.lists.map((list) => (
-                                <span></span>
+                                <span
+                                    className='h-[calc(350px/ 3 - 6px)] bg-white'
+                                ></span>
                             ))}
                         </div>
                     ))}
