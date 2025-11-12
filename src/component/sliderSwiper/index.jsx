d9b92303dd7e08ee1e194560bacd0bcaeee91d9c
amir <amirtaki623@gmail.com>
2025-11-12 23:22:59 +0330

    day 35 part 47

--- src/component/sliderSwiper/index.jsx
+++ src/component/sliderSwiper/index.jsx
@@ -1,7 +1,10 @@
 const SliderSwiper = () => {
     return(
         // cubeBox
-        <div className="w-full h-screen bg-blue-600 overflow-hidden relative"></div>
+        <div className="w-full h-screen bg-blue-600 overflow-hidden relative">
+            {/* container */}
+            <div className="w-[600px] h-[600px] bg-red-500 absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 flex justify-center items-center perspective-[1000px]"></div>
+        </div>
     )
 }
 export default SliderSwiper;
