12bd72438751516a081bcaa632f132a78ffa9267
amir <amirtaki623@gmail.com>
2025-11-12 23:46:07 +0330

    day 35 part 58

--- src/component/sliderSwiper/index.jsx
+++ src/component/sliderSwiper/index.jsx
@@ -10,13 +10,13 @@ const SliderSwiper = () => {
             {/* container */}
             <div className="w-[600px] h-[600px] absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 flex justify-center items-center perspective-[1000px]">
                 {/* box */}
-                <div className="absolute w-[350px] h-[350px]  transform-3d duration-1000 ease-in-out rotate-x-360 rotate-y-180 -rotate-z-45">
+                <div className="absolute w-[350px] h-[350px]  transform-3d duration-1000 ease-in-out   transform">
                     {/* cubs images */}
                     {cubes.images.map((cube) => (
                         <div 
                             style={{backgroundColor : `#${cube.color}`}}
                             key = {cube.id}
-                            className={`${cube.name} w-[350px] h-[350px] `}>
+                            className={`${cube.name} w-[350px] h-[350px] absolute `}>
                         </div>
                     ))}
                 </div>
