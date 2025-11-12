44805d267b5d9ce9cf9d09f9cc7dda0e65a7ec62
amir <amirtaki623@gmail.com>
2025-11-12 20:24:04 +0330

    day 35 part 23

--- src/component/photoCube/initalState.jsx
+++ src/component/photoCube/initalState.jsx
@@ -10,36 +10,42 @@ export const InitialStateCube = {
         {
             id : 0,
             name : 'front',
+            color : '#2db911',
             img : img1
 
         },
         {
             id : 1,
             name : 'back',
+            color : '#df980b',
             img : img2
 
         },
         {
             id : 2,
             name : 'right',
+            color : '#04a4f4',
             img : img3
 
         },
         {
             id : 3,
             name : 'left',
+            color : '#04f4a8',
             img : img4
 
         },
         {
             id : 4,
             name : 'top',
+            color : '#1932f3',
             img : img5
 
         },
         {
             id : 5,
             name : 'bottom',
+            color : '#bd0049',
             img : img6
 
         },
