8e248b330ef3872a2ac3f0a098b96e147181776f
amir <amirtaki623@gmail.com>
2025-11-12 19:17:42 +0330

    day 35 part 6

--- src/component/photoCube/initalState.jsx
+++ src/component/photoCube/initalState.jsx
@@ -1,4 +1,10 @@
 import img1 from "./image/c1.jpg"
+import img2 from "./image/c2.jpg"
+import img3 from "./image/c3.jpg"
+import img4 from "./image/c4.jpg"
+import img5 from "./image/c5.jpg"
+import img6 from "./image/c6.jpg"
+
 export const InitalState = {
     image : [
         {
