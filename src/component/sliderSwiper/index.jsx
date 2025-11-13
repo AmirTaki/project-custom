f394cc7057bdf87c38e9ff82b8730b6ef6f7e229
amir <amirtaki623@gmail.com>
2025-11-13 21:20:13 +0330

    day 36 part 11

--- src/component/sliderSwiper/index.jsx
+++ src/component/sliderSwiper/index.jsx
@@ -7,32 +7,32 @@ const SliderSwiper = () => {
    const [cubes, dispatch] =  useReducer(ReducerSwiperCube, InitialStateCubeSiwper )
     return(
         // cubeBox
-        <div className="w-full h-screen  overflow-hidden relative flex justify-center itmes-center ">
+        <div className="w-full h-screen  overflow-hidden relative flex justify-center itmes-center">
             {/* container */}
-            <div className="w-full h-[500px] bg-amber-300    perspective-[1300px]">
+            <div className="w-[500px] h-[500px] bg-amber-300  flex justify-center items-center  perspective-[1300px]">
                 {/* box */}
                 <div 
                     style={{transform : `rotateY(${cubes.rotate}deg)`}}
-                    className="absolute w-[350px] h-[350px]  transform-3d duration-1000 ease-in-out  transform"
+                    className=" w-[350px] h-[350px]  transform-3d duration-1000 ease-in-out  transform"
                 >
                     {/* cubs images */}
                     {cubes.images.map((cube) => (
                         <div 
                             style={{backgroundColor : `#${cube.color}`}}
                             key = {cube.id}
-                            className={`${cube.name} w-[400px] h-[400px] absolute  overflow-hidden  `}
+                            className={`${cube.name} w-[400px] h-[400px]   absolute overflow-hidden  `}
                         >
                             <img src={cube.img} className="w-full h-full bg-cover" alt="" />
                         </div>
                     ))}
                 </div>
 
-                <button className="bg-transparent z-50 text-red-700" onClick={() => {dispatch({type: "handlerCubes", })}}>click me</button>
-                {/* button cubs */}
-                <div className=" z-50 absolute -right-24 top-80 duration-500 hover:scale-120!"><i className="text-4xl bi bi-chevron-double-right text-gray-300 hover:text-blue-700 duration-500 cursor-pointer"></i></div>
-                <div className=" z-50 absolute -left-10 top-80 duration-500 hover:scale-120!"><i className="text-4xl bi bi-chevron-double-left text-gray-300 hover:text-blue-700 duration-500 cursor-pointer"></i></div>
+                <button className="bg-blue-500 z-50! text-red-700 absolute" onClick={() => {dispatch({type: "handlerCubes", })}}>click me</button>
 
             </div>
+            {/* button cubs */}
+            <i className="text-4xl bi bi-chevron-double-right text-gray-300 hover:text-blue-700 duration-500 cursor-pointer"></i>
+            <i className="text-4xl bi bi-chevron-double-left text-gray-300 hover:text-blue-700 duration-500 cursor-pointer"></i>
                     
     </div>
     )
