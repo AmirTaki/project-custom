446b4a23852e72a157f43fda8e29bcd3387a496f
amir <amirtaki623@gmail.com>
2025-11-13 23:42:00 +0330

    day 36 part 45

--- src/component/sliderSwiper/initialState.jsx
+++ src/component/sliderSwiper/initialState.jsx
@@ -1,6 +1,6 @@
 export const Items = Array.from({length : 500} ,(_,i)=> `img: ${i + 1}`)
 
-
+Object.from({length: 500}, (_, i) => `id : ${i} img : ${i+100} ` )
 export const InitalStateItems = {
     images : [
         Items
