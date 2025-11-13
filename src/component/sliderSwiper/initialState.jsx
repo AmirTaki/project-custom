4798e84683e905a158e864dbca91f1426acfba32
amir <amirtaki623@gmail.com>
2025-11-14 00:11:07 +0330

    day 36 part 59

--- src/component/sliderSwiper/initialState.jsx
+++ src/component/sliderSwiper/initialState.jsx
@@ -9,6 +9,20 @@
 // }
 
 
+
+import img1 from "./images/img1"
+import img2 from "./images/img2"
+import img3 from "./images/img3"
+import img4 from "./images/img4"
+import img5 from "./images/img5"
+import img6 from "./images/img6"
+import img7 from "./images/img7"
+import img8 from "./images/img8"
+import img9 from "./images/img9"
+
+
+
+
 const Images = Array.from({length : 500}, (_, i) => ({
     id : i ,
     src : `src ${i + 1}` 
