1b864465e0b0269919e5f170995b4fa858c25c27
amir <amirtaki623@gmail.com>
2025-11-17 20:23:23 +0330

    day 40 part 6

--- src/component/moveSliderTouch/index.jsx
+++ src/component/moveSliderTouch/index.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
+import { InitailDataFlowSlider } from "./InitialData";
+
 const MoveSliderTouch = () => {
+    const [state, setState]  = useState(InitailDataFlowSlider)
+    console.log(state.images)
     return(
         // flex
         <div className=" bg-white flex justify-center items-center">
@@ -6,10 +11,21 @@ const MoveSliderTouch = () => {
             <div className="">◀️</div>
 
             {/* sliderWrapper  */}
-            <div className="w-[800px] h-[400px] bg-blue-800 relative ">
+            <div className="w-[800px] h-[400px]  relative ">
 
                 {/* slider-container */}
-                
+                <div className="flex ">
+
+                    {/* slide */}
+                    {state.images.map((item) => (
+                        <div
+                            style={{backgroundImage: `url(${item.img})`}}
+                            key = {item.id}
+                            className="flex-[0_0_100%] h-[400px] bg-cover bg-center"
+                        ></div>
+                    ))}
+                </div>
+
             </div>
 
             {/* right button */}
