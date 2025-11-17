7028d844fa4212ec6dcdb028e388ce0a558dc77f
amir <amirtaki623@gmail.com>
2025-11-17 20:16:42 +0330

    day 40 part 5

---
+++ src/component/moveSliderTouch/InitialData.jsx
@@ -0,0 +1,66 @@
+import img1 from "./images/pins1.jpg"
+import img2 from "./images/pins2.jpg"
+import img3 from "./images/pins3.jpg"
+import img4 from "./images/pins4.jpg"
+import img5 from "./images/pins5.jpg"
+import img6 from "./images/pins6.jpg"
+import img7 from "./images/pins7.jpg"
+import img8 from "./images/pins8.jpg"
+import img9 from "./images/pins9.jpg"
+import img10 from "./images/pins10.jpg"
+
+export const InitailDataFlowSlider = {
+
+    images : [
+        {
+            id : 0, 
+            img : img1,
+        }
+        ,
+        {
+            id : 1, 
+            img : img2,
+        }
+        ,
+        {
+            id : 2, 
+            img : img3,
+        }
+        ,
+        {
+            id : 3, 
+            img : img4,
+        }
+        ,
+        {
+            id : 4, 
+            img : img5,
+        }
+        ,
+        {
+            id : 5, 
+            img : img6,
+        }
+        ,
+        {
+            id : 6, 
+            img : img7,
+        }
+        ,
+        {
+            id : 7, 
+            img : img8,
+        }
+        ,
+        {
+            id : 8, 
+            img : img9,
+        }
+        ,
+        {
+            id : 9, 
+            img : img10,
+        }
+        ,
+    ]
+}
