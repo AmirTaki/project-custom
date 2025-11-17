3d101ccf1002cb564646810a5e43910f14942511
amir <amirtaki623@gmail.com>
2025-11-17 20:29:26 +0330

    day 40 part 8

--- src/component/moveSliderTouch/index.jsx
+++ src/component/moveSliderTouch/index.jsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { InitailDataFlowSlider } from "./InitialData";
 
 const MoveSliderTouch = () => {
     const [state, setState]  = useState(InitailDataFlowSlider)
-    console.log(state.images)
+    const [index, setIndex] = useState(0)
+
+
+    const goToNext = useCallback((n = 1) => {
+        const newIndex = index + n > state.images.length - 1 ? 0 : index + n
+        setIndex(newIndex)
+    }, [state.images, index])
     return(
         // flex
         <div className=" bg-white flex justify-center items-center">
@@ -14,7 +20,10 @@ const MoveSliderTouch = () => {
             <div className="w-[800px] h-[400px]  relativ overflow-hidden ">
 
                 {/* slider-container */}
-                <div className="flex ">
+                <div 
+                    style={{transform : `translateX(-${index * 100}%)`}}
+                    className="flex cursor-grab transition-transform duration-500 ease-in-out"
+                >
 
                     {/* slide */}
                     {state.images.map((item) => (
@@ -29,7 +38,10 @@ const MoveSliderTouch = () => {
             </div>
 
             {/* right button */}
-            <div className="">▶️</div>
+            <div 
+                onClick={() => {goToNext()}}
+                className="cursor-pointer"
+            >▶️</div>
         </div>
     )
 }
