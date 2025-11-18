a07a53ef6fe2fefb05cb78a51e4adb66eee838d7
amir <amirtaki623@gmail.com>
2025-11-18 21:25:24 +0330

    day 41 part 2

--- src/component/moveSliderTouch/index.jsx
+++ src/component/moveSliderTouch/index.jsx
@@ -4,7 +4,9 @@ const MoveSliderTouch = () => {
  
 
     return(
-        <div className=""></div>
+        <div className="w-full flex flex-col items-center justify-center gap-8">
+            <div className=""></div>
+        </div>
     )
 }
 export default MoveSliderTouch;
