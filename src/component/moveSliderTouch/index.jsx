120ace0e98ef4989c1ce7448d8252960d8b34ed8
amir <amirtaki623@gmail.com>
2025-11-18 21:27:11 +0330

    day 41 part 3

--- src/component/moveSliderTouch/index.jsx
+++ src/component/moveSliderTouch/index.jsx
@@ -5,7 +5,15 @@ const MoveSliderTouch = () => {
 
     return(
         <div className="w-full flex flex-col items-center justify-center gap-8">
-            <div className=""></div>
+            <div 
+                className="relative overflow-hidden cursor-grab active:cursor-grabbing"
+                style={{
+                    width : `${}px`,
+                    height: `${}px`
+                }}
+           >
+
+            </div>
         </div>
     )
 }
