d23a661ca7fa066b1e5d55062754ef7a951932af
amir <amirtaki623@gmail.com>
2025-11-19 01:32:49 +0330

    day 42 part 5

--- src/component/moveSliderTouch/index.jsx
+++ src/component/moveSliderTouch/index.jsx
@@ -2,9 +2,11 @@ import { useCallback, useEffect, useEffectEvent, useMemo, useRef, useState } fro
 import { InitailDataFlowSlider as ImagesFlow } from "./InitialData";
 
 const MoveSliderTouch = () => {
+    const slider = useRef(null)
     const checkDrag =  useRef(false)
     const start = useRef(0)
     const move = useRef(0)
+
 
     const handlerDown = (event) => {
     checkDrag.current = true
@@ -17,6 +19,10 @@ const MoveSliderTouch = () => {
         if(!checkDrag.current) return ;
         move.current = event
         const drag = move - start.current
+        if(slider.current){
+            slider.current.style.scrollBehavior = "smooth"
+            slider.current.scrollLeft += drag
+        }
    }
 
     const handlerUp = (event) => {
@@ -26,10 +32,11 @@ const MoveSliderTouch = () => {
     }
     return(
       <div 
+        ref = {slider}
         onMouseDown={(e) => {handlerDown(e.clientX)}}
         onMouseUp={(e) => {handlerUp(e.clientX)}}
         onMouseMove = {(e) => {handlerMove(e.clientX)}}
-        className="w-[90vw] h-[40vh] bg-white mt-10 relative flex flex-col flex-wrap overflow-x-scroll ">
+        className="w-[90vw] h-[40vh] bg-white mt-10 relative flex flex-col flex-wrap overflow-x-scroll select-none ">
         <div className="w-50 h-full   bg-amber-300"></div>
         <div className="w-50 h-full   bg-green-300"></div>
         <div className="w-50 h-full  bg-red-300"></div>
