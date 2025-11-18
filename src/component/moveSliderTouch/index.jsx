563b2cd3327307766dfdb1ea799fd75b67a3733f
amir <amirtaki623@gmail.com>
2025-11-18 21:33:25 +0330

    day 41 part 4

--- src/component/moveSliderTouch/index.jsx
+++ src/component/moveSliderTouch/index.jsx
@@ -1,8 +1,18 @@
+import { useEffect, useMemo } from "react";
 
 const MoveSliderTouch = () => {
 
+    
  
+    const sliderWidth = useMemo(() => {
 
+    })
+
+    useEffect(() => {
+
+        window.addEventListener('resize', handlerResize)
+        return () => window.removeEventListener('resize', handlerResize)
+    })
     return(
         <div className="w-full flex flex-col items-center justify-center gap-8">
             <div 
