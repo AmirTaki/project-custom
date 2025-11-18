387277d50cd5aba3faf5970dccffd1891cc4fd7d
amir <amirtaki623@gmail.com>
2025-11-19 01:27:48 +0330

    day 42 part 3

--- src/component/moveSliderTouch/index.jsx
+++ src/component/moveSliderTouch/index.jsx
@@ -2,9 +2,28 @@ import { useCallback, useEffect, useEffectEvent, useMemo, useRef, useState } fro
 import { InitailDataFlowSlider as ImagesFlow } from "./InitialData";
 
 const MoveSliderTouch = () => {
-   
+    const checkDrag =  useRef(false)
+
+    const handlerDown = (event) => {
+    checkDrag.current = true
+
+    }
+
+    const handlerMove = (event) => {
+        if(!checkDrag.current) return ;
+        console.log(event)
+
+   }
+
+    const handlerUp = (event) => {
+        checkDrag.current = false
+    }
     return(
-      <div className="w-[90vw] h-[40vh] bg-white mt-10 relative flex flex-col flex-wrap ">
+      <div 
+        onMouseDown={(e) => {handlerDown(e.clientX)}}
+        onMouseUp={(e) => {handlerUp(e.clientX)}}
+        onMouseMove = {(e) => {handlerMove(e.clientX)}}
+        className="w-[90vw] h-[40vh] bg-white mt-10 relative flex flex-col flex-wrap overflow-x-scroll ">
         <div className="w-50 h-full   bg-amber-300"></div>
         <div className="w-50 h-full   bg-green-300"></div>
         <div className="w-50 h-full  bg-red-300"></div>
