1dbc2fe85f021513264e679dda8f7d0136912d04
amir <amirtaki623@gmail.com>
2025-11-19 01:57:59 +0330

    day 42 part 8

--- src/component/moveSliderTouch/index.jsx
+++ src/component/moveSliderTouch/index.jsx
@@ -19,13 +19,17 @@ const MoveSliderTouch = () => {
         if(!checkDrag.current) return ;
         move.current = event
         const drag = move.current - start.current
+        
+        const n = window.innerWidth < 780 ? 3 : 2
         if(slider.current){
             slider.current.style.scrollBehavior = "smooth"
             if(move.current > start.current){
-                slider.current.scrollLeft -= move.current
+
+                slider.current.scrollLeft -= move.current * n
+
             }
             else {
-                slider.current.scrollLeft += move.current
+                slider.current.scrollLeft += move.current * n
             }
         }
    }
@@ -41,17 +45,18 @@ const MoveSliderTouch = () => {
         onMouseDown={(e) => {handlerDown(e.clientX)}}
         onMouseUp={(e) => {handlerUp(e.clientX)}}
         onMouseMove = {(e) => {handlerMove(e.clientX)}}
+        onMouseLeave = {(e) => {handlerUp(e.clientX)}}
         className="w-[90vw] h-[40vh] bg-white mt-10 relative flex flex-col flex-wrap overflow-x-hidden select-none cursor-grab duration-1000 transition-all">
-        <div className="w-50 h-full   bg-amber-300"></div>
-        <div className="w-50 h-full   bg-green-300"></div>
-        <div className="w-50 h-full  bg-red-300"></div>
-        <div className="w-50 h-full  bg-blue-300"></div>
-        <div className="w-50 h-full  bg-pink-300"></div>
-        <div className="w-50 h-full   bg-amber-300"></div>
-        <div className="w-50 h-full   bg-green-300"></div>
-        <div className="w-50 h-full  bg-red-300"></div>
-        <div className="w-50 h-full  bg-blue-300"></div>
-        <div className="w-50 h-full  bg-pink-300"></div>
+        <div className="w-full h-full   bg-amber-300"></div>
+        <div className="w-full h-full   bg-green-300"></div>
+        <div className="w-full h-full  bg-red-300"></div>
+        <div className="w-full h-full  bg-blue-300"></div>
+        <div className="w-full h-full  bg-pink-300"></div>
+        <div className="w-full h-full   bg-amber-300"></div>
+        <div className="w-full h-full   bg-green-300"></div>
+        <div className="w-full h-full  bg-red-300"></div>
+        <div className="w-full h-full  bg-blue-300"></div>
+        <div className="w-full h-full  bg-pink-300"></div>
       </div>
     )
 }
