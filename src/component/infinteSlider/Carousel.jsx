934c88e364a0ee30851f9f7cd114900c644e05c5
amir <amirtaki623@gmail.com>
2025-11-19 23:03:10 +0330

    loop slider part 12

--- src/component/infinteSlider/Carousel.jsx
+++ src/component/infinteSlider/Carousel.jsx
@@ -3,15 +3,26 @@ import { useRef, useState } from "react";
 const Carousel = () => {
     const [isDragging, setIsDragging] = useState(false)
     const [isTranitioning, setIsTransitioning] = useState(false)
+    const [dragOffset, setDragOffset] = useState(0)
    
     const conatinerRef =  useRef(null)
     const startX = useRef(0)
+    const currentX = useRef(0)
 
     const handlerDragStart = (client) => {
         setIsDragging(true)
         startX.current  = client
         setIsTransitioning(false)
     }
+
+    const handlerDragMove = (client) => {
+        if(!isDragging) return;
+        currentX.current = client;
+        const diff = currentX.current - startX.current
+        setDragOffset(diff)        
+
+    }
+
     const onMouseDown = (e) => handlerDragStart(e.clientX)
     const onMouseMove = (e) => handlerDragMove(e.clientX)
     return(
