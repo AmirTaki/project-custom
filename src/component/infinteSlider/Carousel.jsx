84ab0cf2feb46332b7d969642567cb9143d099cd
amir <amirtaki623@gmail.com>
2025-11-19 22:55:44 +0330

    loop slider part 8

--- src/component/infinteSlider/Carousel.jsx
+++ src/component/infinteSlider/Carousel.jsx
@@ -1,6 +1,22 @@
+import { useRef } from "react";
+
 const Carousel = () => {
+    const conatinerRef =  useRef(null)
+   
+    const handlerDragStart = () => {
+        
+    }
+    const onMouseDown = (e) => handlerDragStart(e.clientX)
+
     return(
-        <div className="w-full max-w-4xl mx-auto relative group select-none" dir="ltr">carousel</div>
+        <div className="w-full max-w-4xl mx-auto relative group select-none" dir="ltr">
+            {/* viewport */}
+            <div 
+                ref = {conatinerRef}
+                onMouseDown={onMouseDown}
+                className="overflow-hidden rounded-2xl cursor-grab active:cursor-grabbing touch-pan-y"
+            ></div>
+        </div>
     )
 }
 export default Carousel;
