381aeb06ac3c9408e27ecca153dce02c52cbea45
amir <amirtaki623@gmail.com>
2025-11-19 23:07:59 +0330

    loop slider part 13

--- src/component/infinteSlider/Carousel.jsx
+++ src/component/infinteSlider/Carousel.jsx
@@ -8,6 +8,7 @@ const Carousel = () => {
     const conatinerRef =  useRef(null)
     const startX = useRef(0)
     const currentX = useRef(0)
+    const widthRef = useRef(0)
 
     const handlerDragStart = (client) => {
         setIsDragging(true)
@@ -20,11 +21,30 @@ const Carousel = () => {
         currentX.current = client;
         const diff = currentX.current - startX.current
         setDragOffset(diff)        
+    }
 
+    const handlerDragEnd = () => {
+        if(!isDragging) return;
+        setIsDragging(false)
+        
+        const threshold =  widthRef.current * .2
+
+        if(dragOffset > threshold){
+            
+        }
+        else if(dragOffset < -threshold){
+
+        }
+        else {
+            setIsTransitioning(true)
+        }
+        setDragOffset(0)
+        
     }
 
     const onMouseDown = (e) => handlerDragStart(e.clientX)
     const onMouseMove = (e) => handlerDragMove(e.clientX)
+    const onMouseUp = () => handlerDragEnd();
     return(
         <div className="w-full max-w-4xl mx-auto relative group select-none" dir="ltr">
             {/* viewport */}
@@ -32,6 +52,7 @@ const Carousel = () => {
                 ref = {conatinerRef}
                 onMouseDown={onMouseDown}
                 onMouseMove = {onMouseMove}
+                onMouseUp={onMouseUp}
                 className="overflow-hidden rounded-2xl cursor-grab active:cursor-grabbing touch-pan-y"
             ></div>
         </div>
