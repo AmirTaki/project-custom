c2520abc477b3f2b1c065d47b3301a270239adcb
amir <amirtaki623@gmail.com>
2025-11-19 23:00:27 +0330

    loop slider part 11

--- src/component/infinteSlider/Carousel.jsx
+++ src/component/infinteSlider/Carousel.jsx
@@ -13,13 +13,14 @@ const Carousel = () => {
         setIsTransitioning(false)
     }
     const onMouseDown = (e) => handlerDragStart(e.clientX)
-
+    const onMouseMove = (e) => handlerDragMove(e.clientX)
     return(
         <div className="w-full max-w-4xl mx-auto relative group select-none" dir="ltr">
             {/* viewport */}
             <div 
                 ref = {conatinerRef}
                 onMouseDown={onMouseDown}
+                onMouseMove = {onMouseMove}
                 className="overflow-hidden rounded-2xl cursor-grab active:cursor-grabbing touch-pan-y"
             ></div>
         </div>
