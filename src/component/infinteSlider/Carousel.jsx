505e2ba06c3959a3202b7f1dc54cfc24dc4fc6c1
amir <amirtaki623@gmail.com>
2025-11-20 19:59:41 +0330

    loop slider part 36

--- src/component/infinteSlider/Carousel.jsx
+++ src/component/infinteSlider/Carousel.jsx
@@ -158,6 +158,17 @@ const Carousel = ({items}) => {
                 </svg>
             </button>
 
+            <button
+                onClick={nextSlide}
+                className="absolute right-4 top-1/2 -translate-y-1/2 bg-white/10 backdrop-blue-md hover:bg-white/20 text-white p-3 rounded-full shadow-lg transition-all opacity-0
+                group-hover:opacity-100 focus:opacity-100 z-10 disabled:opacity-0"
+                disabled = {isTranitioning}
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                </svg>
+            </button>
+
         </div>
     )
 }
