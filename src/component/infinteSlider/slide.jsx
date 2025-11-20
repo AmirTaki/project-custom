e8363c1cf4ae8bb5d7a0f530b7b39c839a8a5f19
amir <amirtaki623@gmail.com>
2025-11-20 19:12:18 +0330

    loop slider part 30

--- src/component/infinteSlider/slide.jsx
+++ src/component/infinteSlider/slide.jsx
@@ -1,8 +1,17 @@
 const Slide = ({data, isActive}) => {
     return (
-        <div className={`relative w-full h-[400px] md:h-[500px] shrink-0  rounded-2xl overflow-hidden shadow-2xl transition-all duration-500 select-none
-            ${isActive ? 'scale-100 opacity-100' : 'scale-90 opacity-60'}
-            `}></div>
+        <div 
+            className={`relative w-full h-[400px] md:h-[500px] shrink-0  rounded-2xl overflow-hidden shadow-2xl transition-all duration-500 select-none
+                ${isActive ? 'scale-100 opacity-100' : 'scale-90 opacity-60'}`}
+            draggable = {false}    
+        >
+            <img 
+                src = {data.image}
+                alt = {data.title}
+                className="absolute inset-0 w-full h-full object-cover pointer-events-none"
+                loading="lazy"    
+            />
+        </div>
     )
 }
 export default Slide
