d342dd180a8cbffcf55040b34158e9f177bc0dcc
amir <amirtaki623@gmail.com>
2025-11-20 19:16:55 +0330

    loop slider part 33

--- src/component/infinteSlider/slide.jsx
+++ src/component/infinteSlider/slide.jsx
@@ -14,7 +14,9 @@ const Slide = ({data, isActive}) => {
             <div className={`absolute inset-0 bg-gradient-to-t ${data.color} opacity-80 mix-blend-multiply`}></div>
             <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-transparent to-transparent"></div>
 
-            <div className=""></div>
+            <div className="absolute bottom-0 right-0 p-8 text-right w-full transform transition-transform duration-500">
+                <h2 className={``}></h2>
+            </div>
         </div>
     )
 }
