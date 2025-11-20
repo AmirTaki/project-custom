d497c43c8d032f306d79dd94fa0c23f055b379d9
amir <amirtaki623@gmail.com>
2025-11-20 19:09:57 +0330

    loop slider part 29

--- src/component/infinteSlider/slide.jsx
+++ src/component/infinteSlider/slide.jsx
@@ -1,6 +1,8 @@
-const Slide = () => {
+const Slide = ({data, isActive}) => {
     return (
-        <div className=""></div>
+        <div className={`relative w-full h-[400px] md:h-[500px] shrink-0  rounded-2xl overflow-hidden shadow-2xl transition-all duration-500 select-none
+            ${isActive ? 'scale-100 opacity-100' : 'scale-90 opacity-60'}
+            `}></div>
     )
 }
 export default Slide
